98bc4989b0baadc2839335b43f0a6cb641c5ef52
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-09-24 17:46:17 +0100

    Get secretsmanager ARNs using terraform data
    This means we don't have to hardcode things which are obviously dynamic
    / random.
    
    Doing things this way is still not entirely ideal though, because:
    
    - Getting secrets by name like this means they're global to an AWS
      account - what if we want to run more than one copy of the platform
      with different secrets in the same AWS account
    - The dependency between the terraform and which secrets need to be in
      secretsmanager isn't clear - at the moment these secrets were added by
      hand, but it would be nice to try to automate that as much as
      possible...
    
    Some of these OAUTH_ID / OAUTH_SECRET things look like they were
    probably created by signon, which is not going to be a great pattern.
    
    Ideally we'd be able to generate all the secrets the apps need before
    running the apps themselves (otherwise there's an annyoing bootstrapping
    issue).

--- terraform/modules/apps/content-store/main.tf
+++ terraform/modules/apps/content-store/main.tf
@@ -7,106 +7,125 @@ terraform {
   }
 }
 
+data "aws_secretsmanager_secret" "oauth_id" {
+  name = "content_store_app-OAUTH_ID"
+}
+data "aws_secretsmanager_secret" "oauth_secret" {
+  name = "content_store_app-OAUTH_SECRET"
+}
+data "aws_secretsmanager_secret" "publishing_api_bearer_token" {
+  name = "content_store_app-PUBLISHING_API_BEARER_TOKEN"
+}
+data "aws_secretsmanager_secret" "router_api_bearer_token" {
+  name = "content_store_app-ROUTER_API_BEARER_TOKEN"
+}
+data "aws_secretsmanager_secret" "secret_key_base" {
+  name = "content_store_app-SECRET_KEY_BASE"
+}
+data "aws_secretsmanager_secret" "sentry_dsn" {
+  name = "content_store_app-SENTRY_DSN"
+}
+
 resource "aws_ecs_task_definition" "service" {
   family                   = var.service_name
   requires_compatibilities = ["FARGATE"]
-  container_definitions    = jsonencode([
+  container_definitions = jsonencode([
     {
-      "name": "content-store",
-      "image": "govuk/content-store:with-content-schemas",
-      "essential": true,
-      "environment": [
-        { "name": "APPMESH_VIRTUAL_NODE_NAME", "value": "mesh/govuk/virtualNode/content-store" },
-        { "name": "DEFAULT_TTL", "value": "1800" },
-        { "name": "GOVUK_APP_DOMAIN", "value": "test.govuk-internal.digital" },
-        { "name": "GOVUK_APP_DOMAIN_EXTERNAL", "value": "test.govuk.digital" },
-        { "name": "GOVUK_APP_NAME", "value": "content-store" },
-        { "name": "GOVUK_APP_TYPE", "value": "rack" },
-        { "name": "GOVUK_CONTENT_SCHEMAS_PATH", "value": "/govuk-content-schemas" },
-        { "name": "GOVUK_GROUP", "value": "deploy" },
-        { "name": "GOVUK_STATSD_PREFIX", "value": "fargate" },
-        { "name": "GOVUK_USER", "value": "deploy" },
-        { "name": "GOVUK_WEBSITE_ROOT", "value": "test.publishing.service.gov.uk" },
-        { "name": "MONGODB_URI", "value": "mongodb://mongo-1.test.govuk-internal.digital,mongo-2.test.govuk-internal.digital,mongo-3.test.govuk-internal.digital/content_store_production" },
-        { "name": "PLEK_SERVICE_PERFORMANCEPLATFORM_BIG_SCREEN_VIEW_URI", "value": "" },
-        { "name": "PLEK_SERVICE_PUBLISHING_API_URI", "value": "http://publishing-api.mesh.govuk-internal.digital" },
-        { "name": "PLEK_SERVICE_RUMMAGER_URI", "value": "" },
-        { "name": "PLEK_SERVICE_SPOTLIGHT_URI", "value": "" },
-        { "name": "PORT", "value": "80" },
-        { "name": "RAILS_ENV", "value": "production" },
-        { "name": "STATSD_PROTOCOL", "value": "tcp" },
-        { "name": "STATSD_HOST", "value": "statsd.test.govuk-internal.digital" },
-        { "name": "UNICORN_WORKER_PROCESSES", "value": "12" }
+      "name" : "content-store",
+      "image" : "govuk/content-store:with-content-schemas",
+      "essential" : true,
+      "environment" : [
+        { "name" : "APPMESH_VIRTUAL_NODE_NAME", "value" : "mesh/govuk/virtualNode/content-store" },
+        { "name" : "DEFAULT_TTL", "value" : "1800" },
+        { "name" : "GOVUK_APP_DOMAIN", "value" : "test.govuk-internal.digital" },
+        { "name" : "GOVUK_APP_DOMAIN_EXTERNAL", "value" : "test.govuk.digital" },
+        { "name" : "GOVUK_APP_NAME", "value" : "content-store" },
+        { "name" : "GOVUK_APP_TYPE", "value" : "rack" },
+        { "name" : "GOVUK_CONTENT_SCHEMAS_PATH", "value" : "/govuk-content-schemas" },
+        { "name" : "GOVUK_GROUP", "value" : "deploy" },
+        { "name" : "GOVUK_STATSD_PREFIX", "value" : "fargate" },
+        { "name" : "GOVUK_USER", "value" : "deploy" },
+        { "name" : "GOVUK_WEBSITE_ROOT", "value" : "test.publishing.service.gov.uk" },
+        { "name" : "MONGODB_URI", "value" : "mongodb://mongo-1.test.govuk-internal.digital,mongo-2.test.govuk-internal.digital,mongo-3.test.govuk-internal.digital/content_store_production" },
+        { "name" : "PLEK_SERVICE_PERFORMANCEPLATFORM_BIG_SCREEN_VIEW_URI", "value" : "" },
+        { "name" : "PLEK_SERVICE_PUBLISHING_API_URI", "value" : "http://publishing-api.mesh.govuk-internal.digital" },
+        { "name" : "PLEK_SERVICE_RUMMAGER_URI", "value" : "" },
+        { "name" : "PLEK_SERVICE_SPOTLIGHT_URI", "value" : "" },
+        { "name" : "PORT", "value" : "80" },
+        { "name" : "RAILS_ENV", "value" : "production" },
+        { "name" : "STATSD_PROTOCOL", "value" : "tcp" },
+        { "name" : "STATSD_HOST", "value" : "statsd.test.govuk-internal.digital" },
+        { "name" : "UNICORN_WORKER_PROCESSES", "value" : "12" }
       ],
-      "logConfiguration": {
-          "logDriver": "awslogs",
-          "options": {
-              "awslogs-create-group": "true",
-              "awslogs-group": "awslogs-fargate",
-              "awslogs-region": "eu-west-1",
-              "awslogs-stream-prefix": "awslogs-content-store"
-          }
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-create-group" : "true",
+          "awslogs-group" : "awslogs-fargate",
+          "awslogs-region" : "eu-west-1",
+          "awslogs-stream-prefix" : "awslogs-content-store"
+        }
       },
-      "mountPoints": [],
-      "portMappings": [
+      "mountPoints" : [],
+      "portMappings" : [
         {
-          "containerPort": 80,
-          "hostPort": 80,
-          "protocol": "tcp"
+          "containerPort" : 80,
+          "hostPort" : 80,
+          "protocol" : "tcp"
         }
       ],
-      "secrets": [
+      "secrets" : [
         {
-          "name": "OAUTH_ID",
-          "valueFrom": "arn:aws:secretsmanager:eu-west-1:430354129336:secret:content_store_app-OAUTH_ID-11LnJS"
+          "name" : "OAUTH_ID",
+          "valueFrom" : data.aws_secretsmanager_secret.oauth_id.arn
         },
         {
-          "name": "OAUTH_SECRET",
-          "valueFrom": "arn:aws:secretsmanager:eu-west-1:430354129336:secret:content_store_app-OAUTH_SECRET-7qilGD"
+          "name" : "OAUTH_SECRET",
+          "valueFrom" : data.aws_secretsmanager_secret.oauth_secret.arn
         },
         {
-          "name": "PUBLISHING_API_BEARER_TOKEN",
-          "valueFrom": "arn:aws:secretsmanager:eu-west-1:430354129336:secret:content_store_app-PUBLISHING_API_BEARER_TOKEN-haQc7Q"
+          "name" : "PUBLISHING_API_BEARER_TOKEN",
+          "valueFrom" : data.aws_secretsmanager_secret.publishing_api_bearer_token.arn
         },
         {
-          "name": "ROUTER_API_BEARER_TOKEN",
-          "valueFrom": "arn:aws:secretsmanager:eu-west-1:430354129336:secret:content_store_app-ROUTER_API_BEARER_TOKEN-c2zv3E"
+          "name" : "ROUTER_API_BEARER_TOKEN",
+          "valueFrom" : data.aws_secretsmanager_secret.router_api_bearer_token.arn
         },
         {
-          "name": "SECRET_KEY_BASE",
-          "valueFrom": "arn:aws:secretsmanager:eu-west-1:430354129336:secret:content_store_app-SECRET_KEY_BASE-3QKPrJ"
+          "name" : "SECRET_KEY_BASE",
+          "valueFrom" : data.aws_secretsmanager_secret.secret_key_base.arn
         },
         {
-          "name": "SENTRY_DSN",
-          "valueFrom": "arn:aws:secretsmanager:eu-west-1:430354129336:secret:content_store_app-SENTRY_DSN-Ixx0fZ"
+          "name" : "SENTRY_DSN",
+          "valueFrom" : data.aws_secretsmanager_secret.sentry_dsn.arn
         }
       ]
     },
     {
-      "name": "envoy",
-      "image": "840364872350.dkr.ecr.eu-west-1.amazonaws.com/aws-appmesh-envoy:v1.15.0.0-prod",
-      "user": "1337",
-      "environment": [
-        { "name": "APPMESH_VIRTUAL_NODE_NAME", "value": "mesh/govuk/virtualNode/content-store" },
-        { "name": "ENVOY_LOG_LEVEL", "value": "debug" }
+      "name" : "envoy",
+      "image" : "840364872350.dkr.ecr.eu-west-1.amazonaws.com/aws-appmesh-envoy:v1.15.0.0-prod",
+      "user" : "1337",
+      "environment" : [
+        { "name" : "APPMESH_VIRTUAL_NODE_NAME", "value" : "mesh/govuk/virtualNode/content-store" },
+        { "name" : "ENVOY_LOG_LEVEL", "value" : "debug" }
       ],
-      "essential": true,
-      "logConfiguration": {
-          "logDriver": "awslogs",
-          "options": {
-              "awslogs-create-group": "true",
-              "awslogs-group": "awslogs-fargate",
-              "awslogs-region": "eu-west-1",
-              "awslogs-stream-prefix": "awslogs-content-store-envoy"
-          }
+      "essential" : true,
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-create-group" : "true",
+          "awslogs-group" : "awslogs-fargate",
+          "awslogs-region" : "eu-west-1",
+          "awslogs-stream-prefix" : "awslogs-content-store-envoy"
+        }
       }
     }
   ])
-  network_mode             = "awsvpc"
-  cpu                      = 512
-  memory                   = 1024
-  task_role_arn            = var.task_role_arn
-  execution_role_arn       = var.execution_role_arn
+  network_mode       = "awsvpc"
+  cpu                = 512
+  memory             = 1024
+  task_role_arn      = var.task_role_arn
+  execution_role_arn = var.execution_role_arn
 
   proxy_configuration {
     type           = "APPMESH"
