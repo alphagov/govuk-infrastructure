55e1c4258de5f10bb28ff7eb4799887a7825ed5b
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-10-27 11:54:26 +0000

    Create router app deployment module
    This will enable a Concourse deployment pipeline to use Terraform
    to create a new task definition revision for the app in the test
    environment, by providing only the image_tag.
    
    Co-authored-by: Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>

---
+++ terraform/deployments/apps/test/router/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "s3" {
+    bucket  = "govuk-terraform-test"
+    key     = "projects/router.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.69"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "task_definition" {
+  source             = "../../../../modules/task-definitions/router"
+  image_tag          = var.image_tag
+  mesh_name          = local.mesh_name
+  execution_role_arn = data.aws_iam_role.execution.arn
+  mongodb_host       = "router-backend-1.test.govuk-internal.digital,router-backend-2.test.govuk-internal.digital,router-backend-3.test.govuk-internal.digital"
+  task_role_arn      = data.aws_iam_role.task.arn
+  sentry_environment = local.sentry_environment
+}
