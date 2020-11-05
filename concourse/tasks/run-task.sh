e0a305baa70e47a210c3dac488b4fd5688994473
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-11-05 10:41:27 +0000

    Run database migrations for Publisher during deploy
    This will run database migrations using a new task Publisher application task
    definition prior to updating the ECS Service to use the new task definition.
    
    The command 'rails db:migrate' will run in a separate task_runner cluster,
    and the one-off containers will not be responsible for serving user requests.
    
    This follows the design proposal in [proposal 005].
    
    [proposal 005]: https://github.com/alphagov/govuk-replatforming-discovery-2020/blob/1ceebbb0319ecb3b6be35e3dedea2331638cf5a8/proposals/005-ecs-fargate-deployment.md#database-migrations-will-be-performed-before-new-code-is-deployed
    
    The run-task task is intentionally generic so that it can run arbitrary
    commands in a container with the same configuration as those serving users.
    
    Additional commits are required to enable db migrations for other apps.
    
    Story: https://trello.com/c/jepU0c7G/232

---
+++ concourse/tasks/run-task.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env sh
+# This script runs an arbitrary task using an existing task defintion in a
+# new container, using ECS RunTask.
+
+set -eu
+
+root_dir=$(pwd)
+
+# Raise error if env vars not set
+: "${ASSUME_ROLE_ARN:?ASSUME_ROLE_ARN not set}"
+: "${AWS_REGION:?AWS_REGION not set}"
+: "${APPLICATION:?APPLICATION not set}"
+: "${COMMAND:?COMMAND not set}"
+: "${CLUSTER:?COMMAND not set}"
+
+mkdir -p ~/.aws
+
+cat <<EOF > ~/.aws/config
+[profile default]
+role_arn = $ASSUME_ROLE_ARN
+credential_source = Ec2InstanceMetadata
+region = $AWS_REGION
+EOF
+
+task_definition_arn=$(cat terraform-outputs/task_definition_arn)
+network_config=$(cat terraform-outputs/task_network_config)
+
+echo "Starting task..."
+
+task=$(aws ecs run-task --cluster $CLUSTER \
+--task-definition $task_definition_arn --launch-type FARGATE --count 1 \
+--network-configuration $network_config \
+--overrides '{
+  "containerOverrides": [{
+    "name": "'"$APPLICATION"'",
+    "command": ["/bin/bash", "-c", "'"$COMMAND"'"]
+  }]
+}')
+
+task_arn=$(echo $task | jq .tasks[0].taskArn)
+
+echo "waiting for task $task_arn to finish..."
+
+aws ecs wait tasks-stopped --tasks=[$task_arn] --cluster $CLUSTER
+
+echo "task finished."
+
+task_results=$(aws ecs describe-tasks --tasks=[$task_arn] --cluster $CLUSTER)
+echo $task_results
+
+exit_code=$(echo $task_results | jq [.tasks[0].containers[].exitCode] | jq add)
+
+echo "Exiting with code $exit_code"
+
+exit $exit_code
