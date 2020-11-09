5d2cc079fc0cfca7a734044b728b3f9d79df9cf0
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2020-11-09 15:03:29 +0000

    Add Static App
    This PR adds the Static app in the new platform as part of the work on
    spinning up a minimal GOV.UK stack in ECS.
    
    We verified that:
    1. new secrets have been added via aws cli
    2. govuk-test deployment can be applied with no errors
    3. concourse deployment of the Static app is successful
    
    Ref:
    1. [task trello card]
    (https://trello.com/c/iC7BWjB6/282-spin-up-static-app)
    2. [minimal set of apps trello card]
    (https://trello.com/c/X4jlkXyE/200-determine-a-minimal-subset-of-apps)
    
    Co-authored-by: smford <stephen.ford@digital.cabinet-office.gov.uk>

---
+++ terraform/modules/apps/static/README.md
@@ -0,0 +1,3 @@
+# Static application
+
+This project module manages the resources required to run Static.
---
+++ terraform/modules/task-definitions/static/variables.tf
@@ -0,0 +1,44 @@
+variable "image_tag" {
+  description = "Container Image Tag"
+  type        = string
+}
+
+variable "mesh_name" {
+  type = string
+}
+
+variable "execution_role_arn" {
+  type = string
+}
+
+variable "task_role_arn" {
+  type = string
+}
+
+variable "sentry_environment" {
+  type = string
+}
+
+variable "assume_role_arn" {
+  type        = string
+  description = "(optional) AWS IAM role to assume. Uses the role from the environment by default."
+  default     = null
+}
+
+variable "service_name" {
+  type = string
+}
+
+variable "redis_host" {
+  type = string
+}
+
+variable "redis_port" {
+  type    = number
+  default = 6379
+}
+
+variable "assets_url" {
+  type        = string
+  description = "URL of the Assets service"
+}
