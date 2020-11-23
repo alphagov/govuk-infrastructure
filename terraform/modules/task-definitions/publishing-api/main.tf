5848d418d9c8d1d8e89709ab608f5f407e34dff8
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-11-23 16:15:49 +0000

    WIP Add Sidekiq workers as sidecar containers

--- terraform/modules/task-definitions/publishing-api/main.tf
+++ terraform/modules/task-definitions/publishing-api/main.tf
@@ -59,6 +59,119 @@ data "aws_secretsmanager_secret" "sentry_dsn" {
   name = "SENTRY_DSN"
 }
 
+locals {
+  default_container_definition = {
+    "image" : "govuk/${local.service_name}:${var.image_tag}",
+    "essential" : true,
+    "environment" : [
+      # TODO: factor our hardcoded stuff
+      { "name" : "CONTENT_API_PROTOTYPE", "value" : "yes" },
+      { "name" : "CONTENT_STORE", "value" : "http://content-store.${var.service_discovery_namespace_name}" },
+      { "name" : "DRAFT_CONTENT_STORE", "value" : "https://draft-content-store.${var.service_discovery_namespace_name}" },
+      { "name" : "EVENT_LOG_AWS_ACCESS_ID", "value" : "AKIAJE6VSW25CYBUMQJA" },
+      { "name" : "EVENT_LOG_AWS_BUCKETNAME", "value" : "govuk-${local.service_name}-event-log-test" },
+      { "name" : "EVENT_LOG_AWS_USERNAME", "value" : "govuk-${local.service_name}-event-log_user" },
+      { "name" : "DEFAULT_TTL", "value" : "1800" },
+      { "name" : "GOVUK_APP_DOMAIN", "value" : var.service_discovery_namespace_name },
+      { "name" : "GOVUK_APP_DOMAIN_EXTERNAL", "value" : var.govuk_app_domain_external },
+      { "name" : "GOVUK_APP_NAME", "value" : "publishing-api" },
+      { "name" : "GOVUK_APP_TYPE", "value" : "rack" },
+      { "name" : "GOVUK_CONTENT_SCHEMAS_PATH", "value" : "/govuk-content-schemas" },
+      { "name" : "GOVUK_GROUP", "value" : "deploy" }, # TODO: clean up?
+      { "name" : "GOVUK_STATSD_PREFIX", "value" : "fargate" },
+      { "name" : "GOVUK_USER", "value" : "deploy" }, # TODO: clean up?
+      { "name" : "GOVUK_WEBSITE_ROOT", "value" : var.govuk_website_root },
+      { "name" : "PLEK_SERVICE_CONTENT_STORE_URI", "value" : "http://content-store.${var.service_discovery_namespace_name}" },
+      { "name" : "PLEK_SERVICE_DRAFT_CONTENT_STORE_URI", "value" : "http://draft-content-store.${var.service_discovery_namespace_name}" },
+      { "name" : "PORT", "value" : "80" },
+      { "name" : "RABBITMQ_HOSTS", "value" : "rabbitmq.${var.govuk_app_domain_internal}" },
+      { "name" : "RABBITMQ_USER", "value" : "publishing_api" },
+      { "name" : "RABBITMQ_VHOST", "value" : "/" },
+      { "name" : "REDIS_HOST", "value" : var.redis_host },
+      { "name" : "REDIS_PORT", "value" : tostring(var.redis_port) },
+      { "name" : "REDIS_URL", "value" : "redis://${var.redis_host}:${var.redis_port}" },
+      { "name" : "RAILS_ENV", "value" : "production" },
+      { "name" : "STATSD_PROTOCOL", "value" : "tcp" },
+      { "name" : "STATSD_HOST", "value" : var.statsd_host },
+      { "name" : "UNICORN_WORKER_PROCESSES", "value" : "8" }
+    ],
+    "dependsOn" : [{
+      "containerName" : "envoy",
+      "condition" : "START"
+    }],
+    "logConfiguration" : {
+      "logDriver" : "awslogs",
+      "options" : {
+        "awslogs-create-group" : "true",
+        "awslogs-group" : "awslogs-fargate",
+        "awslogs-region" : "eu-west-1",
+        "awslogs-stream-prefix" : "awslogs-${local.service_name}"
+      }
+    },
+    "mountPoints" : [],
+    "secrets" : [
+      {
+        "name" : "CONTENT_STORE_BEARER_TOKEN",
+        "valueFrom" : data.aws_secretsmanager_secret.content_store_bearer_token.arn
+      },
+      {
+        "name" : "DATABASE_URL",
+        "valueFrom" : data.aws_secretsmanager_secret.database_url.arn
+      },
+      {
+        "name" : "DRAFT_CONTENT_STORE_BEARER_TOKEN",
+        "valueFrom" : data.aws_secretsmanager_secret.draft_content_store_bearer_token.arn
+      },
+      {
+        "name" : "EVENT_LOG_AWS_SECRET_KEY",
+        "valueFrom" : data.aws_secretsmanager_secret.event_log_aws_secret_key.arn
+      },
+      {
+        "name" : "GDS_SSO_OAUTH_ID",
+        "valueFrom" : data.aws_secretsmanager_secret.oauth_id.arn
+      },
+      {
+        "name" : "GDS_SSO_OAUTH_SECRET",
+        "valueFrom" : data.aws_secretsmanager_secret.oauth_secret.arn
+      },
+      {
+        "name" : "RABBITMQ_PASSWORD",
+        "valueFrom" : data.aws_secretsmanager_secret.rabbitmq_password.arn
+      },
+      {
+        "name" : "ROUTER_API_BEARER_TOKEN",
+        "valueFrom" : data.aws_secretsmanager_secret.router_api_bearer_token.arn
+      },
+      {
+        "name" : "SECRET_KEY_BASE",
+        "valueFrom" : data.aws_secretsmanager_secret.secret_key_base.arn
+      },
+      {
+        "name" : "SENTRY_DSN",
+        "valueFrom" : data.aws_secretsmanager_secret.sentry_dsn.arn
+      }
+    ]
+  }
+
+  worker_container_parameters = {
+    "name" : "${local.service_name}-worker",
+    "command" : ["foreman", "run", "worker"],
+    "portMappings" : []
+  }
+
+  web_container_parameters = {
+    "name" : local.service_name,
+    "command" : ["foreman", "run", "web"],
+    "portMappings" : [
+      {
+        "containerPort" : 80,
+        "hostPort" : 80,
+        "protocol" : "tcp"
+      }
+    ]
+  }
+}
+
 module "task_definition" {
   source             = "../../task-definition"
   mesh_name          = var.mesh_name
@@ -69,105 +182,7 @@ module "task_definition" {
   task_role_arn      = var.task_role_arn
 
   container_definitions = [
-    {
-      "name" : local.service_name,
-      "image" : "govuk/${local.service_name}:${var.image_tag}",
-      "essential" : true,
-      "environment" : [
-        # TODO: factor our hardcoded stuff
-        { "name" : "CONTENT_API_PROTOTYPE", "value" : "yes" },
-        { "name" : "CONTENT_STORE", "value" : "http://content-store.${var.service_discovery_namespace_name}" },
-        { "name" : "DRAFT_CONTENT_STORE", "value" : "https://draft-content-store.${var.service_discovery_namespace_name}" },
-        { "name" : "EVENT_LOG_AWS_ACCESS_ID", "value" : "AKIAJE6VSW25CYBUMQJA" },
-        { "name" : "EVENT_LOG_AWS_BUCKETNAME", "value" : "govuk-${local.service_name}-event-log-test" },
-        { "name" : "EVENT_LOG_AWS_USERNAME", "value" : "govuk-${local.service_name}-event-log_user" },
-        { "name" : "DEFAULT_TTL", "value" : "1800" },
-        { "name" : "GOVUK_APP_DOMAIN", "value" : var.service_discovery_namespace_name },
-        { "name" : "GOVUK_APP_DOMAIN_EXTERNAL", "value" : var.govuk_app_domain_external },
-        { "name" : "GOVUK_APP_NAME", "value" : "publishing-api" },
-        { "name" : "GOVUK_APP_TYPE", "value" : "rack" },
-        { "name" : "GOVUK_CONTENT_SCHEMAS_PATH", "value" : "/govuk-content-schemas" },
-        { "name" : "GOVUK_GROUP", "value" : "deploy" }, # TODO: clean up?
-        { "name" : "GOVUK_STATSD_PREFIX", "value" : "fargate" },
-        { "name" : "GOVUK_USER", "value" : "deploy" }, # TODO: clean up?
-        { "name" : "GOVUK_WEBSITE_ROOT", "value" : var.govuk_website_root },
-        { "name" : "PLEK_SERVICE_CONTENT_STORE_URI", "value" : "http://content-store.${var.service_discovery_namespace_name}" },
-        { "name" : "PLEK_SERVICE_DRAFT_CONTENT_STORE_URI", "value" : "http://draft-content-store.${var.service_discovery_namespace_name}" },
-        { "name" : "PORT", "value" : "80" },
-        { "name" : "RABBITMQ_HOSTS", "value" : "rabbitmq.${var.govuk_app_domain_internal}" },
-        { "name" : "RABBITMQ_USER", "value" : "publishing_api" },
-        { "name" : "RABBITMQ_VHOST", "value" : "/" },
-        { "name" : "REDIS_HOST", "value" : var.redis_host },
-        { "name" : "REDIS_PORT", "value" : tostring(var.redis_port) },
-        { "name" : "REDIS_URL", "value" : "redis://${var.redis_host}:${var.redis_port}" },
-        { "name" : "RAILS_ENV", "value" : "production" },
-        { "name" : "STATSD_PROTOCOL", "value" : "tcp" },
-        { "name" : "STATSD_HOST", "value" : var.statsd_host },
-        { "name" : "UNICORN_WORKER_PROCESSES", "value" : "8" }
-      ],
-      "dependsOn" : [{
-        "containerName" : "envoy",
-        "condition" : "START"
-      }],
-      "logConfiguration" : {
-        "logDriver" : "awslogs",
-        "options" : {
-          "awslogs-create-group" : "true",
-          "awslogs-group" : "awslogs-fargate",
-          "awslogs-region" : "eu-west-1",
-          "awslogs-stream-prefix" : "awslogs-${local.service_name}"
-        }
-      },
-      "mountPoints" : [],
-      "portMappings" : [
-        {
-          "containerPort" : 80,
-          "hostPort" : 80,
-          "protocol" : "tcp"
-        }
-      ],
-      "secrets" : [
-        {
-          "name" : "CONTENT_STORE_BEARER_TOKEN",
-          "valueFrom" : data.aws_secretsmanager_secret.content_store_bearer_token.arn
-        },
-        {
-          "name" : "DATABASE_URL",
-          "valueFrom" : data.aws_secretsmanager_secret.database_url.arn
-        },
-        {
-          "name" : "DRAFT_CONTENT_STORE_BEARER_TOKEN",
-          "valueFrom" : data.aws_secretsmanager_secret.draft_content_store_bearer_token.arn
-        },
-        {
-          "name" : "EVENT_LOG_AWS_SECRET_KEY",
-          "valueFrom" : data.aws_secretsmanager_secret.event_log_aws_secret_key.arn
-        },
-        {
-          "name" : "GDS_SSO_OAUTH_ID",
-          "valueFrom" : data.aws_secretsmanager_secret.oauth_id.arn
-        },
-        {
-          "name" : "GDS_SSO_OAUTH_SECRET",
-          "valueFrom" : data.aws_secretsmanager_secret.oauth_secret.arn
-        },
-        {
-          "name" : "RABBITMQ_PASSWORD",
-          "valueFrom" : data.aws_secretsmanager_secret.rabbitmq_password.arn
-        },
-        {
-          "name" : "ROUTER_API_BEARER_TOKEN",
-          "valueFrom" : data.aws_secretsmanager_secret.router_api_bearer_token.arn
-        },
-        {
-          "name" : "SECRET_KEY_BASE",
-          "valueFrom" : data.aws_secretsmanager_secret.secret_key_base.arn
-        },
-        {
-          "name" : "SENTRY_DSN",
-          "valueFrom" : data.aws_secretsmanager_secret.sentry_dsn.arn
-        }
-      ]
-    }
+    merge(local.default_container_definition, local.web_container_parameters),
+    merge(local.default_container_definition, local.worker_container_parameters),
   ]
 }
