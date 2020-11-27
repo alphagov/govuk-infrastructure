58154f70874f2de3bf21ba80fff40056330be0da
mahmudh <mahmud.hussain@digital.cabinet-office.gov.uk>
2020-11-27 16:06:20 +0000

    Add worker service for Publishing API

--- terraform/modules/apps/publishing-api/main.tf
+++ terraform/modules/apps/publishing-api/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-module "app" {
+module "web" {
   source                           = "../../app"
   execution_role_arn               = var.execution_role_arn
   vpc_id                           = var.vpc_id
@@ -20,4 +20,19 @@ module "app" {
   service_discovery_namespace_id   = var.service_discovery_namespace_id
   service_discovery_namespace_name = var.service_discovery_namespace_name
   extra_security_groups            = [var.govuk_management_access_security_group]
+}
+
+module "worker" {
+  source                           = "../../app"
+  execution_role_arn               = var.execution_role_arn
+  vpc_id                           = var.vpc_id
+  cluster_id                       = var.cluster_id
+  service_name                     = "${var.service_name}-worker"
+  subnets                          = var.private_subnets
+  mesh_name                        = var.mesh_name
+  desired_count                    = var.desired_count
+  mesh_subdomain                   = "publishing-api-worker"
+  service_discovery_namespace_id   = var.service_discovery_namespace_id
+  service_discovery_namespace_name = var.service_discovery_namespace_name
+  extra_security_groups            = [module.web.security_group_id, var.govuk_management_access_security_group]
 }
--- terraform/modules/apps/publishing-api/outputs.tf
+++ terraform/modules/apps/publishing-api/outputs.tf
@@ -1,4 +1,4 @@
 output "app_security_group_id" {
-  value       = module.app.security_group_id
+  value       = module.web.security_group_id
   description = "ID of the security group for Publishing API instances."
 }
