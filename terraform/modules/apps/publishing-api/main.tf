b5c402ca41a4341d037c8cf0e1eb4afc9278a17a
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-11-27 12:31:34 +0000

    Rename publishing-api to publishing-api-web
    Publishing API runs both a web and worker process, so this adds
    the -web suffix for clarity. The subdomain for the service in the
    mesh will still be publishing-api, so we don't need to change any
    other task definitions.

--- terraform/modules/apps/publishing-api/main.tf
+++ terraform/modules/apps/publishing-api/main.tf
@@ -12,10 +12,11 @@ module "app" {
   execution_role_arn               = var.execution_role_arn
   vpc_id                           = var.vpc_id
   cluster_id                       = var.cluster_id
-  service_name                     = var.service_name
+  service_name                     = "${var.service_name}-web"
   subnets                          = var.private_subnets
   mesh_name                        = var.mesh_name
   desired_count                    = var.desired_count
+  mesh_subdomain                   = "publishing-api"
   service_discovery_namespace_id   = var.service_discovery_namespace_id
   service_discovery_namespace_name = var.service_discovery_namespace_name
   extra_security_groups            = [var.govuk_management_access_security_group]
