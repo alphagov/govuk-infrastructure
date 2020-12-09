b5623cbc065f7f72df88c9515ba9887eb1b23dbc
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-12-09 16:00:18 +0000

    Remove an unused terraform deployment and a module
    Neither of these are currently being used - they're from an earlier bit
    of the project where we were doing proofs of concept. We can always
    bring them back from the git history if we need them later, but removing
    them reduces clutter a bit.

--- terraform/modules/fargate-console/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "service_name" {
-  description = "Service name of the Fargate service, cluster, task etc."
-  type        = string
-}
-
-variable "container_definitions" {
-  description = "List of container definitions, usually provided as a JSON file"
-  type        = string
-}
