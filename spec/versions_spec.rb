a4847fbd99c90f172fd40de192671c0329b7c364
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-12-11 18:46:28 +0000

    Add rspec tests to check versions are in sync
    We've got a few places popping up where versions of things will need to
    be kept in sync. For example, we're using terraform 0.13.3 locally, and
    in concourse, and in GitHub Actions. There's no simple way of making all
    these tools read from a canonical source, so instead let's just add some
    tests that things are in-sync.
    
    I'm using ruby / rspec for these tests, because this is
    govuk-infrastructure and govuk developers are familiar with ruby /
    rspec. This adds a new set of versions to keep in sync 🙈.
    
    I hope that these tests will expand to cover more areas (not just
    version syncing) in future.
    
    One thing to note is that my use of `.dig()` is a bit fragile - if we
    move tasks around, these tests will break. I could do it in a more
    robust way (i.e. by specifying "the task with name run-terraform"
    instead of specifying "the task with index 1"), but it would be a bit
    more code.

---
+++ spec/versions_spec.rb
@@ -0,0 +1,57 @@
+require 'yaml'
+
+YamlPath = Struct.new(:file, :path)
+
+RSpec.describe "Versions" do
+  describe "Terraform" do
+    canonical_terraform_version = File.read("terraform/.terraform-version").chomp
+
+    describe "Concourse" do
+      paths = [
+        YamlPath.new("concourse/pipelines/deploy.yml", ["jobs", 1, "plan", 1, "config", "image_resource", "source", "tag"]),
+        YamlPath.new("concourse/tasks/update-task-definition.yml", ["image_resource", "source", "tag"]),
+      ]
+
+      paths.each do |yaml_path|
+        it "#{yaml_path.file} should use the canonical terraform version (#{canonical_terraform_version})" do
+          yaml = YAML.load_file(yaml_path.file)
+          terraform_image_tag = yaml.dig(*yaml_path.path)
+          expect(terraform_image_tag).to eql "terraform-#{canonical_terraform_version}"
+        end
+      end
+    end
+
+    describe "GitHub Actions" do
+      paths = [
+        YamlPath.new(".github/workflows/ci.yml", ["jobs", "test", "steps", 1, "with", "terraform_version"])
+      ]
+
+      paths.each do |yaml_path|
+        it "#{yaml_path.file} should use the canonical terraform version (#{canonical_terraform_version})" do
+          yaml = YAML.load_file(yaml_path.file)
+          terraform_version = yaml.dig(*yaml_path.path)
+          expect(terraform_version).to eql canonical_terraform_version
+        end
+      end
+    end
+
+  end
+
+  describe "Ruby" do
+    canonical_ruby_version = File.read(".ruby-version").chomp
+
+    describe "GitHub Actions" do
+      paths = [
+        YamlPath.new(".github/workflows/ci.yml", ["jobs", "test", "steps", 2, "with", "ruby-version"])
+      ]
+
+      paths.each do |yaml_path|
+        it "#{yaml_path.file} should use the canonical ruby version (#{canonical_ruby_version})" do
+          yaml = YAML.load_file(yaml_path.file)
+          ruby_version = yaml.dig(*yaml_path.path)
+          expect(ruby_version).to eql canonical_ruby_version
+        end
+      end
+    end
+  end
+end
