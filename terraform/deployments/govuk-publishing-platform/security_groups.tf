8b53ad560bebcad3034e4437693098bfb1048744
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2021-01-20 19:03:15 +0000

    Add smokey application to ECS
    This will enable us to run smoke tests as an ECS Task, both
    as an ad-hoc job run after every deploy, and as a
    ECS ScheduledTask.
    
    The ScheduledTask part (for alerting, currently called the smokey
    loop by puppet) will be implemented later.

--- terraform/deployments/govuk-publishing-platform/security_groups.tf
+++ terraform/deployments/govuk-publishing-platform/security_groups.tf
@@ -5,3 +5,9 @@ resource "aws_security_group" "mesh_ecs_service" {
   vpc_id      = local.vpc_id
   description = "Associated with all ECS Services that are virtual services in the AppMesh mesh"
 }
+
+resource "aws_security_group" "smokey" {
+  name        = "ecs_fargate_smokey"
+  vpc_id      = local.vpc_id
+  description = "Smoke test runner"
+}
