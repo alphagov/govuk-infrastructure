cdcc1d7143e632805c7d5c2e8885239865354f99
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2021-01-20 18:46:05 +0000

    Output task_network_config from publisher-web deployment
    This output will make it possible to acquire both the task
    to be run and the network configuration for the task in a
    single terraform apply / terraform output step.
    
    This is useful, since we can run any tasks with ECS RunTask
    using the Terraform output from a single deployment.
    Concretely, this will cut down on the number of tasks in
    our Concourse pipeline.

--- terraform/deployments/apps/publisher-web/outputs.tf
+++ terraform/deployments/apps/publisher-web/outputs.tf
@@ -2,3 +2,8 @@ output "web_task_definition" {
   value       = module.task_definition.arn
   description = "ARN of the web task definition revision"
 }
+
+output "task_network_config" {
+  value       = module.network_config.network_config
+  description = "Used by ECS RunTask."
+}
