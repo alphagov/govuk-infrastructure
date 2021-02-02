26f34f32360d722bce6e08ec090574782101d6d7
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2021-02-02 09:22:41 +0000

    review: Improve task logging

--- concourse/tasks/update-ecs-service.sh
+++ concourse/tasks/update-ecs-service.sh
@@ -12,7 +12,7 @@ EOF
 
 new_task_definition_arn="$(cat "task-definition-arn/task-definition-arn")"
 if [ -z "${new_task_definition_arn}" ]; then
-  echo "failed to retrieve new task definition, exiting..."
+  echo "failed to retrieve new task definition for $ECS_SERVICE, exiting..."
   exit 1
 fi
 
