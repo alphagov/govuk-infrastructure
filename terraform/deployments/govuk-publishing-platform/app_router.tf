0accbaa4d0a055a6c561e45bac8ac7536aa1b3b3
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2021-02-04 21:30:18 +0000

    refactor (draft-)router to new deployment style
    In PR #150, we refactored the way we deploy applications by outputting the
    application task definition json when applying the govuk platforming terraform.
    This json file is then diffed with previous version and if changes exist, a new
    task definition is created for the app using AWS cli.
    
    This PR refactors  (draft-)router app to use this new deployment style.
    
    Similar PRs are #152, #153 and #154 for other apps.
    
    Ref:
    1. [trello card](https://trello.com/c/1lZxLN8V/380-update-the-router-application-to-use-the-new-deployment-approach)

--- terraform/deployments/govuk-publishing-platform/app_router.tf
+++ terraform/deployments/govuk-publishing-platform/app_router.tf
@@ -1,49 +1,89 @@
+locals {
+  router_defaults = {
+    cpu    = 512  # TODO parameterize this
+    memory = 1024 # TODO parameterize this
+
+    environment_variables = merge(
+      local.defaults.environment_variables,
+      {
+        GOVUK_APP_NAME                = "router",
+        GOVUK_APP_ROOT                = "/var/apps/router",
+        ROUTER_APIADDR                = ":3055",
+        ROUTER_BACKEND_HEADER_TIMEOUT = "20s",
+        ROUTER_PUBADDR                = ":80",
+      }
+    )
+
+    secrets_from_arns = local.defaults.secrets_from_arns
+
+    mongodb_hosts = "mongodb://${join(",", [
+      data.terraform_remote_state.govuk_aws_router_mongo.outputs.router_backend_1_service_dns_name,
+      data.terraform_remote_state.govuk_aws_router_mongo.outputs.router_backend_2_service_dns_name,
+      data.terraform_remote_state.govuk_aws_router_mongo.outputs.router_backend_3_service_dns_name,
+    ])}"
+  }
+}
+
 module "router" {
+  source = "../../modules/app"
+
   service_name                     = "router"
   mesh_name                        = aws_appmesh_mesh.govuk.id
   service_discovery_namespace_id   = aws_service_discovery_private_dns_namespace.govuk_publishing_platform.id
   service_discovery_namespace_name = aws_service_discovery_private_dns_namespace.govuk_publishing_platform.name
-  subnets                          = local.private_subnets
-  vpc_id                           = local.vpc_id
   cluster_id                       = aws_ecs_cluster.cluster.id
-  source                           = "../../modules/app"
+  vpc_id                           = local.vpc_id
+  subnets                          = local.private_subnets
   desired_count                    = var.router_desired_count
   extra_security_groups            = [local.govuk_management_access_security_group, aws_security_group.mesh_ecs_service.id]
   custom_container_services = [
     { container_service = "router", port = 80, protocol = "tcp" },
     { container_service = "router-reload", port = 3055, protocol = "tcp" },
   ]
-  environment_variables = {} # TODO
-  secrets_from_arns     = {} # TODO
-  log_group             = local.log_group
-  aws_region            = data.aws_region.current.name
-  cpu                   = 512
-  memory                = 1024
-  task_role_arn         = aws_iam_role.task.arn
-  execution_role_arn    = aws_iam_role.execution.arn
+  environment_variables = merge(
+    local.router_defaults.environment_variables,
+    {
+      ROUTER_MONGO_DB  = "router"
+      ROUTER_MONGO_URL = "mongodb://${local.router_defaults.mongodb_hosts}/router",
+    },
+  )
+  secrets_from_arns  = local.router_defaults.secrets_from_arns
+  log_group          = local.log_group
+  aws_region         = data.aws_region.current.name
+  cpu                = local.router_defaults.cpu
+  memory             = local.router_defaults.memory
+  task_role_arn      = aws_iam_role.task.arn
+  execution_role_arn = aws_iam_role.execution.arn
 }
 
 module "draft_router" {
+  source = "../../modules/app"
+
   service_name                     = "draft-router"
   mesh_name                        = aws_appmesh_mesh.govuk.id
   service_discovery_namespace_id   = aws_service_discovery_private_dns_namespace.govuk_publishing_platform.id
   service_discovery_namespace_name = aws_service_discovery_private_dns_namespace.govuk_publishing_platform.name
-  subnets                          = local.private_subnets
-  vpc_id                           = local.vpc_id
   cluster_id                       = aws_ecs_cluster.cluster.id
-  source                           = "../../modules/app"
+  vpc_id                           = local.vpc_id
+  subnets                          = local.private_subnets
   desired_count                    = var.draft_router_desired_count
   extra_security_groups            = [local.govuk_management_access_security_group, aws_security_group.mesh_ecs_service.id]
   custom_container_services = [
     { container_service = "draft-router", port = 80, protocol = "tcp" },
     { container_service = "draft-router-reload", port = 3055, protocol = "tcp" },
   ]
-  environment_variables = {} # TODO
-  secrets_from_arns     = {} # TODO
-  log_group             = local.log_group
-  aws_region            = data.aws_region.current.name
-  cpu                   = 512
-  memory                = 1024
-  task_role_arn         = aws_iam_role.task.arn
-  execution_role_arn    = aws_iam_role.execution.arn
+  environment_variables = merge(
+    local.router_defaults.environment_variables,
+    {
+      ROUTER_MONGO_DB  = "draft_router"
+      ROUTER_MONGO_URL = "mongodb://${local.router_defaults.mongodb_hosts}/draft_router",
+    },
+  )
+  secrets_from_arns  = local.router_defaults.secrets_from_arns
+  log_group          = local.log_group
+  aws_region         = data.aws_region.current.name
+  cpu                = local.router_defaults.cpu
+  memory             = local.router_defaults.memory
+  task_role_arn      = aws_iam_role.task.arn
+  execution_role_arn = aws_iam_role.execution.arn
 }
