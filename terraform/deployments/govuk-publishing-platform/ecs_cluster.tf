2fcb6b22ae2494353b7c85487443daa32de1fb94
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2021-03-04 18:48:17 +0000

    Make ECS cluster workspace aware

--- terraform/deployments/govuk-publishing-platform/ecs_cluster.tf
+++ terraform/deployments/govuk-publishing-platform/ecs_cluster.tf
@@ -7,7 +7,7 @@ locals {
 
 # All services running on GOV.UK run in this single cluster.
 resource "aws_ecs_cluster" "cluster" {
-  name               = "govuk"
+  name               = local.mesh_name
   capacity_providers = ["FARGATE", "FARGATE_SPOT"]
 
   default_capacity_provider_strategy {
