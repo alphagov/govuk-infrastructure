5090b79e266d25e1ca9f02011cd1e2c9a6981077
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2021-03-04 19:11:17 +0000

    Make security groups workspace aware

--- terraform/deployments/govuk-publishing-platform/security_groups.tf
+++ terraform/deployments/govuk-publishing-platform/security_groups.tf
@@ -1,13 +1,13 @@
 # Security groups for the GOV.UK Publishing microservices are defined here.
 
 resource "aws_security_group" "mesh_ecs_service" {
-  name        = "mesh_ecs_service"
+  name        = "mesh_ecs_service-${terraform.workspace}"
   vpc_id      = local.vpc_id
   description = "Associated with all ECS Services that are virtual services in the AppMesh mesh"
 }
 
 resource "aws_security_group" "smokey" {
-  name        = "ecs_fargate_smokey"
+  name        = "ecs_fargate_smokey-${terraform.workspace}"
   vpc_id      = local.vpc_id
   description = "Smoke test runner"
 }
