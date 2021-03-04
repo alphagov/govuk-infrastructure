1ca623b1ceeb6e2347200c16d6cda2a7a1f487f9
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2021-03-04 20:56:24 +0000

    Make redis workspace aware

--- terraform/deployments/govuk-publishing-platform/shared_redis_cluster.tf
+++ terraform/deployments/govuk-publishing-platform/shared_redis_cluster.tf
@@ -1,8 +1,13 @@
 # TODO - inline this module
 
+locals {
+  cluster_name      = "${terraform.workspace == "default" ? "shared" : "${terraform.workspace}"}"
+}
+
 module "shared_redis_cluster" {
   source              = "../../modules/redis"
   vpc_id              = local.vpc_id
+  cluster_name        = local.cluster_name
   internal_app_domain = var.internal_app_domain
   subnet_ids          = local.redis_subnets
 }
