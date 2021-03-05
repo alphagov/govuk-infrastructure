4f8fb5864aaebd77aa67695a44fe40cfc76b5147
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2021-03-05 11:08:41 +0000

    Make Statsd workspace aware

--- terraform/deployments/govuk-publishing-platform/app_statsd.tf
+++ terraform/deployments/govuk-publishing-platform/app_statsd.tf
@@ -2,7 +2,7 @@ module "statsd" {
   cluster_id                       = aws_ecs_cluster.cluster.id
   execution_role_arn               = aws_iam_role.execution.arn
   internal_app_domain              = var.internal_app_domain
-  mesh_name                        = var.mesh_name
+  mesh_name                        = aws_appmesh_mesh.govuk.id
   private_subnets                  = local.private_subnets
   security_groups                  = [aws_security_group.mesh_ecs_service.id, local.govuk_management_access_sg_id]
   service_discovery_namespace_id   = aws_service_discovery_private_dns_namespace.govuk_publishing_platform.id
