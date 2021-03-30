db4cbd3c414d3dcf864ff4342c25827278cae8ad
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2021-03-30 13:29:51 +0100

    Make path to rails assets bucket workspace aware

--- concourse/tasks/upload-rails-assets.sh
+++ concourse/tasks/upload-rails-assets.sh
@@ -10,6 +10,12 @@ role_arn = $ASSUME_ROLE_ARN
 credential_source = Ec2InstanceMetadata
 EOF
 
+if [[ "$WORKSPACE" == "default" ]]; then
+  WORKSPACE=ecs;
+fi
+
+S3_BUCKET_PATH=$(printf "$S3_BUCKET_PATH_PATTERN" "$WORKSPACE")
+
 echo "Uploading rails assets from ${IMAGE_ASSETS_PATH} to ${S3_BUCKET_PATH} ..."
 
 aws s3 sync \
