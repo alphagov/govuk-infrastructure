230c6119df399f7621b47749b41a3e9b3160be4e
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2021-05-26 12:11:11 +0100

    parametize backend config for `task-runner` deployment
    This is needed as part of the deployment in integration.

--- terraform/deployments/task-runner/main.tf
+++ terraform/deployments/task-runner/main.tf
@@ -1,10 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket  = "govuk-terraform-test"
-    key     = "projects/rake-task.tfstate"
-    region  = "eu-west-1"
-    encrypt = true
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
