fa850ba33b183f48b38c4718cc14eb0ef07d8892
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2021-05-27 12:46:37 +0100

    Log Smokey output to AWS
    This commit updates the logging for Smokey to be sent to AWS, rather than Splunk. This is necessary so that developers can view the entire output of a particular Smokey job when debugging issues, as logs sent to Splunk are broken up by log line and means navigating multiple search results pages to see the output of a single job.

--- terraform/modules/container-definition/variables.tf
+++ terraform/modules/container-definition/variables.tf
@@ -38,16 +38,19 @@ variable "image" {
 variable "splunk_url_secret_arn" {
   type        = string
   description = "ARN to the secret containing the URL for the Splunk instance (of the form `https://http-inputs-XXXXXXXX.splunkcloud.com:PORT`)."
+  default     = null
 }
 
 variable "splunk_token_secret_arn" {
   type        = string
   description = "ARN to the secret containing the HTTP Event Collector (HEC) token."
+  default     = null
 }
 
 variable "splunk_index" {
   type        = string
   description = "Splunk index to log events to (which HEC token must have access to write to)."
+  default     = null
 }
 
 variable "splunk_sourcetype" {
@@ -79,4 +82,20 @@ DESC
 variable "user" {
   type    = string
   default = null
+}
+
+variable "log_group" {
+  type    = string
+  default = null
+}
+
+variable "log_stream_prefix" {
+  type        = string
+  description = "Set log_stream_prefix to an ECS Service name, if applicable. A prefix makes it easier to associate a log with a service."
+  default     = null
+}
+
+variable "log_to_splunk" {
+  type    = bool
+  default = true
 }
