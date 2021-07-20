e791d490c6611f1c76100b730f87589cc2635ba4
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2021-07-20 14:36:15 +0100

    Add ECR repository for infra-concourse-task

--- terraform/deployments/ecr/main.tf
+++ terraform/deployments/ecr/main.tf
@@ -24,6 +24,7 @@ locals {
   repositories = [
     "content-store",
     "frontend",
+    "infra-concourse-task",
     "publisher",
     "publishing-api",
     "router",
@@ -100,6 +101,9 @@ resource "aws_iam_role_policy_attachment" "push_to_ecr_role_attachment" {
   policy_arn = aws_iam_policy.push_image_to_ecr_policy.arn
 }
 
+# TODO: aws_ecr_repository must be created first, but this dependency is hidden
+# from Terraform. Using aws_ecr_repository.repositories rather than
+# local.repositories may resolve this.
 resource "aws_ecr_repository_policy" "pull_images_from_ecr_policy_policy" {
   for_each   = toset(local.repositories)
   repository = each.key
