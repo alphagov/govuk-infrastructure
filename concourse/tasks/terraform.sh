3f67be8e3945c84270bdca2847bd65f52d642b7a
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2021-08-13 18:12:24 +0100

    Generalise the shell script for running Terraform.
    It's likely that in the near future we'll have a job which needs to run
    `terraform plan` from Concourse, so generalise this now to stave off the
    copypasta.
    
    Also add a file comment describing the script, make it pass any
    additional args to Terraform and add usage instructions.

--- concourse/tasks/terraform.sh
+++ concourse/tasks/terraform.sh
@@ -1,26 +1,47 @@
 #!/bin/sh
+#
+# terraform.sh runs Terraform plan or apply from the directory
+# $DEPLOYMENT_PATH, acting as the IAM role specified in $ASSUME_ROLE_ARN.
 
 set -eu
+
+usage() {
+    echo "usage: $0 plan|apply|destroy [additional_terraform_args ...]" >&2
+    exit 64  # EX_USAGE
+}
+
+auto_approve=""
+tf_action="${1:-}" && shift
+case $tf_action in
+    plan )
+        ;;
+    apply | destroy )
+        auto_approve="-auto-approve"  # Necessary even with -input=false.
+        ;;
+    * )
+        usage
+        ;;
+esac
 
 export TF_IN_AUTOMATION=1
 
 # Assume role once here, rather than configuring the same thing (via different
 # options) in each Terraform provider in every root module.
 #
-# TODO: If this ends up being used elsewhere, factor it out into a script and
-# build it into the image.
+# TODO: Consider factoring this out into a script and including it in the image.
 creds="$(aws sts assume-role \
   --role-session-name "concourse-$(date +%d-%m-%y_%H-%M-%S)" \
   --role-arn "${ASSUME_ROLE_ARN}" \
   | jq .Credentials)"
-export AWS_ACCESS_KEY_ID="$(echo $creds | jq -r .AccessKeyId)"
-export AWS_SECRET_ACCESS_KEY="$(echo $creds | jq -r .SecretAccessKey)"
-export AWS_SESSION_TOKEN="$(echo $creds | jq -r .SessionToken)"
+AWS_ACCESS_KEY_ID="$(echo "$creds" | jq -r .AccessKeyId)"
+AWS_SECRET_ACCESS_KEY="$(echo "$creds" | jq -r .SecretAccessKey)"
+AWS_SESSION_TOKEN="$(echo "$creds" | jq -r .SessionToken)"
+export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
 
 cd "${DEPLOYMENT_PATH}"
 
 terraform init -input=false -backend-config "${ENVIRONMENT}.backend"
 
-terraform apply -input=false \
+terraform "$tf_action" -input=false \
   -var-file "../variables/${ENVIRONMENT}/common.tfvars" \
-  -auto-approve
+  $auto_approve "$@"
