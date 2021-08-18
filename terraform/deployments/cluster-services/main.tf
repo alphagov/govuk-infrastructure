c34b69fab1d95d7652a221111d0d0f4edf14e908
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2021-08-18 16:27:23 +0100

    Move cluster-services/k8s_auth.tf contents into its main.tf.
    k8s_auth.tf was really about defining a provider, so move it into
    main.tf.

--- terraform/deployments/cluster-services/main.tf
+++ terraform/deployments/cluster-services/main.tf
@@ -9,3 +9,17 @@
 terraform {
   backend "s3" {}
 }
+
+data "aws_eks_cluster_auth" "auth" {
+  name = data.terraform_remote_state.cluster_infrastructure.outputs.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.cluster_infrastructure.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster_infrastructure.outputs.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cluster_infrastructure.outputs.cluster_id]
+  }
+}
