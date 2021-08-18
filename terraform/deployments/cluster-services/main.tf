29427fcfd484e82c70a06b8d93fad9e81385acd9
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2021-08-18 16:41:45 +0100

    Add the Helm provider to the cluster-services module.

--- terraform/deployments/cluster-services/main.tf
+++ terraform/deployments/cluster-services/main.tf
@@ -14,8 +14,25 @@ provider "kubernetes" {
   host                   = data.terraform_remote_state.cluster_infrastructure.outputs.cluster_endpoint
   cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster_infrastructure.outputs.cluster_certificate_authority_data)
   exec {
+    # TODO: use `client.authentication.k8s.io/v1beta1` once aws eks get-token
+    # supports it (again). As of aws-cli 2.2.30 it still only supports
+    # v1alpha1. https://github.com/aws/aws-cli/pull/6309
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cluster_infrastructure.outputs.cluster_id]
+  }
+}
+
+provider "helm" {
+  # TODO: If/when TF makes provider configs a first-class language object,
+  # reuse the identical config from above.
+  kubernetes {
+    host                   = data.terraform_remote_state.cluster_infrastructure.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster_infrastructure.outputs.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cluster_infrastructure.outputs.cluster_id]
+    }
   }
 }
