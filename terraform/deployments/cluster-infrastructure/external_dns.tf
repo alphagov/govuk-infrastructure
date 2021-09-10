b81e810b007e6728bd1b186a5cd2c682a9bb9695
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2021-09-10 17:01:43 +0100

    Create a wildcard TLS cert for the external-dns domain.
    We include `*.$env.publishing.service.gov.uk` as a SAN, since this will
    ultimately be the domain which faces upstream to the user or the CDN.
    (Those domains will be CNAMEs to `*.$env.govuk.digital` and that's where
    traffic will be switched from the old rig to the new one.)
    
    Unfortunately the validation DNS records for publishing.service.gov.uk
    cannot be managed automatically from here, but they are in place already
    for all four existing accounts (test, integration, staging, production).
    To fix that, we'll need to move the user-facing non-prod domains like
    integration.publishing.service.gov.uk into separate zones and delegate
    them to the non-prod accounts.
    
    Also clean up some names for consistency and trim the trailing dot (root
    domain) from local.external_dns_zone_name.

--- terraform/deployments/cluster-infrastructure/external_dns.tf
+++ terraform/deployments/cluster-infrastructure/external_dns.tf
@@ -1,12 +1,14 @@
 # external_dns.tf defines a Route53 zone and an IAM role and policy to allow
-# the k8s external-dns addon to manage the zone.
+# the k8s external-dns addon to manage the zone, plus a wildcard TLS cert which
+# covers the external-dns domain and the user-facing
+# *.(env.)?publishing.service.gov.uk domain.
 #
 # The k8s side of the external-dns addon is in
 # ../cluster-services/external_dns.tf.
 
 locals {
   external_dns_service_account_name = "external-dns"
-  external_dns_domain_name          = "${var.external_dns_subdomain}.${data.terraform_remote_state.infra_root_dns_zones.outputs.external_root_domain_name}"
+  external_dns_zone_name            = trimsuffix("${var.external_dns_subdomain}.${data.terraform_remote_state.infra_root_dns_zones.outputs.external_root_domain_name}", ".")
 }
 
 module "external_dns_iam_role" {
@@ -44,14 +46,54 @@ data "aws_iam_policy_document" "external_dns" {
 }
 
 resource "aws_route53_zone" "cluster_public" {
-  name          = local.external_dns_domain_name
+  name          = local.external_dns_zone_name
   force_destroy = var.force_destroy
 }
 
-resource "aws_route53_record" "cluster_public_ns" {
+resource "aws_route53_record" "cluster_public_ns_parent" {
   zone_id = data.terraform_remote_state.infra_root_dns_zones.outputs.external_root_zone_id
   name    = var.external_dns_subdomain
   type    = "NS"
   ttl     = 21600
   records = aws_route53_zone.cluster_public.name_servers
+}
+
+resource "aws_acm_certificate" "cluster_public" {
+  domain_name               = "*.${local.external_dns_zone_name}"
+  subject_alternative_names = ["*.${var.publishing_service_domain}"]
+  validation_method         = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Name = local.external_dns_zone_name
+  }
+}
+
+# See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation#example-usage
+resource "aws_route53_record" "cluster_public_cert_validation" {
+  for_each = {
+    # TODO: Delegate the non-prod domains to the non-prod AWS accounts so that
+    # the validation records for *.(env.)?publishing.service.gov.uk can easily
+    # be added here as part of turnup/DR automation. Those records currently
+    # exist for the test/integration/staging/prod environments in a single zone
+    # managed in alphagov/govuk-dns-config.
+    for dvo in aws_acm_certificate.cluster_public.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+    if length(regexall("${local.external_dns_zone_name}\\.$", dvo.resource_record_name)) > 0
+    # i.e. if dvo.resource_record_name.endswith(local.external_dns_zone_name + ".")
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 300
+  type            = each.value.type
+  zone_id         = aws_route53_zone.cluster_public.id
+}
+
+resource "aws_acm_certificate_validation" "cluster_public" {
+  certificate_arn = aws_acm_certificate.cluster_public.arn
 }
