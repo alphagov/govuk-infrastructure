6e6fa6a5d220328c0115730da629f48ffae12c8b
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2021-10-04 13:40:22 +0100

    Update to latest terraform-aws-eks module.
    https://github.com/terraform-aws-modules/terraform-aws-eks/blob/master/CHANGELOG.md#v17200---2021-09-17

--- terraform/deployments/cluster-infrastructure/main.tf
+++ terraform/deployments/cluster-infrastructure/main.tf
@@ -44,7 +44,7 @@ provider "aws" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "17.8.0"
+  version = "17.20.0"
 
   cluster_name     = var.cluster_name
   cluster_version  = var.cluster_version
