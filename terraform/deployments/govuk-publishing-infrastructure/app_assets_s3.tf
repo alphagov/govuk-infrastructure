d2745029c78597010c21f0b2db1bffd2b1d45f25
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-02-25 18:11:09 +0000

    Add Terraform for `app-assets-$env` S3 buckets.
    These buckets (one per AWS account / environment) are for storing and
    serving app assets such as CSS, JavaScript, logo images and so on.
    Asset files are uploaded when an app is deployed.
    
    See alphagov/govuk-helm-charts#178 for more background.
    
    Trello: https://trello.com/c/ZR6s2YhF/839

---
+++ terraform/deployments/govuk-publishing-infrastructure/app_assets_s3.tf
@@ -0,0 +1,52 @@
+resource "aws_s3_bucket" "app_assets" {
+  bucket = "govuk-app-assets-${var.govuk_environment}"
+  tags = {
+    Name        = "App static assets for ${var.govuk_environment}"
+    Environment = var.govuk_environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "app_assets" {
+  bucket = aws_s3_bucket.app_assets.id
+  versioning_configuration { status = "Suspended" }
+}
+
+resource "aws_s3_bucket_policy" "app_assets" {
+  bucket = aws_s3_bucket.app_assets.id
+  policy = data.aws_iam_policy_document.app_assets.json
+}
+
+# TODO: instead of granting write access to nodes, use IRSA (IAM Roles for
+# Service Accounts aka pod identity) so that only ArgoCD can write.
+data "aws_iam_policy_document" "app_assets" {
+  statement {
+    sid = "PublicCanReadButNotList"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.app_assets.arn}/*"]
+  }
+  statement {
+    sid = "EKSNodesCanList"
+    principals {
+      type        = "AWS"
+      identifiers = [data.terraform_remote_state.cluster_infrastructure.outputs.worker_iam_role_arn]
+    }
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.app_assets.arn]
+  }
+  statement {
+    sid = "EKSNodesCanWrite"
+    principals {
+      type        = "AWS"
+      identifiers = [data.terraform_remote_state.cluster_infrastructure.outputs.worker_iam_role_arn]
+    }
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    resources = ["${aws_s3_bucket.app_assets.arn}/*"]
+  }
+}
