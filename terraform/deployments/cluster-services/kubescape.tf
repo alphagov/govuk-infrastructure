2267abe9fbefbcda723dea83d85cbf3c1e6eb704
Luke Wiltshire <luke@lukewiltshire.co.uk>
2022-03-30 12:42:32 +0100

    Add kubescape scanner via Helm

---
+++ terraform/deployments/cluster-services/kubescape.tf
@@ -0,0 +1,24 @@
+data "aws_secretsmanager_secret" "kubescape-account" {
+  name = "govuk/kubescape"
+}
+
+data "aws_secretsmanager_secret_version" "kubescape-account-guid" {
+  secret_id = data.aws_secretsmanager_secret.kubescape-account.id
+}
+
+resource "helm_release" "kubescape" {
+  chart      = "armo-cluster-components"
+  name       = "armo"
+  namespace  = "armo-system"
+  repository = "https://armosec.github.io/armo-helm/"
+  version    = "1.7.0"
+  set {
+    name  = "clusterName"
+    value = "govuk-${var.govuk_environment}"
+  }
+  set {
+    name  = "accountGuid"
+    value = jsondecode(data.aws_secretsmanager_secret_version.kubescape-account-guid.secret_string)["accountGuid"]
+  }
+  wait_for_jobs = false
+}
