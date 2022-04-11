942b63130e6f5ebf10cab4a2ac511d4e86841850
Sean Rankine <sean.rankine@me.com>
2022-04-11 16:31:00 +0100

    Add Argo Event webhook secrets for the GitHub org
    This adds GitHub Organisation secrets for the webhook token and URL to
    access Argo Events.

--- terraform/deployments/github/main.tf
+++ terraform/deployments/github/main.tf
@@ -44,3 +44,15 @@ resource "github_actions_organization_secret_repositories" "ci_user_github_api_t
   secret_name             = "GOVUK_CI_GITHUB_API_TOKEN"
   selected_repository_ids = [for repo in data.github_repository.govuk : repo.repo_id]
 }
+
+resource "github_actions_organization_secret" "argo_events_webhook_token" {
+  secret_name             = "ARGO_EVENTS_WEBHOOK_TOKEN"
+  visibility              = "selected"
+  selected_repository_ids = [for repo in data.github_repository.govuk : repo.repo_id]
+}
+
+resource "github_actions_organization_secret" "argo_events_webhook_url" {
+  secret_name             = "ARGO_EVENTS_WEBHOOK_URL"
+  visibility              = "selected"
+  selected_repository_ids = [for repo in data.github_repository.govuk : repo.repo_id]
+}
