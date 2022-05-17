4e6909488ff61bb61ec135dcdd2d6e79d20501aa
Seun Adiomowo <Seun Adiomowo>
2022-05-17 15:05:52 +0100

    Adding the ECR-SCAN for docker images

--- terraform/deployments/ecr/variables.tf
+++ terraform/deployments/ecr/variables.tf
@@ -2,3 +2,10 @@ variable "puller_arns" {
   type        = list(string)
   description = "List of IAM principals who should be authorised to pull from this registry."
 }
+
+variable "emails" {
+  type    = list(string)
+  default = [] # TODO: Set a default email address before applying.
+
+}
+
