1ebfdf3a9077b37bb2470bfdeb5a0dcc3f4fb58c
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-08-23 13:25:19 +0100

    Enable multi-AZ for the redis cluster.
    We have this enabled on the EC2 equivalent; must have been missed when
    setting our one up.

--- terraform/deployments/govuk-publishing-infrastructure/shared_redis.tf
+++ terraform/deployments/govuk-publishing-infrastructure/shared_redis.tf
@@ -23,6 +23,7 @@ resource "aws_elasticache_replication_group" "shared_redis_cluster" {
   node_type                  = var.shared_redis_cluster_node_type
   num_cache_clusters         = 2
   automatic_failover_enabled = true
+  multi_az_enabled           = true
   parameter_group_name       = "default.redis6.x"
   engine_version             = "6.x"
   subnet_group_name          = aws_elasticache_subnet_group.shared_redis_cluster.name
