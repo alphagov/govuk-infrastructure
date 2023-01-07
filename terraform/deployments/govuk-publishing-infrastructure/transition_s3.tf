5aa9b85d7720c7f1baa10e147c91aa5fb3d376fd
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-01-07 22:24:13 +0000

    Grant access to processed Transition logs.
    The transition app has a periodic job that reads these from S3.

---
+++ terraform/deployments/govuk-publishing-infrastructure/transition_s3.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_policy" "transition_s3" {
+  name        = "transition_s3"
+  description = "Read the processed CDN request logs for transitioned site redirects."
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetBucketLocation",
+          "s3:ListBucket",
+          "s3:GetObject",
+        ]
+        Resource = "arn:aws:s3:::govuk-${var.govuk_environment}-transition-fastly-logs*"
+      }
+    ]
+  })
+}
+
+# TODO: consider IRSA (pod identity) rather than granting to nodes.
+resource "aws_iam_role_policy_attachment" "transition_s3" {
+  role       = data.terraform_remote_state.cluster_infrastructure.outputs.worker_iam_role_name
+  policy_arn = aws_iam_policy.transition_s3.arn
+}
