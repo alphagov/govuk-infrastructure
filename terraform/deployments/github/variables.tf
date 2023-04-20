427053e657bf8481a1f0211e3885c410513f5d6c
Sean Rankine <sean.rankine@me.com>
2023-04-20 14:59:48 +0100

    Use GitHub App credentials for GitHub Provider
    This allows terraform to use a GitHub App creds to access GitHub instead
    of a Personal Token, which needs to be assigned to a specific user.

---
+++ terraform/deployments/github/variables.tf
@@ -0,0 +1,14 @@
+variable "github_app_id" {
+  type        = string
+  description = "The id of the GitHub App used for authentication."
+}
+
+variable "github_app_installation_id" {
+  type        = string
+  description = "The id of the installation of the GitHub App used for authentication."
+}
+
+variable "github_app_pem_file" {
+  type        = string
+  description = "The private key to sign access token requests."
+}
