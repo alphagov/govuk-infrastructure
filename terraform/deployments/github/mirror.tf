836390ed2deb1d3d8a5d93638898fbdd99ec0dbe
Sean Rankine <sean.rankine@me.com>
2023-04-24 14:45:29 +0100

    Add OIDC provider for mirror repo GHA
    This adds an OIDC provider, IAM role and policy for the mirror repos
    GitHub Action to use. This is to provide the workflow with access to
    push git repos to CodeCommit.

--- terraform/deployments/github/mirror.tf
+++ terraform/deployments/github/mirror.tf
@@ -1,6 +1,52 @@
-resource "aws_codecommit_repository" "govuk" {
+resource "aws_codecommit_repository" "govuk_repos" {
   for_each = data.github_repository.govuk
 
   repository_name = each.value.name
   default_branch  = "main"
+}
+
+resource "aws_iam_role" "github_action_mirror_repos_role" {
+  name = "github_action_mirror_repos_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "${aws_iam_openid_connect_provider.github_provider.arn}"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:alphagov/govuk-infrastructure:ref:refs/heads/main"
+            ]
+          },
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "${one(aws_iam_openid_connect_provider.github_provider.client_id_list)}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_action_mirror_repos_policy" {
+  name = "github_action_mirror_repos_policy"
+  role = aws_iam_role.github_action_mirror_repos_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "codecommit:GitPull",
+          "codecommit:GitPush"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
