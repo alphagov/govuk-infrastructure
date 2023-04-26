e3983cbb97bafd812a69b9b20bf21fe23ea2a1c1
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-04-26 15:22:42 +0100

    Add locations_api_import_csvs S3 bucket for ONSPD import prototype

---
+++ terraform/deployments/govuk-publishing-infrastructure/locations_api_s3.tf
@@ -0,0 +1,44 @@
+resource "aws_s3_bucket" "location_api_import_csvs" {
+  bucket        = "govuk-${var.govuk_environment}-locations-api-import-csvs"
+  force_destroy = var.force_destroy
+  tags = {
+    Name        = "CSVs used for importing postcode information into Locations API in ${var.govuk_environment}"
+    Environment = var.govuk_environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "location_api_import_csvs" {
+  bucket = aws_s3_bucket.location_api_import_csvs.id
+  versioning_configuration { status = "Suspended" }
+}
+
+resource "aws_s3_bucket_policy" "location_api_import_csvs" {
+  bucket = aws_s3_bucket.location_api_import_csvs.id
+  policy = data.aws_iam_policy_document.location_api_import_csvs.json
+}
+
+# TODO: instead of granting write access to nodes, use IRSA (IAM Roles for
+# Service Accounts aka pod identity) so that only locations-api can write.
+data "aws_iam_policy_document" "location_api_import_csvs" {
+  statement {
+    sid = "EKSNodesCanList"
+    principals {
+      type        = "AWS"
+      identifiers = [data.terraform_remote_state.cluster_infrastructure.outputs.worker_iam_role_arn]
+    }
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.location_api_import_csvs.arn]
+  }
+  statement {
+    sid = "EKSNodesCanWrite"
+    principals {
+      type        = "AWS"
+      identifiers = [data.terraform_remote_state.cluster_infrastructure.outputs.worker_iam_role_arn]
+    }
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    resources = ["${aws_s3_bucket.location_api_import_csvs.arn}/*"]
+  }
+}
