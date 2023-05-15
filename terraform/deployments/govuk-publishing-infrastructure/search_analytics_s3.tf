e94263106881be4f2a1a77d3601f3e594fc7b3a9
Sean Rankine <sean.rankine@me.com>
2023-05-15 13:28:24 +0100

    Add AWS resources search-analytics-page-load GitHub Action
    This creates a S3 bucket and IAM Role for access to the bucket that is
    used by the Export analytics GitHub Actions workflow in the
    search-analytics repo.

---
+++ terraform/deployments/govuk-publishing-infrastructure/search_analytics_s3.tf
@@ -0,0 +1,82 @@
+resource "aws_s3_bucket" "search_analytics" {
+  bucket        = "govuk-search-analytics-${var.govuk_environment}"
+  force_destroy = var.force_destroy
+  tags = {
+    Name        = "Search analytics reports for ${var.govuk_environment}"
+    Environment = var.govuk_environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "search_analytics" {
+  bucket = aws_s3_bucket.search_analytics.id
+  versioning_configuration { status = "Enabled" }
+}
+
+resource "aws_s3_bucket_policy" "search_analytics" {
+  bucket = aws_s3_bucket.search_analytics.id
+  policy = data.aws_iam_policy_document.search_analytics.json
+}
+
+resource "aws_iam_role" "search_analytics_github_action_role" {
+  name = "search_analytics_github_action_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "${aws_iam_openid_connect_provider.github_provider.arn}"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:alphagov/search-analytics:ref:refs/heads/main"
+            ],
+            "token.actions.githubusercontent.com:aud" : "${one(aws_iam_openid_connect_provider.github_provider.client_id_list)}"
+          },
+        }
+      }
+    ]
+  })
+}
+
+# TODO: instead of granting write access to nodes, use IRSA (IAM Roles for
+# Service Accounts aka pod identity).
+data "aws_iam_policy_document" "search_analytics" {
+  statement {
+    sid = "EKSNodesCanList"
+    principals {
+      type        = "AWS"
+      identifiers = [data.terraform_remote_state.cluster_infrastructure.outputs.worker_iam_role_arn]
+    }
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.search_analytics.arn]
+  }
+  statement {
+    sid = "EKSNodesCanReadAndWrite"
+    principals {
+      type        = "AWS"
+      identifiers = [data.terraform_remote_state.cluster_infrastructure.outputs.worker_iam_role_arn]
+    }
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    resources = ["${aws_s3_bucket.search_analytics.arn}/*"]
+  }
+  statement {
+    sid = "GitHubCanWrite"
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.search_analytics_github_action_role.arn]
+    }
+    actions = [
+      "s3:PutObject",
+    ]
+    resources = ["${aws_s3_bucket.search_analytics.arn}/*"]
+  }
+}
+
+
