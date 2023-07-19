4bda47653d1d0aaaa81105b9e8744e2940118411
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-07-19 14:43:20 +0100

    Add in SG for DGU cluster to access backend redis

--- terraform/deployments/datagovuk-infrastructure/security.tf
+++ terraform/deployments/datagovuk-infrastructure/security.tf
@@ -7,3 +7,13 @@ resource "aws_security_group_rule" "postgres_from_eks_workers" {
   security_group_id        = data.terraform_remote_state.infra_security_groups.outputs.sg_ckan_id
   source_security_group_id = data.terraform_remote_state.cluster_infrastructure.outputs.node_security_group_id
 }
+
+resource "aws_security_group_rule" "redis_from_eks_workers" {
+  description              = "Redis accepts requests from EKS nodes"
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.infra_security_groups.outputs.sg_backend-redis_id
+  source_security_group_id = data.terraform_remote_state.cluster_infrastructure.outputs.node_security_group_id
+}
