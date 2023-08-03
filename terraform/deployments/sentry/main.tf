3169b3316b8952aaa66fd6532d5f07074a9c4d75
Murilo Dal Ri <murilodalri@gmail.com>
2023-08-03 15:38:07 +0100

    Move Sentry project management into Terraform
    We want to remove the Rake task in https://github.com/alphagov/govuk-saas-config/blob/main/sentry/Rakefile
    and manage it all in Terraform. We've already moved team management to
    https://github.com/alphagov/govuk-user-reviewer/blob/main/terraform/saas/sentry.tf

---
+++ terraform/deployments/sentry/main.tf
@@ -0,0 +1,11 @@
+resource "sentry_project" "govuk" {
+  for_each = local.sentry_projects
+
+  organization = "govuk"
+
+  teams = concat(local.common_teams, each.value)
+  name  = each.key
+  slug  = "app-${each.key}"
+
+  platform = "ruby"
+}
