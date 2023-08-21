f88d4e47adbe259bed7ed2590e6aa742b2243ab7
Murilo Dal Ri <murilodalri@gmail.com>
2023-08-21 14:06:02 +0100

    Manage team access to gov.uk repos using Terraform
    This is currently a manual process and is frequently not done.

---
+++ terraform/deployments/github/import_govuk_production_admin.tf
@@ -0,0 +1,553 @@
+import {
+  to = github_team.govuk_production_admin
+  id = "gov-uk-production-admin"
+}
+
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-docker"]
+  id = "3279244:govuk-docker"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["transition-config"]
+  id = "3279244:transition-config"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["miller-columns-element"]
+  id = "3279244:miller-columns-element"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["static"]
+  id = "3279244:static"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-user-journey-analysis-tools"]
+  id = "3279244:govuk-user-journey-analysis-tools"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_seed_crawler"]
+  id = "3279244:govuk_seed_crawler"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-replatform-test-app"]
+  id = "3279244:govuk-replatform-test-app"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-dependency-checker"]
+  id = "3279244:govuk-dependency-checker"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["plek"]
+  id = "3279244:plek"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["bouncer"]
+  id = "3279244:bouncer"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_publishing_components"]
+  id = "3279244:govuk_publishing_components"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["fonts"]
+  id = "3279244:fonts"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-account-architecture"]
+  id = "3279244:govuk-account-architecture"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["content-store-proxy"]
+  id = "3279244:content-store-proxy"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["datagovuk_find"]
+  id = "3279244:datagovuk_find"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["authenticating-proxy"]
+  id = "3279244:authenticating-proxy"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["datagovuk-tech-docs"]
+  id = "3279244:datagovuk-tech-docs"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["router"]
+  id = "3279244:router"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-ruby-images"]
+  id = "3279244:govuk-ruby-images"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-developer-docs"]
+  id = "3279244:govuk-developer-docs"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["public-asset-checker"]
+  id = "3279244:public-asset-checker"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-ask-export"]
+  id = "3279244:govuk-ask-export"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["collections"]
+  id = "3279244:collections"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["accessible-autocomplete-multiselect"]
+  id = "3279244:accessible-autocomplete-multiselect"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["optic14n"]
+  id = "3279244:optic14n"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["datagovuk_publish"]
+  id = "3279244:datagovuk_publish"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["search-admin"]
+  id = "3279244:search-admin"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["account-api"]
+  id = "3279244:account-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-aws-data"]
+  id = "3279244:govuk-aws-data"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_message_queue_consumer"]
+  id = "3279244:govuk_message_queue_consumer"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-browser-extension"]
+  id = "3279244:govuk-browser-extension"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["specialist-publisher"]
+  id = "3279244:specialist-publisher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["travel-advice-publisher"]
+  id = "3279244:travel-advice-publisher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-networkx-tutorial"]
+  id = "3279244:govuk-networkx-tutorial"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["short-url-manager"]
+  id = "3279244:short-url-manager"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["seal"]
+  id = "3279244:seal"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["pay-pagerduty"]
+  id = "3279244:pay-pagerduty"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["email-alert-api"]
+  id = "3279244:email-alert-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["transition"]
+  id = "3279244:transition"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["bulk-changer"]
+  id = "3279244:bulk-changer"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["content-publisher"]
+  id = "3279244:content-publisher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_personalisation"]
+  id = "3279244:govuk_personalisation"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["router-api"]
+  id = "3279244:router-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["slimmer"]
+  id = "3279244:slimmer"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["rubocop-govuk"]
+  id = "3279244:rubocop-govuk"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["special-route-publisher"]
+  id = "3279244:special-route-publisher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["government-frontend"]
+  id = "3279244:government-frontend"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["stylelint-config-gds"]
+  id = "3279244:stylelint-config-gds"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["puppet-aptly"]
+  id = "3279244:puppet-aptly"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["content-tagger"]
+  id = "3279244:content-tagger"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govspeak"]
+  id = "3279244:govspeak"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["info-frontend"]
+  id = "3279244:info-frontend"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["siteimprove_api_client"]
+  id = "3279244:siteimprove_api_client"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["search-api"]
+  id = "3279244:search-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["manuals-publisher"]
+  id = "3279244:manuals-publisher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_document_types"]
+  id = "3279244:govuk_document_types"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-user-feedback-processing"]
+  id = "3279244:govuk-user-feedback-processing"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-rfcs"]
+  id = "3279244:govuk-rfcs"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["contacts-admin"]
+  id = "3279244:contacts-admin"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["licensify"]
+  id = "3279244:licensify"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["smart-answers"]
+  id = "3279244:smart-answers"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-secrets"]
+  id = "3279244:govuk-secrets"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-google-analytics"]
+  id = "3279244:govuk-google-analytics"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-user-reviewer"]
+  id = "3279244:govuk-user-reviewer"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["rack-logstasher"]
+  id = "3279244:rack-logstasher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-rota-announcer"]
+  id = "3279244:govuk-rota-announcer"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-saas-config"]
+  id = "3279244:govuk-saas-config"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["asset-manager"]
+  id = "3279244:asset-manager"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["whitehall-prototype-2023"]
+  id = "3279244:whitehall-prototype-2023"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-load-testing"]
+  id = "3279244:govuk-load-testing"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["email-alert-service"]
+  id = "3279244:email-alert-service"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_sidekiq"]
+  id = "3279244:govuk_sidekiq"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-app-deployment"]
+  id = "3279244:govuk-app-deployment"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-jenkinslib"]
+  id = "3279244:govuk-jenkinslib"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["whitehall-asset-analysis"]
+  id = "3279244:whitehall-asset-analysis"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["content-data-admin"]
+  id = "3279244:content-data-admin"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_admin_template"]
+  id = "3279244:govuk_admin_template"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["imminence"]
+  id = "3279244:imminence"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-paas-office-ip-router"]
+  id = "3279244:govuk-paas-office-ip-router"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["ckan-functional-tests"]
+  id = "3279244:ckan-functional-tests"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-puppet"]
+  id = "3279244:govuk-puppet"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_ab_testing"]
+  id = "3279244:govuk_ab_testing"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-related-links-recommender"]
+  id = "3279244:govuk-related-links-recommender"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["gds-sso"]
+  id = "3279244:gds-sso"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["publisher"]
+  id = "3279244:publisher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["content-store"]
+  id = "3279244:content-store"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-ckan-charts"]
+  id = "3279244:govuk-ckan-charts"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-rota-generator"]
+  id = "3279244:govuk-rota-generator"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["icinga_slack_webhook"]
+  id = "3279244:icinga_slack_webhook"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_app_config"]
+  id = "3279244:govuk_app_config"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["collections-publisher"]
+  id = "3279244:collections-publisher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["signon"]
+  id = "3279244:signon"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-dns-tf"]
+  id = "3279244:govuk-dns-tf"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-fastly-secrets"]
+  id = "3279244:govuk-fastly-secrets"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["email-alert-monitoring"]
+  id = "3279244:email-alert-monitoring"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["locations-api"]
+  id = "3279244:locations-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["datagovuk_publish_elasticsearch_monitor"]
+  id = "3279244:datagovuk_publish_elasticsearch_monitor"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-infrastructure"]
+  id = "3279244:govuk-infrastructure"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["release"]
+  id = "3279244:release"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-spaCy"]
+  id = "3279244:govuk-spaCy"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["hmrc-manuals-api"]
+  id = "3279244:hmrc-manuals-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["finder-frontend"]
+  id = "3279244:finder-frontend"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["gds-api-adapters"]
+  id = "3279244:gds-api-adapters"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_schemas"]
+  id = "3279244:govuk_schemas"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["paste-html-to-govspeak"]
+  id = "3279244:paste-html-to-govspeak"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["bulk-merger"]
+  id = "3279244:bulk-merger"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["search-analytics"]
+  id = "3279244:search-analytics"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["frontend"]
+  id = "3279244:frontend"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["link-checker-api"]
+  id = "3279244:link-checker-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-fastly"]
+  id = "3279244:govuk-fastly"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["maslow"]
+  id = "3279244:maslow"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_test"]
+  id = "3279244:govuk_test"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["support"]
+  id = "3279244:support"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["smokey"]
+  id = "3279244:smokey"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-pact-broker"]
+  id = "3279244:govuk-pact-broker"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-display-screen"]
+  id = "3279244:govuk-display-screen"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["rails_translation_manager"]
+  id = "3279244:rails_translation_manager"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["ckanext-datagovuk"]
+  id = "3279244:ckanext-datagovuk"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["ckan-mock-harvest-sources"]
+  id = "3279244:ckan-mock-harvest-sources"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["content-data-api"]
+  id = "3279244:content-data-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["packager"]
+  id = "3279244:packager"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk_crawler_worker"]
+  id = "3279244:govuk_crawler_worker"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["whitehall"]
+  id = "3279244:whitehall"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["feedback"]
+  id = "3279244:feedback"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["email-alert-frontend"]
+  id = "3279244:email-alert-frontend"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["local-links-manager"]
+  id = "3279244:local-links-manager"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["service-manual-publisher"]
+  id = "3279244:service-manual-publisher"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-helm-charts"]
+  id = "3279244:govuk-helm-charts"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["datagovuk-visual-regression-tests"]
+  id = "3279244:datagovuk-visual-regression-tests"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-intent-detector"]
+  id = "3279244:govuk-intent-detector"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["tagging-suggester"]
+  id = "3279244:tagging-suggester"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["gds_zendesk"]
+  id = "3279244:gds_zendesk"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-connect"]
+  id = "3279244:govuk-connect"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govspeak-preview"]
+  id = "3279244:govspeak-preview"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["govuk-technical-2ndline-dashboard"]
+  id = "3279244:govuk-technical-2ndline-dashboard"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["publishing-api"]
+  id = "3279244:publishing-api"
+}
+import {
+  to = github_team_repository.govuk_production_admin_repos["support-api"]
+  id = "3279244:support-api"
+}
