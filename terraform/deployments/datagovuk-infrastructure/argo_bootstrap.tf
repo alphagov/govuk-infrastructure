f63c5000971a95568edaa1dd092254d3e6ec1ab4
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-11-08 16:57:02 +0000

    Update argo bootstrap target repo for datagovuk
    DGU PaaS apps will be migrated to the same repository to make it easier to manage the datagovuk stack so the govuk-ckan-charts repo is has been forked to govuk-dgu-charts to make the move without interrupting the service.

--- terraform/deployments/datagovuk-infrastructure/argo_bootstrap.tf
+++ terraform/deployments/datagovuk-infrastructure/argo_bootstrap.tf
@@ -3,7 +3,7 @@ resource "helm_release" "argo_bootstrap" {
   name             = "datagovuk-argo-bootstrap"
   namespace        = local.services_ns
   create_namespace = true
-  repository       = "https://alphagov.github.io/govuk-ckan-charts/"
+  repository       = "https://alphagov.github.io/govuk-dgu-charts/"
   version          = "1.0.4" # TODO: Dependabot or equivalent so this doesn't get neglected.
   values = [yamlencode({
     environment = var.govuk_environment
