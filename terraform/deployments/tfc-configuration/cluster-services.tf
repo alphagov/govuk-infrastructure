a3e87afe48bbedf8f3a287d06a768f228b0143d4
Marc Pomfret <marc.pomfret@digital.cabinet-office.gov.uk>
2023-11-30 15:10:07 +0000

    Cluster-services-integration workspace

---
+++ terraform/deployments/tfc-configuration/cluster-services.tf
@@ -0,0 +1,39 @@
+module "cluster-services-integration" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.9.0"
+
+  organization      = var.organization
+  workspace_name    = "cluster-services-integration"
+  workspace_desc    = "The cluster-services module is responsible for the AWS resources which constitute the EKS cluster."
+  workspace_tags    = ["integration", "cluster-services", "eks", "aws"]
+  terraform_version = "1.5.2"
+  execution_mode    = "remote"
+  working_directory = "/terraform/deployments/cluster-services/"
+  trigger_patterns  = ["/terraform/deployments/cluster-services/**/*"]
+
+  project_name = "govuk-infrastructure"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Non-Production" = "write",
+    "GOV.UK Production"     = "write"
+  }
+
+  tfvars = {
+    cluster_infrastructure_state_bucket = "govuk-terraform-integration"
+    govuk_environment                   = "integration"
+    github_read_write_team              = "alphagov:gov-uk"
+    argo_redis_ha                       = false
+    desired_ha_replicas                 = 1
+  }
+
+  # Variable Sets must already exist
+  variable_set_names = [
+    "aws-credentials-integration"
+  ]
+
+}
