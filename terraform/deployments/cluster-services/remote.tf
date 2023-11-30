61819c2fc3329400e9d98031d40607fe6fdc2111
Marc Pomfret <marc.pomfret@digital.cabinet-office.gov.uk>
2023-11-30 15:48:36 +0000

    Cluster-services remote state

--- terraform/deployments/cluster-services/remote.tf
+++ terraform/deployments/cluster-services/remote.tf
@@ -11,3 +11,21 @@ data "terraform_remote_state" "cluster_infrastructure" {
     region = data.aws_region.current.name
   }
 }
+
+# Not got as far as testing this yet but assuming it probably some thing like this
+# data "terraform_remote_state" "cluster_infrastructure" {
+#   backend = "remote"
+#   config = {
+#     organization = "govuk"
+#     workspaces = {
+#       tags = ["cluster-service", "eks", "aws"]
+#     }
+#   }
+# }
+
+
+# or this 
+# data "tfe_outputs" "cluster_infrastructure_integration" {
+#   organization = "govuk"
+#   workspace = "cluster-infrastructure-integration"
+# }
