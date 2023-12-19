807401fbcef45eebe5b23b1db0fb2eb44c8e55c8
David Mays <david.mays@digital.cabinet-office.gov.uk>
2023-12-19 11:40:54 +0000

    We could just slam EKS to the new Gateways

--- terraform/deployments/cluster-infrastructure/vpc.tf
+++ terraform/deployments/cluster-infrastructure/vpc.tf
@@ -49,7 +49,7 @@ resource "aws_route" "eks_control_plane_nat" {
   for_each               = var.eks_control_plane_subnets
   route_table_id         = aws_route_table.eks_control_plane[each.key].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.eks[each.key].id
+  nat_gateway_id         = aws_nat_gateway.eks_licensify[each.key].id
   timeouts {
     create = local.route_create_timeout
   }
@@ -149,6 +149,6 @@ resource "aws_route" "eks_private_nat" {
   for_each               = var.eks_private_subnets
   route_table_id         = aws_route_table.eks_private[each.key].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.eks[each.key].id
+  nat_gateway_id         = aws_nat_gateway.eks_licensify[each.key].id
   timeouts { create = local.route_create_timeout }
 }
