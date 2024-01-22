120498047ac571f581eb3896d55aa4be341d908c
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-01-22 14:29:33 +0000

    Add cluster-services integration TFC workspace

---
+++ terraform/deployments/tfc-configuration/cluster-services.tf
@@ -0,0 +1,31 @@
+module "cluster-services-integration" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.9.0"
+
+  organization      = var.organization
+  workspace_name    = "cluster-services-integration"
+  workspace_desc    = "The cluster-services module is responsible for the AWS resources which constitute the EKS cluster."
+  workspace_tags    = ["integration", "cluster-services", "eks", "aws"]
+  terraform_version = "1.5.2"
+  execution_mode    = "remote"
+  working_directory = "/terraform/deployments/cluster-services/"
+  trigger_patterns  = ["/terraform/deployments/cluster-services/**/*"]
+
+  project_name = "govuk-infrastructure"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Non-Production" = "write"
+    "GOV.UK Production"     = "write"
+  }
+
+  variable_set_names = [
+    "aws-credentials-integration",
+    "common",
+    "integration-common"
+  ]
+}
--- terraform/deployments/tfc-configuration/variable-set/variables.tf
+++ terraform/deployments/tfc-configuration/variable-set/variables.tf
@@ -1,15 +1,15 @@
 variable "tfvars" {
-  type = any
+  type        = any
   description = "Variables to add to the variable set"
 }
 
 variable "name" {
-  type = string
+  type        = string
   description = "Name of the created variable set"
 }
 
 variable "priority" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
   description = "Should this variable set override others?"
 }
