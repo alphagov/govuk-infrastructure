249ff9d4cd65ac3d8d6fad7709f94a4cafef375e
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-01-23 11:46:18 +0000

    Use variable sets for cluster infrastructure

--- terraform/deployments/tfc-configuration/cluster-infrastructure.tf
+++ terraform/deployments/tfc-configuration/cluster-infrastructure.tf
@@ -24,39 +24,11 @@ module "cluster-infrastructure-integration" {
     "GOV.UK Production"     = "write"
   }
 
-  tfvars = {
-    govuk_aws_state_bucket        = "govuk-terraform-steppingstone-integration"
-    govuk_environment             = "integration"
-    cluster_version               = 1.28
-    cluster_log_retention_in_days = 7
-    eks_control_plane_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.1.19.0/28" }
-      b = { az = "eu-west-1b", cidr = "10.1.19.16/28" }
-      c = { az = "eu-west-1c", cidr = "10.1.19.32/28" }
-    }
-    # Intentionally empty in Integration
-    eks_licensify_gateways = {}
-    eks_public_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.1.20.0/24" }
-      b = { az = "eu-west-1b", cidr = "10.1.21.0/24" }
-      c = { az = "eu-west-1c", cidr = "10.1.22.0/24" }
-    }
-    eks_private_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.1.24.0/22" }
-      b = { az = "eu-west-1b", cidr = "10.1.28.0/22" }
-      c = { az = "eu-west-1c", cidr = "10.1.32.0/22" }
-    }
-    force_destroy                   = true
-    publishing_service_domain       = "integration.publishing.service.gov.uk"
-    grafana_db_auto_pause           = true
-    rds_apply_immediately           = true
-    rds_backup_retention_period     = 1
-    rds_skip_final_snapshot         = true
-    secrets_recovery_window_in_days = 0
-  }
-  # Variable Sets must already exist
-  variable_set_names = ["aws-credentials-integration"]
-
+  variable_set_names = [
+    "aws-credentials-integration",
+    "common",
+    "common-integration"
+  ]
 }
 module "cluster-infrastructure-staging" {
   source  = "alexbasista/workspacer/tfe"
@@ -85,38 +57,13 @@ module "cluster-infrastructure-staging" {
     "GOV.UK Production"     = "write"
   }
 
-  tfvars = {
-    govuk_aws_state_bucket        = "govuk-terraform-steppingstone-staging"
-    govuk_environment             = "staging"
-    cluster_version               = 1.28
-    cluster_log_retention_in_days = 7
-    eks_control_plane_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.12.19.0/28" }
-      b = { az = "eu-west-1b", cidr = "10.12.19.16/28" }
-      c = { az = "eu-west-1c", cidr = "10.12.19.32/28" }
-    }
-    eks_licensify_gateways = {
-      a = { az = "eu-west-1a", cidr = "10.12.20.0/24", eip = "eipalloc-0c15477b55906cc2c" }
-      b = { az = "eu-west-1b", cidr = "10.12.21.0/24", eip = "eipalloc-0a08b03e7bb1202df" }
-      c = { az = "eu-west-1c", cidr = "10.12.22.0/24", eip = "eipalloc-0eb4541bf8dc33dc6" }
-    }
-    eks_public_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.12.20.0/24" }
-      b = { az = "eu-west-1b", cidr = "10.12.21.0/24" }
-      c = { az = "eu-west-1c", cidr = "10.12.22.0/24" }
-    }
-    eks_private_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.12.24.0/22" }
-      b = { az = "eu-west-1b", cidr = "10.12.28.0/22" }
-      c = { az = "eu-west-1c", cidr = "10.12.32.0/22" }
-    }
-    publishing_service_domain   = "staging.publishing.service.gov.uk"
-    rds_backup_retention_period = 1
-  }
-  # Variable Sets must already exist
-  variable_set_names = ["aws-credentials-staging"]
-
+  variable_set_names = [
+    "aws-credentials-staging",
+    "common",
+    "common-staging"
+  ]
 }
+
 module "cluster-infrastructure-production" {
   source  = "alexbasista/workspacer/tfe"
   version = "0.9.0"
@@ -140,31 +87,9 @@ module "cluster-infrastructure-production" {
   }
   team_access = { "GOV.UK Production" = "write" }
 
-  tfvars = {
-    govuk_aws_state_bucket        = "govuk-terraform-steppingstone-production"
-    govuk_environment             = "production"
-    cluster_version               = 1.28
-    cluster_log_retention_in_days = 7
-    eks_control_plane_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.13.19.0/28" }
-      b = { az = "eu-west-1b", cidr = "10.13.19.16/28" }
-      c = { az = "eu-west-1c", cidr = "10.13.19.32/28" }
-    }
-    # TODO: Set up in Production when Ready.
-    eks_licensify_gateways = {}
-    eks_public_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.13.20.0/24" }
-      b = { az = "eu-west-1b", cidr = "10.13.21.0/24" }
-      c = { az = "eu-west-1c", cidr = "10.13.22.0/24" }
-    }
-    eks_private_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.13.24.0/22" }
-      b = { az = "eu-west-1b", cidr = "10.13.28.0/22" }
-      c = { az = "eu-west-1c", cidr = "10.13.32.0/22" }
-    }
-    publishing_service_domain = "publishing.service.gov.uk"
-    workers_instance_types    = ["m6i.8xlarge", "m6a.8xlarge"]
-  }
-  # Variable Sets must already exist
-  variable_set_names = ["aws-credentials-production"]
+  variable_set_names = [
+    "aws-credentials-production",
+    "common",
+    "common-production"
+  ]
 }
