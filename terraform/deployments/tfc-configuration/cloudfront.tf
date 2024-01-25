a5303c3d1952905f8e170c9644520e1a7d2bc952
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-01-25 12:37:35 +0000

    Add cloudfront production workspace

--- terraform/deployments/tfc-configuration/cloudfront.tf
+++ terraform/deployments/tfc-configuration/cloudfront.tf
@@ -20,7 +20,7 @@ module "cloudfront-staging" {
   }
 
   team_access = {
-    "GOV.UK Production"     = "write"
+    "GOV.UK Production" = "write"
   }
 
   variable_set_names = [
@@ -28,5 +28,38 @@ module "cloudfront-staging" {
     "common",
     "common-staging",
     "cloudfront-staging"
+  ]
+}
+
+module "cloudfront-production" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.9.0"
+
+  organization        = var.organization
+  workspace_name      = "cloudfront-production"
+  workspace_desc      = "The cloudfront module is responsible for the AWS resources which constitute the EKS cluster."
+  workspace_tags      = ["production", "cloudfront", "eks", "aws"]
+  terraform_version   = "1.7.0"
+  execution_mode      = "remote"
+  working_directory   = "/terraform/deployments/cloudfront/"
+  trigger_patterns    = ["/terraform/deployments/cloudfront/**/*"]
+  global_remote_state = true
+
+  project_name = "govuk-infrastructure"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "samsimpson1/tfc"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Production" = "write"
+  }
+
+  variable_set_names = [
+    "aws-credentials-production",
+    "common",
+    "common-production",
+    "cloudfront-production"
   ]
 }
