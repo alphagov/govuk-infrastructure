d5256a9b65ef9a461425034f4e48935d98e856b5
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-02-15 13:25:23 +0000

    Add integration RDS workspace

---
+++ terraform/deployments/tfc-configuration/rds.tf
@@ -0,0 +1,32 @@
+module "rds-integration" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.9.0"
+
+  organization      = var.organization
+  workspace_name    = "rds-integration"
+  workspace_desc    = "This module manages AWS resources for creating RDS databases."
+  workspace_tags    = ["integration", "rds", "eks", "aws"]
+  terraform_version = "1.7.0"
+  execution_mode    = "remote"
+  working_directory = "/terraform/deployments/rds/"
+  trigger_patterns  = ["/terraform/deployments/rds/**/*"]
+
+  project_name = "govuk-infrastructure"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "samsimpson1/rds"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Non-Production" = "write"
+    "GOV.UK Production"     = "write"
+  }
+
+  variable_set_names = [
+    "aws-credentials-integration",
+    "common",
+    "common-integration",
+    "rds-integration"
+  ]
+}
