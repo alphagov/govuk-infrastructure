5dd19ee381e6b9da2fc0a7aac4ca31b83fc64cb5
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-03-14 16:58:52 +0000

    Fixes for datagovuk-infrastructure to work on TFC

--- terraform/deployments/datagovuk-infrastructure/remote.tf
+++ terraform/deployments/datagovuk-infrastructure/remote.tf
@@ -1,23 +1,8 @@
 data "aws_region" "current" {}
 
-data "terraform_remote_state" "cluster_infrastructure" {
-  backend   = "s3"
-  workspace = terraform.workspace
-  config = {
-    bucket = var.cluster_infrastructure_state_bucket
-    key    = "projects/cluster-infrastructure.tfstate"
-    region = data.aws_region.current.name
-  }
-}
-
-data "terraform_remote_state" "app_govuk_rds" {
-  backend = "s3"
-
-  config = {
-    bucket = var.govuk_aws_state_bucket
-    key    = "blue/app-govuk-rds.tfstate"
-    region = data.aws_region.current.name
-  }
+data "tfe_outputs" "cluster_infrastructure" {
+  organization = "govuk"
+  workspace    = "cluster-infrastructure-${var.govuk_environment}"
 }
 
 data "terraform_remote_state" "infra_vpc" {
