509c7a35a7cb91bd92b0eadc2a4dbb4776bd612f
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-03-19 10:45:06 +0000

    Add vpc module

---
+++ terraform/deployments/vpc/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  # backend "s3" {}
+  cloud {
+    organization = "govuk"
+    workspaces {
+      tags = ["vpc", "eks", "aws"]
+    }
+  }
+  required_version = "~> 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      Product              = "GOV.UK"
+      System               = "VPC"
+      Environment          = var.govuk_environment
+      Owner                = "govuk-platform-engineering@digital.cabinet-office.gov.uk"
+      repository           = "govuk-infrastructure"
+      terraform_deployment = basename(abspath(path.root))
+    }
+  }
+}
---
+++ terraform/deployments/vpc/vpc.tf
@@ -0,0 +1,41 @@
+data "aws_region" "current" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "govuk-${var.govuk_environment}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_internet_gateway" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "govuk-${var.govuk_environment}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public.id
+  }
+
+  tags = {
+    Name = "govuk-${var.govuk_environment}"
+  }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.vpc.id
+  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
+}
