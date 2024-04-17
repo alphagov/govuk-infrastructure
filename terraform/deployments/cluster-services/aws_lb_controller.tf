5ddc9e644a41e2ab97a56f564ffd7dcdc36031b9
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2024-04-17 14:28:03 +0100

    Roll out aws-lb-controller 1.7 to production.
    It's been working well in the integration cluster for a few days now.

--- terraform/deployments/cluster-services/aws_lb_controller.tf
+++ terraform/deployments/cluster-services/aws_lb_controller.tf
@@ -5,8 +5,15 @@
 # ../cluster-infrastructure/aws_lb_controller_iam.tf. See
 # https://github.com/alphagov/govuk-infrastructure/blob/main/docs/architecture/decisions/0003-split-terraform-state-into-separate-aws-cluster-and-kubernetes-resource-phases.md#decision for rationale.
 
-locals {
-  aws_lb_values_integration = {
+resource "helm_release" "aws_lb_controller" {
+  name             = "aws-load-balancer-controller"
+  repository       = "https://aws.github.io/eks-charts"
+  chart            = "aws-load-balancer-controller"
+  version          = "1.7.2"
+  namespace        = local.services_ns
+  create_namespace = true
+  timeout          = var.helm_timeout_seconds
+  values = [yamlencode({
     clusterName        = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_id
     defaultSSLPolicy   = "ELBSecurityPolicy-TLS13-1-2-2021-06"
     defaultTargetType  = "ip"
@@ -27,39 +34,5 @@ locals {
         "eks.amazonaws.com/role-arn" = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.aws_lb_controller_role_arn
       }
     }
-  }
-}
-
-resource "helm_release" "aws_lb_controller" {
-  name             = "aws-load-balancer-controller"
-  repository       = "https://aws.github.io/eks-charts"
-  chart            = "aws-load-balancer-controller"
-  version          = var.govuk_environment == "integration" ? "1.7.2" : "1.4.4"
-  namespace        = local.services_ns
-  create_namespace = true
-  timeout          = var.helm_timeout_seconds
-  values = [var.govuk_environment == "integration" ? yamlencode(local.aws_lb_values_integration) : yamlencode({
-    clusterName      = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_id
-    defaultSSLPolicy = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-    serviceAccount = {
-      name = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.aws_lb_controller_service_account_name
-      annotations = {
-        "eks.amazonaws.com/role-arn" = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.aws_lb_controller_role_arn
-      }
-    }
-    replicaCount = var.desired_ha_replicas
   })]
-}
-
-resource "helm_release" "aws_lb_ingress_class" {
-  count      = var.govuk_environment == "integration" ? 0 : 1
-  name       = "aws-lb-ingress-class"
-  repository = "https://alphagov.github.io/govuk-helm-charts/"
-  chart      = "ingress-class"
-  version    = "0.1.1"
-  timeout    = var.helm_timeout_seconds
-}
-moved {
-  from = helm_release.aws_lb_ingress_class
-  to   = helm_release.aws_lb_ingress_class[0]
 }
