7353757783550aba499f52d9bab6fbab0f002f38
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-05-10 10:07:00 +0100

    Re-import content-data-api security group

--- terraform/deployments/govuk-publishing-infrastructure/removed_sg.tf
+++ terraform/deployments/govuk-publishing-infrastructure/removed_sg.tf
@@ -1,12 +1,5 @@
-removed {
-  from = aws_security_group_rule.postgres_from_eks_workers
-  lifecycle {
-    destroy = false
-  }
-}
-removed {
-  from = aws_security_group_rule.mysql_from_eks_workers
-  lifecycle {
-    destroy = false
-  }
+import {
+  for_each = { "content_data_api" = data.terraform_remote_state.infra_security_groups.outputs.sg_content-data-api-postgresql-primary_id }
+  to = aws_security_group_rule.postgres_from_eks_workers[each.key]
+  id = "${each.value}_ingress_tcp_5432_5432_${data.tfe_outputs.cluster_infrastructure.nonsensitive_values.node_security_group_id}"
 }
