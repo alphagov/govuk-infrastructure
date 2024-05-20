63fe1a2fdd56f0f884bceb77f64df8f308ae1e0f
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2024-05-20 14:53:37 +0100

    Remove commented-out variables and fix descriptions.
    - Delete commented-out code.
    - Delete tautological description fields.

--- terraform/deployments/tfc-configuration/variables.tf
+++ terraform/deployments/tfc-configuration/variables.tf
@@ -1,6 +1,7 @@
-#------------------------------------------------------------------------------
+#
 # Organisation
-#------------------------------------------------------------------------------
+#
+
 variable "tfc_hostname" {
   type        = string
   default     = "app.terraform.io"
@@ -9,7 +10,7 @@ variable "tfc_hostname" {
 
 variable "organization" {
   type        = string
-  description = "Name of Organization to create Workspace in."
+  description = "Name of TFC Organization that the workspace will belong to."
   default     = "govuk"
 }
 
@@ -19,52 +20,33 @@ variable "token" {
   sensitive   = true
 }
 
-#------------------------------------------------------------------------------
+#
 # Projects
-#------------------------------------------------------------------------------
+#
+
 variable "project_names" {
   description = "List of project names"
   type        = list(string)
   default     = ["govuk-infrastructure"]
 }
 
-#------------------------------------------------------------------------------
+#
 # Workspace
-#------------------------------------------------------------------------------
+#
 
 variable "workspace_name" {
-  type        = string
-  description = "Name of Workspace."
-  default     = "Default Workspace"
+  type    = string
+  default = "Default Workspace"
 }
 
 variable "workspace_desc" {
-  type        = string
-  description = "Description of Workspace."
-  default     = "Created by Terraform Workspacer module."
+  type    = string
+  default = ""
 }
-
-# variable "agent_pool_id" {
-#   type        = string
-#   description = "ID of existing Agent Pool to assign to Workspace. Only use if `execution_mode` is set to `agent`."
-#   default     = null
-# }
-
-# variable "allow_destroy_plan" {
-#   type        = bool
-#   description = "Boolean setting to allow destroy plans on Workspace."
-#   default     = true
-# }
-
-# variable "auto_apply" {
-#   type        = bool
-#   description = "Boolean to automatically run Terraform Apply when a Terraform Plan with changes is successful."
-#   default     = false
-# }
 
 variable "execution_mode" {
   type        = string
-  description = "Execution mode of Workspace. Valid values are `remote`, `local`, or `agent`."
+  description = "Valid values are `remote`, `local`, or `agent`."
   default     = "remote"
 
   validation {
@@ -73,57 +55,15 @@ variable "execution_mode" {
   }
 }
 
-# variable "assessments_enabled" {
-#   type        = bool
-#   description = "Boolean to enable Health Assessments such as Drift Detection on Workspace."
-#   default     = false
-# }
-
-# variable "file_triggers_enabled" {
-#   type        = bool
-#   description = "Boolean to filter Runs triggered via webhook (VCS push) based on `working_directory` and `trigger_prefixes`."
-#   default     = true
-# }
-
-# variable "global_remote_state" {
-#   type        = bool
-#   description = "Boolean to allow all Workspaces within the Organization to remotely access the State of this Workspace."
-#   default     = false
-# }
-
-# variable "remote_state_consumer_ids" {
-#   type        = list(string)
-#   description = "List of existing Workspace IDs allowed to remotely access the State of Workspace."
-#   default     = null
-# }
-
 variable "queue_all_runs" {
   type        = bool
-  description = "Boolean setting for Workspace to automatically queue all Runs after creation."
+  description = "Whether the workspace should start automatically performing runs immediately after its creation."
   default     = true
 }
 
-# variable "speculative_enabled" {
-#   type        = bool
-#   description = "Boolean to allow Speculative Plans on Workspace."
-#   default     = true
-# }
-
-# variable "structured_run_output_enabled" {
-#   type        = bool
-#   description = "Boolean to enable the advanced Run UI. Set to `false` for the traditional console-based Run output."
-#   default     = true
-# }
-
-# variable "ssh_key_id" {
-#   type        = string
-#   description = "SSH private key the Workspace will use for downloading Terraform modules from Git-based module sources. Key must exist in Organization first."
-#   default     = null
-# }
-
 variable "workspace_tags" {
   type        = list(string)
-  description = "List of tag names to apply to Workspace. Tags must only contain letters, numbers, or colons."
+  description = "List of tag names to apply to the workspace. Tags must only contain letters, numbers, or colons."
   default     = []
 }
 
@@ -133,15 +73,9 @@ variable "terraform_version" {
   default     = "~> 1.8.0"
 }
 
-# variable "trigger_prefixes" {
-#   type        = list(string)
-#   description = "List of paths relative to the root of the VCS repo to filter on when `file_triggers_enabled` is `true`."
-#   default     = null
-# }
-
 variable "trigger_patterns" {
   type        = list(string)
-  description = "List of glob patterns that describe the files monitored for changes to trigger Runs in Workspace. Mutually exclusive with `trigger_prefixes`. Only available with TFC."
+  description = "List of glob patterns that describe the files monitored for changes to trigger Runs in the workspace. Mutually exclusive with `trigger_prefixes`. Only available with TFC."
   default     = null
 }
 
@@ -153,138 +87,42 @@ variable "working_directory" {
 
 variable "vcs_repo" {
   type        = map(string)
-  description = "Map of settings to connect Workspace to VCS repository."
+  description = "Map of settings to connect the workspace to a VCS repository."
   default     = {}
 }
-
-# variable "tags_regex" {
-#   type        = string
-#   description = "A regular expression used to trigger a Run in Workspace for matching Git tags. This option conflicts with `trigger_patterns` and `trigger_prefixes`. Should only set this value if the former is not being used."
-#   default     = null
-# }
-
-# variable "force_delete" {
-#   type        = bool
-#   description = "Boolean to allow deletion of the Workspace if there is a Terraform state that contains resources."
-#   default     = null
-# }
 
 variable "project_name" {
   type        = string
-  description = "Name of existing Project to place Workspace in."
+  description = "Name of existing TFC project that the workspace will belong to."
   default     = null
 }
 
-#------------------------------------------------------------------------------
-# Workspace Variables
-#------------------------------------------------------------------------------
+#
+# Workspace variables
+#
+
 variable "tfvars" {
   type        = any
-  description = "Map of Terraform variables to add to Workspace."
+  description = "Map of Terraform variables to add to the workspace."
   default     = {}
 }
 
-# variable "tfvars_sensitive" {
-#   type        = any
-#   description = "Map of sensitive Terraform variables to add to Workspace."
-#   default     = {}
-# }
+#
+# Team access
+#
 
-# variable "tfvars_ignore_changes" {
-#   type        = any
-#   description = "Map of Terraform variables to add to Workspace whereby changes made outside of Terraform will be ignored."
-#   default     = {}
-# }
-
-# variable "envvars" {
-#   type        = map(string)
-#   description = "Map of Environment variables to add to Workspace."
-#   default     = {}
-# }
-
-# variable "envvars_sensitive" {
-#   type        = map(string)
-#   description = "Map of sensitive Environment variables to add to Workspace."
-#   default     = {}
-# }
-
-# variable "envvars_ignore_changes" {
-#   type        = map(string)
-#   description = "Map of sensitive Environment variables to add to Workspace whereby changes made outside of Terraform will be ignored."
-#   default     = {}
-# }
-
-# #------------------------------------------------------------------------------
-# # Team Access
-# #------------------------------------------------------------------------------
 variable "team_access" {
   type        = map(string)
-  description = "Map of existing Team(s) and built-in permissions to grant on Workspace."
+  description = "Map of existing Team(s) and built-in permissions to grant on the workspace."
   default     = {}
 }
 
-# variable "custom_team_access" {
-#   type = map(
-#     object(
-#       {
-#         runs              = string
-#         variables         = string
-#         state_versions    = string
-#         sentinel_mocks    = string
-#         workspace_locking = bool
-#         run_tasks         = bool
-#       }
-#     )
-#   )
-#   description = "Map of existing Team(s) and custom permissions to grant on Workspace. If used, all keys in the object must be specified."
-#   default     = {}
-# }
+#
+# Workspace variable sets
+#
 
-# #------------------------------------------------------------------------------
-# # Notifications
-# #------------------------------------------------------------------------------
-# variable "notifications" {
-#   type = list(
-#     object(
-#       {
-#         name             = string
-#         destination_type = string
-#         url              = optional(string)
-#         token            = optional(string)
-#         email_addresses  = optional(list(string))
-#         email_user_ids   = optional(list(string))
-#         triggers         = list(string)
-#         enabled          = bool
-#       }
-#     )
-#   )
-#   description = "List of Notification objects to configure on Workspace."
-#   default     = []
-# }
-
-# #------------------------------------------------------------------------------
-# # Run Triggers
-# #------------------------------------------------------------------------------
-# variable "run_trigger_source_workspaces" {
-#   type        = list(string)
-#   description = "List of existing Workspace names that will trigger runs on Workspace."
-#   default     = []
-# }
-
-# #------------------------------------------------------------------------------
-# # Workspace Variable Sets
-# #------------------------------------------------------------------------------
 variable "variable_set_names" {
   type        = list(string)
-  description = "List of names of existing Variable Sets to add this Workspace into."
+  description = "List of names of existing Variable Sets to add to the workspace."
   default     = []
 }
-
-# #------------------------------------------------------------------------------
-# # Workspace Policy Sets
-# #------------------------------------------------------------------------------
-# variable "policy_set_names" {
-#   type        = list(string)
-#   description = "List of names of existing Policy Sets to add this Workspace into."
-#   default     = []
-# }
