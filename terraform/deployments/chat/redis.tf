b188264da7ab50532ba7ebb5920e3dbf35b16573
roch <trinque@free.fr>
2024-05-20 18:02:08 +0100

    Add dedicated Redis for GOV.UK chat in integration/staging/prod
    To reduce the blast radius of GOV.UK chat we use a separated Redis
    cluster for it.

---
+++ terraform/deployments/chat/redis.tf
@@ -0,0 +1,46 @@
+locals {
+  chat_redis_name     = "chat-redis"
+  elasticache_subnets = data.terraform_remote_state.infra_networking.outputs.private_subnet_elasticache_ids
+}
+
+resource "aws_elasticache_subnet_group" "chat_redis_cluster" {
+  name       = local.chat_redis_name
+  subnet_ids = local.elasticache_subnets
+}
+
+resource "aws_security_group" "chat_redis_cluster" {
+  name        = local.chat_redis_name
+  vpc_id      = data.tfe_outputs.vpc.nonsensitive_values.id
+  description = "GOV.UK Chat Redis cluster"
+  tags = {
+    Name   = local.chat_redis_name
+    System = "Sidekiq"
+  }
+}
+
+resource "aws_elasticache_replication_group" "chat_redis_cluster" {
+  apply_immediately          = var.chat_redis_cluster_apply_immediately
+  replication_group_id       = local.chat_redis_name
+  description                = "Redis for Sidekiq queues"
+  node_type                  = var.chat_redis_cluster_node_type
+  num_cache_clusters         = var.chat_redis_cluster_num_cache_clusters
+  automatic_failover_enabled = var.chat_redis_cluster_automatic_failover_enabled
+  multi_az_enabled           = var.chat_redis_cluster_multi_az_enabled
+  parameter_group_name       = var.chat_redis_cluster_parameter_group_name
+  engine_version             = var.chat_redis_cluster_engine_version
+  subnet_group_name          = aws_elasticache_subnet_group.chat_redis_cluster.name
+  security_group_ids         = [aws_security_group.chat_redis_cluster.id]
+  tags = {
+    Name   = local.chat_redis_name
+    System = "Sidekiq"
+  }
+}
+
+resource "aws_route53_record" "chat_redis_cluster" {
+  zone_id = local.internal_dns_zone_id
+  # TODO: consider removing EKS suffix once the old EC2 environments are gone.
+  name    = "${local.chat_redis_name}.eks"
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_elasticache_replication_group.chat_redis_cluster.primary_endpoint_address]
+}
---
+++ terraform/deployments/chat/variables.tf
@@ -0,0 +1,45 @@
+variable "engine_version" {
+  type = string
+}
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+variable "govuk_aws_state_bucket" {
+  type        = string
+  description = "Bucket where govuk-aws state is stored"
+}
+variable "govuk_environment" {
+  type        = string
+  description = "Acceptable values are test, integration, staging, production"
+}
+
+variable "chat_redis_cluster_apply_immediately" {
+  type        = bool
+  description = "Specifies whether any modifications are applied immediately, or during the next maintenance window."
+}
+variable "chat_redis_cluster_node_type" {
+  type        = string
+  description = "Instance class to be used."
+}
+variable "chat_redis_cluster_num_cache_clusters" {
+  type        = string
+  description = "Number of cache clusters (primary and replicas) this replication group will have."
+}
+variable "chat_redis_cluster_automatic_failover_enabled" {
+  type        = bool
+  description = "Specifies whether a read-only replica will be automatically promoted to read/write primary if the existing primary fails."
+}
+variable "chat_redis_cluster_multi_az_enabled" {
+  type        = bool
+  description = "Specifies whether to enable Multi-AZ Support for the replication group."
+}
+variable "chat_redis_cluster_parameter_group_name" {
+  type        = string
+  description = "Name of the parameter group to associate with this replication group. If this argument is omitted, the default cache parameter group for the specified engine is used."
+}
+variable "chat_redis_cluster_engine_version" {
+  type        = string
+  description = "Version number of the cache engine to be used for the cache clusters in this replication group."
+}
