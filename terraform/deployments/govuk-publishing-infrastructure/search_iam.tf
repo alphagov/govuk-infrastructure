894b1a6f9dbd2b27105bc55c99e6f705a3656f10
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2024-05-20 14:35:01 +0100

    Remove learning to rank infrastructure
    Learning to rank is no longer used by search-api.
    
    We don't need to retain the relevance bucket, or allow search-api access to sagemaker.

--- terraform/deployments/govuk-publishing-infrastructure/search_iam.tf
+++ terraform/deployments/govuk-publishing-infrastructure/search_iam.tf
@@ -1,39 +1,6 @@
 # TODO: instead of granting access to nodes, use IRSA (IAM Roles for Service
 # Accounts aka pod identity) to grant access specifically to Search.
 
-data "aws_iam_policy_document" "search_relevancy_bucket_policy" {
-  statement {
-    sid       = "ReadListOfBuckets"
-    actions   = ["s3:ListAllMyBuckets"]
-    resources = ["*"]
-  }
-
-  statement {
-    sid = "SearchRelevancyAccess"
-    actions = [
-      "s3:DeleteObject",
-      "s3:Put*",
-      "s3:Get*",
-      "s3:List*",
-    ]
-    resources = [
-      "arn:aws:s3:::${aws_s3_bucket.search_relevancy_bucket.id}",
-      "arn:aws:s3:::${aws_s3_bucket.search_relevancy_bucket.id}/*",
-    ]
-  }
-}
-
-resource "aws_iam_policy" "search_relevancy_bucket_access" {
-  name        = "govuk-${var.govuk_environment}-search-relevancy-bucket-access-policy"
-  policy      = data.aws_iam_policy_document.search_relevancy_bucket_policy.json
-  description = "Allows reading and writing of the search relevancy bucket"
-}
-
-resource "aws_iam_role_policy_attachment" "search_relevancy_s3_eks_policy_attachment" {
-  role       = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.worker_iam_role_name
-  policy_arn = aws_iam_policy.search_relevancy_bucket_access.arn
-}
-
 data "aws_iam_policy_document" "sitemaps_bucket_policy" {
   statement {
     sid       = "ReadListOfBuckets"
@@ -64,26 +31,4 @@ resource "aws_iam_policy" "sitemaps_bucket_access" {
 resource "aws_iam_role_policy_attachment" "sitemaps_s3_eks_policy_attachment" {
   role       = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.worker_iam_role_name
   policy_arn = aws_iam_policy.sitemaps_bucket_access.arn
-}
-
-data "aws_iam_policy_document" "use_sagemaker" {
-  statement {
-    sid = "InvokeSagemaker"
-    actions = [
-      "sagemaker:DescribeEndpoint",
-      "sagemaker:InvokeEndpoint",
-    ]
-    resources = ["arn:aws:sagemaker:*"]
-  }
-}
-
-resource "aws_iam_policy" "use_sagemaker" {
-  name        = "govuk-${var.govuk_environment}-search-use-sagemaker-policy"
-  policy      = data.aws_iam_policy_document.use_sagemaker.json
-  description = "Allows invoking and describing SageMaker endpoints"
-}
-
-resource "aws_iam_role_policy_attachment" "search_api_sagemaker_attachment" {
-  role       = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.worker_iam_role_name
-  policy_arn = aws_iam_policy.use_sagemaker.arn
 }
