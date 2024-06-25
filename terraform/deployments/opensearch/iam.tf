40b77fdabe1bce8f75d5a023c9c4b9852f5bfdcb
Ian Howell <ian.howell@digital.cabinet-office.gov.uk>
2024-06-25 16:08:20 +0100

    Delete locals.tf & iam_policy.tftpl and reconfigure locals as per Chris Banks suggestion modified:   iam.tf deleted:    iam_policy.tftpl deleted:    locals.tf

--- terraform/deployments/opensearch/iam.tf
+++ terraform/deployments/opensearch/iam.tf
@@ -1,4 +1,18 @@
 # IAM roles required for manual snapshot process
+locals {
+  snapshot_bucket_arns = {
+    production = ["arn:aws:s3:::govuk-production-chat-opensearch-snapshots"]
+    staging = [
+      "arn:aws:s3:::govuk-production-chat-opensearch-snapshots",
+      "arn:aws:s3:::govuk-staging-chat-opensearch-snapshots",
+    ]
+    integration = [
+      "arn:aws:s3:::govuk-staging-chat-opensearch-snapshots",
+      "arn:aws:s3:::govuk-integration-chat-opensearch-snapshots",
+    ]
+  }[var.govuk_environment]
+}
+
 resource "aws_iam_role" "opensearch_snapshot" {
   name               = "govuk-${var.govuk_environment}-${var.service}-opensearch-snapshot-role"
   assume_role_policy = data.aws_iam_policy_document.opensearch_snapshot_assume_role.json
@@ -14,9 +28,24 @@ data "aws_iam_policy_document" "opensearch_snapshot_assume_role" {
   }
 }
 
+data "aws_iam_policy_document" "opensearch_snapshot" {
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = var.snapshot_bucket_arns
+  }
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+    resources = formatlist("%s/*", var.snapshot_bucket_arns)
+  }
+}
+
 resource "aws_iam_policy" "opensearch_snapshot" {
   name   = "govuk-${var.govuk_environment}-${var.service}-opensearch-snapshot-bucket-policy"
-  policy = templatefile("iam_policy.tftpl", { BucketArns = replace(local.snapshot_bucket_arns, "/*", ""), BucketWildArns = local.snapshot_bucket_arns })
+  policy = data.aws_iam_policy_document.opensearch_snapshot.json
 }
 
 resource "aws_iam_policy_attachment" "opensearch_snapshot" {
