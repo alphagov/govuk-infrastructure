f48d5153c4e699148e5a9029a28aa3a761af5bda
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-07-11 14:17:46 +0100

    Attempt temporary fix for GCP permissions

--- terraform/deployments/vpc/google_logging_bucket.tf
+++ terraform/deployments/vpc/google_logging_bucket.tf
@@ -1,5 +1,5 @@
 data "google_project" "project" {}
-
+/*
 resource "google_storage_bucket" "google_logging" {
   name          = "govuk-${var.govuk_environment}-gcp-logging"
   location      = "eu"
@@ -27,4 +27,17 @@ resource "google_storage_bucket_acl" "google_logging" {
   role_entity = [
     "WRITER:group-cloud-storage-analytics@google.com",
   ]
+}*/
+
+locals {
+  google_project = var.govuk_environment == "staging" ? "govuk-staging-160211" : "govuk-${var.govuk_environment}"
+  sae = "terraform-cloud-${var.govuk_environment}@${local.google_project}.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_binding" "tfc" {
+  project = local.google_project
+  role    = "roles/owner"
+  members = ["user:sam.simpson@digital.cabinet-office.gov.uk",
+  "group:govuk-gcp-access@digital.cabinet-office.gov.uk",
+  "serviceAccount:${local.sae}"]
 }
