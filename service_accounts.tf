985b56d29b3b9b3f6056571a1cc660ad55176ad5
Christian Sutter <git@doublesignal.com>
2024-07-17 10:22:12 +0000

    Add IAM service account and role for Search Admin

--- service_accounts.tf
+++ service_accounts.tf
@@ -37,6 +37,45 @@ resource "google_project_iam_binding" "api" {
   ]
 }
 
+# Creates and configures service accounts, IAM roles, role bindings, and keys for `search-admin` to
+# be able to access the Discovery Engine API.
+resource "google_service_account" "search_admin" {
+  account_id   = "search-admin"
+  display_name = "Search Admin (Rails admin app)"
+  description  = "Service account to provide access to the search-admin Rails app"
+}
+
+resource "google_service_account_key" "search_admin" {
+  service_account_id = google_service_account.search_admin.id
+}
+
+resource "google_project_iam_custom_role" "search_admin" {
+  role_id     = "search_admin"
+  title       = "Search Admin"
+  description = "Provides the required permissions for Search Admin to access Discovery Engine"
+
+  permissions = [
+    "discoveryengine.controls.create",
+    "discoveryengine.controls.delete",
+    "discoveryengine.controls.get",
+    "discoveryengine.controls.list",
+    "discoveryengine.controls.update",
+    "discoveryengine.operations.get",
+    "discoveryengine.servingConfigs.get",
+    "discoveryengine.servingConfigs.list",
+    "discoveryengine.servingConfigs.update",
+  ]
+}
+
+resource "google_project_iam_binding" "search_admin" {
+  project = var.gcp_project_id
+  role    = google_project_iam_custom_role.search_admin.id
+
+  members = [
+    google_service_account.search_admin.member
+  ]
+}
+
 resource "google_service_account" "analytics_events_pipeline" {
   account_id   = "analytics-events-pipeline"
   display_name = "analytics-events-pipeline"
