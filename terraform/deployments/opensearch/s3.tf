b8755390bab5c9adc24db30e556164bafcd8dddc
Ian Howell <ian.howell@digital.cabinet-office.gov.uk>
2024-07-19 10:50:14 +0100

    Change SSE for S3 bucket from "aws:kms" to "AES256" so that cross account decryption of files can take place.

--- terraform/deployments/opensearch/s3.tf
+++ terraform/deployments/opensearch/s3.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "opensearch_snapsh
   bucket = aws_s3_bucket.opensearch_snapshot.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      sse_algorithm = "AES256"
     }
   }
 }
