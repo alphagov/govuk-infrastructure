64ef911dd44a814a117f436a924a4f4443a2a3e2
Sean Rankine <sean.rankine@me.com>
2024-07-31 16:22:46 +0100

    Migrate govuk-mirror buckets from govuk-aws repo
    This migrates the infrastructure from "infra-mirror-bucket" project in
    govuk-aws.

--- terraform/deployments/govuk-publishing-infrastructure/main.tf
+++ terraform/deployments/govuk-publishing-infrastructure/main.tf
@@ -65,4 +65,15 @@ provider "fastly" {
 provider "tfe" {
 }
 
+provider "google" {
+  default_labels = {
+    product              = "gov-uk"
+    system               = "terraform-cloud"
+    environment          = var.govuk_environment
+    owner                = "govuk-platform-engineering"
+    repository           = "govuk-infrastructure"
+    terraform_deployment = lower(basename(abspath(path.root)))
+  }
+}
+
 data "aws_caller_identity" "current" {}
