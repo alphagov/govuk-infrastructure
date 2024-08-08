3f472d70554e3d18d5736cdeb16322b9594b03ea
roch <trinque@free.fr>
2024-08-08 09:53:39 +0100

    Add new variables for the chat app in integration

--- terraform/deployments/tfc-configuration/variables-integration.tf
+++ terraform/deployments/tfc-configuration/variables-integration.tf
@@ -73,6 +73,13 @@ module "variable-set-chat-integration" {
     chat_redis_cluster_node_type                  = "cache.r6g.xlarge"
     chat_redis_cluster_num_cache_clusters         = "1"
     chat_redis_cluster_parameter_group_name       = "default.redis7"
+    cloudfront_create                             = "1"
+    cloudfront_enable                             = true
+    service_disabled                              = false
+    origin_chat_domain                            = "chat.eks.integration.govuk.digital"
+    origin_chat_id                                = "Chat origin"
+    cloudfront_chat_distribution_aliases          = ["chat.integration.publishing.service.gov.uk"]
+    chat_certificate_arn                          = "arn:aws:acm:us-east-1:210287912431:certificate/458b8373-08a4-4cdc-bb00-524bcc480b5b"
   }
 }
 
