8c32049e015a84ee46ede425d8ecdec2cb218db2
Ian Howell <ian.howell@digital.cabinet-office.gov.uk>
2024-09-30 12:22:53 +0100

    Remove S3 lifecycle configuration from chat opensearch snapshot bucket

--- terraform/deployments/opensearch/s3.tf
+++ terraform/deployments/opensearch/s3.tf
@@ -32,32 +32,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "opensearch_snapsh
   }
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "opensearch_snapshot" {
-  bucket = aws_s3_bucket.opensearch_snapshot.id
-  rule {
-    id     = "production"
-    status = var.govuk_environment == "production" ? "Enabled" : "Disabled"
-    filter {}
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-    transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
-    expiration { days = 120 }
-    noncurrent_version_expiration { noncurrent_days = 1 }
-  }
-  rule {
-    id     = "non-production"
-    status = var.govuk_environment != "production" ? "Enabled" : "Disabled"
-    filter {}
-    expiration { days = 2 }
-    noncurrent_version_expiration { noncurrent_days = 1 }
-  }
-}
-
 resource "aws_s3_bucket_policy" "opensearch_snapshot" {
   bucket = aws_s3_bucket.opensearch_snapshot.id
   policy = data.aws_iam_policy_document.opensearch_snapshot_bucket_policy.json
