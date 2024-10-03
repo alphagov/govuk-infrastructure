18903acd94f55290b7841ad1158b8299b4d1ad7b
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2024-10-03 13:29:25 +0100

    Output root zone IDs in vpc module

--- terraform/deployments/vpc/outputs.tf
+++ terraform/deployments/vpc/outputs.tf
@@ -14,3 +14,13 @@ output "rds_enhanced_monitoring_role_arn" {
   description = "The ARN of the IAM role for RDS Enhanced Monitoring"
   value       = aws_iam_role.rds_enhanced_monitoring.arn
 }
+
+output "internal_root_zone_id" {
+  description = "ID of the internal Route53 DNS zone"
+  value       = aws_route53_zone.internal_zone.id
+}
+
+output "external_root_zone_id" {
+  description = "ID of the external Route53 DNS zone"
+  value       = aws_route53_zone.external_zone.id
+}
