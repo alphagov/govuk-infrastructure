8fb7e1f9494990c6dca5c6d53a2c4bdde7ec1d38
Ian Ansell <ian.ansell.uk@gmail.com>
2024-10-25 15:04:47 +0100

    clean up workspace and directory names and add non prod read access

--- terraform/deployments/tfc-configuration/gcp-projects.tf
+++ terraform/deployments/tfc-configuration/gcp-projects.tf
@@ -5,11 +5,11 @@ module "gcp-ga4-analytics" {
   organization        = var.organization
   workspace_name      = "gcp-ga4-analytics"
   workspace_desc      = "GCP project management for the GA4 production project"
-  workspace_tags      = ["production", "ga4-analytics-352613", "gcp"]
+  workspace_tags      = ["production", "ga4-analytics", "gcp"]
   terraform_version   = var.terraform_version
   execution_mode      = "remote"
-  working_directory   = "/terraform/deployments/ga4-analytics-352613/"
-  trigger_patterns    = ["/terraform/deployments/ga4-analytics-352613/**/*"]
+  working_directory   = "/terraform/deployments/ga4-analytics/"
+  trigger_patterns    = ["/terraform/deployments/ga4-analytics/**/*"]
   global_remote_state = true
 
   project_name = "govuk-infrastructure"
@@ -22,6 +22,7 @@ module "gcp-ga4-analytics" {
   team_access = {
     "GOV.UK Production"            = "write"
     "Google Cloud Data Production" = "write"
+    "GOV.UK Non-Production"        = "read"
   }
 
   variable_set_names = [
