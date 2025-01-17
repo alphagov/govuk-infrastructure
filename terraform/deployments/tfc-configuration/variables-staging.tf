2270f794d505c95f0e6a6e86f3186d2a14301f15
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-01-17 12:43:15 +0000

    explicitly enable NAT for legacy subnets that need it
    This is required due to type constraints on the variable in the vpc module

--- terraform/deployments/tfc-configuration/variables-staging.tf
+++ terraform/deployments/tfc-configuration/variables-staging.tf
@@ -36,9 +36,9 @@ module "variable-set-staging" {
     }
 
     legacy_private_subnets = {
-      a = { az = "eu-west-1a", cidr = "10.12.4.0/24" }
-      b = { az = "eu-west-1b", cidr = "10.12.5.0/24" }
-      c = { az = "eu-west-1c", cidr = "10.12.6.0/24" }
+      a = { az = "eu-west-1a", cidr = "10.12.4.0/24", nat = true }
+      b = { az = "eu-west-1b", cidr = "10.12.5.0/24", nat = true }
+      c = { az = "eu-west-1c", cidr = "10.12.6.0/24", nat = true }
 
       rds_a = { az = "eu-west-1a", cidr = "10.12.10.0/24", nat = false }
       rds_b = { az = "eu-west-1b", cidr = "10.12.11.0/24", nat = false }
