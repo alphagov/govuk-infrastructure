809e2fe61ac81380a588a13c2daf27886c547dfd
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-01-20 13:38:33 +0000

    Add elasticsearch subnets to legacy subnet list
    These were missed during the initial legacy subnet changes

--- terraform/deployments/tfc-configuration/variables-production.tf
+++ terraform/deployments/tfc-configuration/variables-production.tf
@@ -47,6 +47,10 @@ module "variable-set-production" {
       elasticache_a = { az = "eu-west-1a", cidr = "10.13.7.0/24", nat = false }
       elasticache_b = { az = "eu-west-1b", cidr = "10.13.8.0/24", nat = false }
       elasticache_c = { az = "eu-west-1c", cidr = "10.13.9.0/24", nat = false }
+
+      elasticsearch_a = { az = "eu-west-1a", cidr = "10.13.16.0/24", nat = false }
+      elasticsearch_b = { az = "eu-west-1b", cidr = "10.13.17.0/24", nat = false }
+      elasticsearch_c = { az = "eu-west-1c", cidr = "10.13.18.0/24", nat = false }
     }
 
     govuk_environment = "production"
