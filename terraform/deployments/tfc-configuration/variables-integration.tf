235a93fe973767ab12ba003e65cac0be0128ee8a
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-02-21 14:37:33 +0000

    Add initial serverless ElastiCache resources

--- terraform/deployments/tfc-configuration/variables-integration.tf
+++ terraform/deployments/tfc-configuration/variables-integration.tf
@@ -630,11 +630,14 @@ module "variable-set-elasticache-integration" {
 
   tfvars = {
     instances = {
-      test = {
-        max_storage_gb       = 20
-        max_ecpus_per_second = 6000
+      /*
+      "example" = {
+        max_storage_gb       = 30
+        max_ecpus_per_second = 7000
+        major_engine_version = "7"
       }
-      test_defaults = {}
+      */
+      "publishing-api" = {}
     }
   }
 }
