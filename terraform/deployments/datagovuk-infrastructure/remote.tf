7507f0f294fd412901d45d2e64db235d2c1b35f1
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2025-03-05 19:06:51 +0000

    Remove terraform_remote_state infra_vpc from datagovuk-infrastructure

--- terraform/deployments/datagovuk-infrastructure/remote.tf
+++ terraform/deployments/datagovuk-infrastructure/remote.tf
@@ -4,12 +4,3 @@ data "tfe_outputs" "cluster_infrastructure" {
   organization = "govuk"
   workspace    = "cluster-infrastructure-${var.govuk_environment}"
 }
-
-data "terraform_remote_state" "infra_vpc" {
-  backend = "s3"
-  config = {
-    bucket = var.govuk_aws_state_bucket
-    key    = "govuk/infra-vpc.tfstate"
-    region = data.aws_region.current.name
-  }
-}
