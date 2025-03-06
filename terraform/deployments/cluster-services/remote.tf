f81dbec8dda901f3eb162b100d6a5b94a84e5267
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-03-06 15:55:14 +0000

    Fix tfe_outputs for ephemeral envs in cluster-services

--- terraform/deployments/cluster-services/remote.tf
+++ terraform/deployments/cluster-services/remote.tf
@@ -1,3 +1,7 @@
+locals {
+  env_suffix = startswith(var.govuk_environment, "eph-") ? "ephemeral" : "${var.govuk_environment}"
+}
+
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
@@ -9,5 +13,5 @@ data "tfe_outputs" "cluster_infrastructure" {
 
 data "tfe_outputs" "vpc" {
   organization = "govuk"
-  workspace    = "vpc-${var.govuk_environment}"
+  workspace    = startswith(var.govuk_environment, "eph-") ? "vpc-ephemeral" : "vpc-${var.govuk_environment}"
 }
