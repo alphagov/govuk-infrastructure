11cbe8bcc158f202945dac3bf1006dfdff0d72df
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-03-07 09:07:09 +0000

    Move namespaces in cluster-services to their own file

---
+++ terraform/deployments/cluster-services/namespaces.tf
@@ -0,0 +1,35 @@
+resource "kubernetes_namespace" "apps" {
+  metadata {
+    name = var.apps_namespace
+    annotations = {
+      "argocd.argoproj.io/sync-options" = "ServerSideApply=true"
+    }
+    labels = {
+      "app.kubernetes.io/managed-by"  = "Terraform"
+      "argocd.argoproj.io/managed-by" = "cluster-services"
+      # https://kubernetes-sigs.github.io/aws-load-balancer-controller/latest/deploy/pod_readiness_gate/
+      "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled"
+      "pod-security.kubernetes.io/audit"        = "restricted"
+      "pod-security.kubernetes.io/enforce"      = "restricted"
+      "pod-security.kubernetes.io/warn"         = "restricted"
+    }
+  }
+}
+
+resource "kubernetes_namespace" "licensify" {
+  metadata {
+    name = var.licensify_namespace
+    annotations = {
+      "argocd.argoproj.io/sync-options" = "ServerSideApply=true"
+    }
+    labels = {
+      "app.kubernetes.io/managed-by"  = "Terraform"
+      "argocd.argoproj.io/managed-by" = "cluster-services"
+      # https://kubernetes-sigs.github.io/aws-load-balancer-controller/latest/deploy/pod_readiness_gate/
+      "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled"
+      "pod-security.kubernetes.io/audit"        = "restricted"
+      "pod-security.kubernetes.io/enforce"      = "restricted"
+      "pod-security.kubernetes.io/warn"         = "restricted"
+    }
+  }
+}
