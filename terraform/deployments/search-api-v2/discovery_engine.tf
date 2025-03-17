1661b7bf1e3ab67b810a0991dacebeac9b143fcc
Christian Sutter <git@doublesignal.com>
2025-03-17 11:10:45 +0000

    Search API v2: Split secrets out from DE config
    This splits out the AWS Secrets Manager config into its own file.

--- terraform/deployments/search-api-v2/discovery_engine.tf
+++ terraform/deployments/search-api-v2/discovery_engine.tf
@@ -54,30 +54,3 @@ resource "google_discovery_engine_search_engine" "govuk" {
     company_name = "GOV.UK"
   }
 }
-
-resource "aws_secretsmanager_secret" "discovery_engine_configuration" {
-  name                    = "govuk/search-api-v2/google-cloud-discovery-engine-configuration"
-  recovery_window_in_days = 0 # Force delete to allow re-applying immediately after destroying
-}
-
-resource "aws_secretsmanager_secret_version" "discovery_engine_configuration" {
-  secret_id = aws_secretsmanager_secret.discovery_engine_configuration.id
-  secret_string = jsonencode({
-    "GOOGLE_CLOUD_CREDENTIALS"                 = base64decode(google_service_account_key.api.private_key)
-    "GOOGLE_CLOUD_PROJECT_ID"                  = var.gcp_project_id
-    "DISCOVERY_ENGINE_DEFAULT_COLLECTION_NAME" = local.discovery_engine_default_collection_name
-  })
-}
-
-resource "aws_secretsmanager_secret" "discovery_engine_configuration_search_admin" {
-  name                    = "govuk/search-admin/google-cloud-discovery-engine-configuration"
-  recovery_window_in_days = 0 # Force delete to allow re-applying immediately after destroying
-}
-
-resource "aws_secretsmanager_secret_version" "discovery_engine_configuration_search_admin" {
-  secret_id = aws_secretsmanager_secret.discovery_engine_configuration_search_admin.id
-  secret_string = jsonencode({
-    "GOOGLE_CLOUD_CREDENTIALS"                 = base64decode(google_service_account_key.search_admin.private_key)
-    "DISCOVERY_ENGINE_DEFAULT_COLLECTION_NAME" = local.discovery_engine_default_collection_name
-  })
-}
