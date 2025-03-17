34b5a596b3bad6cf021042410489e187bb80f284
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-03-17 10:00:16 +0000

    Change maintenance window for Link Checker API
    This was added in
    https://github.com/alphagov/govuk-infrastructure/pull/1807 but won't
    apply as it overlaps the with the backup window.
    
    Moving the maintenance window earlier.
    
    AWS requires a minimum of 30 minutes for RDS maintenance, so this should
    be more than adequate.

--- terraform/deployments/tfc-configuration/variables-staging.tf
+++ terraform/deployments/tfc-configuration/variables-staging.tf
@@ -426,7 +426,7 @@ module "variable-set-rds-staging" {
         performance_insights_enabled = false
         freestoragespace_threshold   = 10737418240
         project                      = "GOV.UK - Publishing"
-        maintenance_window           = "Mon:01:00-Mon:03:00"
+        maintenance_window           = "Mon:00:00-Mon:01:00"
       }
 
       local_links_manager = {
