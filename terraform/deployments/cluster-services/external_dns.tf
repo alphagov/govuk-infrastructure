dd65bf8b8322502e4b9a2aa3b367546f8a491a3d
nimalank7 <43585976+nimalank7@users.noreply.github.com>
2025-03-21 15:01:14 +0000

    Revert "Update Helm release external-dns to v1.16.0"

--- terraform/deployments/cluster-services/external_dns.tf
+++ terraform/deployments/cluster-services/external_dns.tf
@@ -2,7 +2,7 @@ resource "helm_release" "external_dns" {
   name             = "external-dns"
   repository       = "https://kubernetes-sigs.github.io/external-dns"
   chart            = "external-dns"
-  version          = "1.16.0"
+  version          = "1.15.2"
   namespace        = local.services_ns
   create_namespace = true
 
