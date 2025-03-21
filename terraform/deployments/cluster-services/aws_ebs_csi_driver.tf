1bf65024356c718210c0a31d91ffa86ded4839ec
nimalank7 <43585976+nimalank7@users.noreply.github.com>
2025-03-21 17:43:20 +0000

    Revert "Update Helm release aws-ebs-csi-driver to v2.41.0"

--- terraform/deployments/cluster-services/aws_ebs_csi_driver.tf
+++ terraform/deployments/cluster-services/aws_ebs_csi_driver.tf
@@ -2,7 +2,7 @@ resource "helm_release" "aws_ebs_csi_driver" {
   name             = "aws-ebs-csi-driver"
   repository       = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
   chart            = "aws-ebs-csi-driver"
-  version          = "2.41.0"
+  version          = "2.39.3"
   namespace        = "kube-system"
   create_namespace = true
   timeout          = var.helm_timeout_seconds
