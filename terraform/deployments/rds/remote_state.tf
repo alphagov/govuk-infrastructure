9843eb426491c6c5099f832d5a73f2f970a4d9f8
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-03-28 14:36:40 +0000

    Update rds module to reference logging module for RDS monitoring role ARN

--- terraform/deployments/rds/remote_state.tf
+++ terraform/deployments/rds/remote_state.tf
@@ -3,6 +3,11 @@ data "tfe_outputs" "cluster_infrastructure" {
   workspace    = "cluster-infrastructure-${var.govuk_environment}"
 }
 
+data "tfe_outputs" "logging" {
+  organization = "govuk"
+  workspace    = "logging-${var.govuk_environment}"
+}
+
 data "tfe_outputs" "vpc" {
   organization = "govuk"
   workspace    = "vpc-${var.govuk_environment}"
