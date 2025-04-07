cd64ab40be84457c7ec599b15479aed65119dbc2
Ian Howell <ian.howell@digital.cabinet-office.gov.uk>
2025-04-07 17:17:24 +0100

    Update DNS reference

--- terraform/deployments/elasticache/route53.tf
+++ terraform/deployments/elasticache/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "cache" {
   for_each = var.caches
 
-  zone_id = data.tfe_outputs.vpc.nonsensitive_values.internal_root_zone_id
+  zone_id = data.tfe_outputs.root-dns.nonsensitive_values.internal_root_zone_id
   name    = each.value.name
   type    = "CNAME"
   ttl     = 300
