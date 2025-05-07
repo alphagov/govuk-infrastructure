94d94599f69f7331948c142fd07c5df091513344
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-05-07 15:55:58 +0100

    Add staging and integration workspaces for release module

--- terraform/deployments/tfc-configuration/release.tf
+++ terraform/deployments/tfc-configuration/release.tf
@@ -1,3 +1,66 @@
+module "release-integration" {
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
+
+  organization        = var.organization
+  workspace_name      = "release-integration"
+  workspace_desc      = "Manages IAM roles and policies for the Release app"
+  workspace_tags      = ["integration", "release", "aws"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/deployments/release/"
+  trigger_patterns    = ["/terraform/deployments/release/**/*"]
+  global_remote_state = true
+
+  project_name = "govuk-infrastructure"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Non-Production (r/o)" = "write"
+    "GOV.UK Production" = "write"
+  }
+
+  variable_set_ids = [
+    local.aws_credentials["integration"],
+    module.variable-set-common.id,
+    module.variable-set-integration.id
+  ]
+}
+
+module "release-staging" {
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
+
+  organization        = var.organization
+  workspace_name      = "release-staging"
+  workspace_desc      = "Manages IAM roles and policies for the Release app"
+  workspace_tags      = ["staging", "release", "aws"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/deployments/release/"
+  trigger_patterns    = ["/terraform/deployments/release/**/*"]
+  global_remote_state = true
+
+  project_name = "govuk-infrastructure"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Production" = "write"
+  }
+
+  variable_set_ids = [
+    local.aws_credentials["staging"],
+    module.variable-set-common.id,
+    module.variable-set-staging.id
+  ]
+}
+
 module "release-production" {
   source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
 
