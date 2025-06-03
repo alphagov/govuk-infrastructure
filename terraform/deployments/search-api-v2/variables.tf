04c7a5d2a3829ec7e47885e4c434b383b0467bae
Christian Sutter <git@doublesignal.com>
2025-06-03 14:12:57 +0100

    Search API v2: Fix/remove outdated comments

--- terraform/deployments/search-api-v2/variables.tf
+++ terraform/deployments/search-api-v2/variables.tf
@@ -40,15 +40,13 @@ variable "aws_region" {
 variable "discovery_engine_api_version" {
   type        = string
   description = "The version of the Discovery Engine API to use, e.g. v1alpha"
-  # Defaulting to `v1alpha` as `v1beta` and `v1` APIs don't support datastore creation yet (as of
-  # October 2023)
+  # Defaulting to `v1alpha` as `v1beta` and `v1` APIs don't support all required features yet
+  # (e.g. `completionConfig` management as of Jun 2025)
   default = "v1alpha"
 }
 
 variable "discovery_engine_location" {
   type        = string
   description = "GCP location to create Discovery Engine Datastore instance in, e.g. global"
-  # As of October 2023, we must use `global` as some event-related features are only available
-  # there, but this may change before going live
-  default = "global"
+  default     = "global"
 }
