7ea18ac7b244684ad22eea44a691a1dff303ac79
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-06-10 07:15:38 +0100

    Create a DNS zone for a subdomain of publishing.service.gov.uk in each environment
    We want to delegate control of these subdomains to other pieces of Terraform.

--- terraform/deployments/root-dns/root_dns_zones.tf
+++ terraform/deployments/root-dns/root_dns_zones.tf
@@ -1,3 +1,6 @@
+locals {
+  publishing_subdomain = var.govuk_environment == "production" ? "" : "${var.govuk_environment}."
+}
 resource "aws_route53_zone" "internal_zone" {
   name = "${var.govuk_environment}.govuk-internal.digital."
 
@@ -8,4 +11,8 @@ resource "aws_route53_zone" "internal_zone" {
 
 resource "aws_route53_zone" "external_zone" {
   name = "${var.govuk_environment}.govuk.digital."
+}
+
+resource "aws_route53_zone" "publishing_subdomain" {
+  name = "${local.publishing_subdomain}publishing.service.gov.uk"
 }
