a34cfa2854ea69a12a5538459125e56713426907
Christian Sutter <git@doublesignal.com>
2025-06-16 11:47:24 +0100

    Search API v2: Delete legacy `govuk` engine
    This engine is now no longer used, and can be deleted (including its
    default serving config that has already been removed from state).

--- terraform/deployments/search-api-v2/serving_config_default.tf
+++
@@ -1,8 +0,0 @@
-# TODO: This was created implicitly on the `govuk` engine when it was first created, so
-# we're just removing it from state in case explicitly deleting it has weird side effects.
-removed {
-  from = module.serving_config_default
-  lifecycle {
-    destroy = false
-  }
-}
