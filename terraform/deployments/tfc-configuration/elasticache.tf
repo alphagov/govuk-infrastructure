0e7585f673494c0cd8de093bf585dadf0a0f2ec6
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-06-19 13:25:37 +0100

    Create `elasticache-production` workspace in TFC
    To deploy stand-alone ElastiCache instances for services in production

--- terraform/deployments/tfc-configuration/elasticache.tf
+++ terraform/deployments/tfc-configuration/elasticache.tf
@@ -63,3 +63,35 @@ module "elasticache-staging" {
     module.variable-set-elasticache-staging.id
   ]
 }
+
+module "elasticache-production" {
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
+
+  organization        = var.organization
+  workspace_name      = "elasticache-production"
+  workspace_desc      = "Serverless ElastiCache instances"
+  workspace_tags      = ["production", "elasticache", "aws"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/deployments/elasticache/"
+  trigger_patterns    = ["/terraform/deployments/elasticache/**/*"]
+  global_remote_state = true
+
+  project_name = "govuk-infrastructure"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Production" = "write"
+  }
+
+  variable_set_ids = [
+    local.aws_credentials["production"],
+    module.variable-set-common.id,
+    module.variable-set-production.id,
+    module.variable-set-elasticache-production.id
+  ]
+}
--- terraform/deployments/tfc-configuration/variables-production.tf
+++ terraform/deployments/tfc-configuration/variables-production.tf
@@ -588,3 +588,34 @@ module "variable-set-amazonmq-production" {
     amazonmq_govuk_chat_retry_message_ttl = 300000
   }
 }
+
+module "variable-set-elasticache-production" {
+  source = "./variable-set"
+
+  name = "elasticache-production"
+
+  # The configuration of the "caches" variable is in
+  # "govuk-infrastructure/terraform/deployments/elasticache/variables.tf".
+  # Only the name and description are required, the rest of the parameters
+  # are optional, with their defaults as follows:
+  #   name                       = required
+  #   description                = required
+  #   num_cache_clusters         = optional (default = "1")
+  #   node_type                  = optional (default = "cache.t4g.small")
+  #   automatic_failover_enabled = optional (default = false)
+  #   multi_az_enabled           = optional (default = false)
+  #   engine                     = optional (default = "valkey")
+  #   engine_version             = optional (default = "8.0")
+  #   family                     = optional (default = "valkey8")
+  # If any further parameters need to be modified in the
+  # elasticache parameter group, they need to be configured here
+  # for them to take effect, otherwise the values will be ignored:
+  #   parameters = {
+  #     maxmemory-policy = optional (default = "noeviction")
+  #   }
+
+  tfvars = {
+    caches = {
+    }
+  }
+}
