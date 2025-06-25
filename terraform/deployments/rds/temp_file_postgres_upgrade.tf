ecfa19db467a4ad7dea39b4919a79115c025e320
nimalank7 <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-06-25 11:28:52 +0100

    Set apply to pending reboot for postgres14 green params
    Description:
    - Terraform sets `apply_method` to `apply immediately` which won't work for static parameters which require a DB reboot
    - This is to satisfy the API as at this moment the DB parameter group is created not the green RDS instance
    - https://github.com/alphagov/govuk-infrastructure/issues/2326

--- terraform/deployments/rds/temp_file_postgres_upgrade.tf
+++ terraform/deployments/rds/temp_file_postgres_upgrade.tf
@@ -4,18 +4,21 @@ resource "aws_db_parameter_group" "postgresql14_green_params" {
   family      = "postgres14"
 
   parameter {
-    name  = "rds.logical_replication"
-    value = "1"
+    name         = "rds.logical_replication"
+    value        = "1"
+    apply_method = "pending-reboot"
   }
 
   parameter {
-    name  = "max_logical_replication_workers"
-    value = "20"
+    name         = "max_logical_replication_workers"
+    value        = "20"
+    apply_method = "pending-reboot"
   }
 
   parameter {
-    name  = "max_worker_processes"
-    value = "25"
+    name         = "max_worker_processes"
+    value        = "25"
+    apply_method = "pending-reboot"
   }
 
   lifecycle { create_before_destroy = true }
