ff3eca2ecd336e3bfc4713d16ec2b511f25faa2e
nimalank7 <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-06-26 12:50:59 +0100

    Remove logical replication parameters for ckan integration postgresql14
    Description:
    - Now that the databases have been switched over and the postgres14 database imported into Terraform we can remove the logical replication parameters and turn off backups
    - https://github.com/alphagov/govuk-infrastructure/issues/2326

--- terraform/deployments/rds/temp_file_postgres_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["ckan"]
-  id = "ckan-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["ckan"]
-  id = "integration-ckan-postgres-20250625103740826800000001"
-}
