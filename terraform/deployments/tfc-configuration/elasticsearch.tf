3ae60177ffcc751e817ffde4973c6bb37ffb9ded
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2025-06-27 17:03:38 +0100

    Add elasticsearch configuration for production

--- terraform/deployments/tfc-configuration/elasticsearch.tf
+++ terraform/deployments/tfc-configuration/elasticsearch.tf
@@ -62,3 +62,34 @@ module "elasticsearch-staging" {
   ]
 }
 
+module "elasticsearch-production" {
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
+
+  organization      = var.organization
+  workspace_name    = "elasticsearch-production"
+  workspace_desc    = "This module manages AWS resources for creating an Elasticsearch cluster."
+  workspace_tags    = ["production", "elasticsearch", "aws"]
+  terraform_version = var.terraform_version
+  execution_mode    = "remote"
+  working_directory = "/terraform/deployments/elasticsearch/"
+  trigger_patterns  = ["/terraform/deployments/elasticsearch/**/*"]
+
+  project_name = "govuk-infrastructure"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Non-Production (r/o)" = "write"
+    "GOV.UK Production"           = "write"
+  }
+
+  variable_set_ids = [
+    local.aws_credentials["production"],
+    module.variable-set-common.id,
+    module.variable-set-production.id,
+    module.variable-set-elasticsearch-production.id
+  ]
+}
