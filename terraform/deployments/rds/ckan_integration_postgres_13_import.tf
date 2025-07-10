33444d669d82c4faa8a68498e02385b38c584778
Murilo Dal Ri <murilodalri@gmail.com>
2025-07-10 09:55:02 +0100

    Remove CKAN PostgreSQL 13 imports

--- terraform/deployments/rds/ckan_integration_postgres_13_import.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["ckan"]
-  id = "ckan-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["ckan"]
-  id = "integration-ckan-postgres-20250708160016009000000001"
-}
