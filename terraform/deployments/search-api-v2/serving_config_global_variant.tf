86c8329e4b14a6bc974cddaa667677762951faa7
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-07-10 14:28:32 +0100

    Remove differences between variant and default
    The only control unique to the variant control should be
    the freshness boost

--- terraform/deployments/search-api-v2/serving_config_global_variant.tf
+++ terraform/deployments/search-api-v2/serving_config_global_variant.tf
@@ -7,7 +7,6 @@ module "serving_config_global_variant" {
 
   boost_control_ids = [
     # specific to serving_config_global_variant
-    module.control_global_boost_demote_historic.id,
     module.control_global_boost_freshness_general.id,
 
     # identical to serving_config_global_default
@@ -16,9 +15,7 @@ module "serving_config_global_variant" {
     module.control_global_boost_demote_low.id,
     module.control_global_boost_demote_medium.id,
     module.control_global_boost_demote_pages.id,
-
-    # explicitly not included in serving_config_global_variant
-    # module.control_global_boost_demote_strong.id,
+    module.control_global_boost_demote_strong.id
   ]
   filter_control_ids = [
     # identical to serving_config_global_default
@@ -28,21 +25,6 @@ module "serving_config_global_variant" {
     # identical to serving_config_global_default
     module.control_global_synonym_hmrc.id,
   ]
-}
-
-module "control_global_boost_demote_historic" {
-  source = "./modules/control"
-
-  id           = "boost_demote_historic"
-  display_name = "Boost: Demote historic"
-  engine_id    = google_discovery_engine_search_engine.govuk_global.engine_id
-  action = {
-    boostAction = {
-      filter     = "is_historic = 1",
-      fixedBoost = -0.25
-      dataStore  = google_discovery_engine_data_store.govuk_content.name
-    }
-  }
 }
 
 module "control_global_boost_freshness_general" {
