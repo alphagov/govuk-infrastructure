5121e5fe8c888645d5dcf84d0b552b13ea7b1f0a
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-07-15 13:59:46 +0100

    Use a single provider for AWS Access Analyzers

---
+++ terraform/deployments/vpc/moved.tf
@@ -0,0 +1,85 @@
+moved {
+  from = aws_accessanalyzer_analyzer.ap-northeast-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["ap-northeast-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.ap-northeast-2[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["ap-northeast-2"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.ap-northeast-3[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["ap-northeast-3"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.ap-south-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["ap-south-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.ap-southeast-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["ap-southeast-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.ap-southeast-2[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["ap-southeast-2"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.ca-central-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["ca-central-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.eu-central-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["eu-central-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.eu-north-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["eu-north-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.eu-west-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["eu-west-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.eu-west-2[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["eu-west-2"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.eu-west-3[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["eu-west-3"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.sa-east-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["sa-east-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.us-east-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["us-east-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.us-east-2[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["us-east-2"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.us-west-1[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["us-west-1"]
+}
+
+moved {
+  from = aws_accessanalyzer_analyzer.us-west-2[0]
+  to   = aws_accessanalyzer_analyzer.analyzer["us-west-2"]
+}
+
