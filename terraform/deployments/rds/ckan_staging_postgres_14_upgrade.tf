be95fefa3fa6902fa2a53a3b1a99d464d825f5b6
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2025-07-18 14:19:02 +0100

    Delete ckan staging postgres upgrade file

--- terraform/deployments/rds/ckan_staging_postgres_14_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["ckan"]
-  id = "ckan-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["ckan"]
-  id = "staging-ckan-postgres-20250704093728599800000001"
-}
