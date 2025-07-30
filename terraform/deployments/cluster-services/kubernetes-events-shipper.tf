02387f581c9dc01beb1c6ee648ec7f8a95b92dfb
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2025-07-30 12:18:39 +0100

    Deploy kubernetes events shipper into cluster-services

---
+++ terraform/deployments/cluster-services/kubernetes-events-shipper.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "kubernetes_events_shipper" {
+  depends_on = [helm_release.cluster_secrets]
+
+  chart      = "kubernetes-events-shipper"
+  name       = "kubernetes-events-shipper"
+  namespace  = local.services_ns
+  repository = "https://alphagov.github.io/govuk-helm-charts/"
+  version    = "1.0.0" # TODO: Dependabot or equivalent so this doesn't get neglected.
+  timeout    = var.helm_timeout_seconds
+}
