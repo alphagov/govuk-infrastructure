f31666e74cfcc06f8565e9398230ca716cc58a41
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2025-08-13 13:54:46 +0100

    ✨ Add default tagging options to cluster-infrastructure terraform
    Standardise AWS resource tagging for cluster-infrastructure deployment:
    - Change product from "GOV.UK" to "govuk"
    - Change system from "EKS cluster infrastructure" to "govuk-platform-engineering"
    - Add service tag "eks" for better resource identification
    - Convert tag keys to lowercase (environment, owner)
    - Convert terraform_deployment to terraform-deployment (lowercase with hyphen)
    - Remove cluster tag and maintain repository tag

--- terraform/deployments/cluster-infrastructure/main.tf
+++ terraform/deployments/cluster-infrastructure/main.tf
@@ -119,13 +119,13 @@ provider "aws" {
   region = "eu-west-1"
   default_tags {
     tags = {
-      Product              = "GOV.UK"
-      System               = "EKS cluster infrastructure"
-      Environment          = var.govuk_environment
-      Owner                = "govuk-platform-engineering@digital.cabinet-office.gov.uk"
-      cluster              = var.cluster_name
+      product              = "govuk"
+      system               = "govuk-platform-engineering"
+      service              = "eks"
+      environment          = var.govuk_environment
+      owner                = "govuk-platform-engineering@digital.cabinet-office.gov.uk"
       repository           = "govuk-infrastructure"
-      terraform_deployment = basename(abspath(path.root))
+      terraform-deployment = basename(abspath(path.root))
     }
   }
 }
