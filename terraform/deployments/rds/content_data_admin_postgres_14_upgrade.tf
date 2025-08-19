7002d65677a6a46c631fe391da2fe0185a24f949
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-08-19 14:14:47 +0100

    Production: import PostgreSQL 14 content-data-admin database

--- terraform/deployments/rds/content_data_admin_postgres_14_upgrade.tf
+++ terraform/deployments/rds/content_data_admin_postgres_14_upgrade.tf
@@ -1,25 +1,9 @@
-resource "aws_db_parameter_group" "content_data_admin_postgresql_14_green_params" {
+import {
+  to = aws_db_instance.instance["content_data_admin"]
+  id = "content-data-admin-postgres"
+}
 
-  name_prefix = "${var.govuk_environment}-content-data-admin-postgres-"
-  family      = "postgres14"
-
-  parameter {
-    name         = "rds.logical_replication"
-    value        = "1"
-    apply_method = "pending-reboot"
-  }
-
-  parameter {
-    name         = "max_logical_replication_workers"
-    value        = "20"
-    apply_method = "pending-reboot"
-  }
-
-  parameter {
-    name         = "max_worker_processes"
-    value        = "25"
-    apply_method = "pending-reboot"
-  }
-
-  lifecycle { create_before_destroy = true }
+import {
+  to = aws_db_parameter_group.engine_params["content_data_admin"]
+  id = "production-content-data-admin-postgres-20250819094634183300000001"
 }
