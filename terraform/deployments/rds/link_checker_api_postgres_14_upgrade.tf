c581ce7fbba912ba469a6482c5442c23dacffa13
nimalank7 <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-08-26 16:01:23 +0100

    Import integration link-checker-api postgres14 database into Terraform
    Description:
    - Imports the link-checker-api postgres14 RDS instance and postgres14 parameter group into Terraform
    - Bumps the engine version to 14.18 in integration in code

--- terraform/deployments/rds/link_checker_api_postgres_14_upgrade.tf
+++ terraform/deployments/rds/link_checker_api_postgres_14_upgrade.tf
@@ -1,25 +1,9 @@
-resource "aws_db_parameter_group" "link_checker_api_postgresql_14_green_params" {
+import {
+  to = aws_db_instance.instance["link_checker_api"]
+  id = "linker-checker-api-postgres"
+}
 
-  name_prefix = "${var.govuk_environment}-link-checker-api-postgres-"
-  family      = "postgres14"
-
-  parameter {
-    name         = "rds.logical_replication"
-    value        = "1"
-    apply_method = "pending-reboot"
-  }
-
-  parameter {
-    name         = "max_logical_replication_workers"
-    value        = "20"
-    apply_method = "pending-reboot"
-  }
-
-  parameter {
-    name         = "max_worker_processes"
-    value        = "25"
-    apply_method = "pending-reboot"
-  }
-
-  lifecycle { create_before_destroy = true }
+import {
+  to = aws_db_parameter_group.engine_params["link_checker_api"]
+  id = "integration-link-checker-api-postgres-20250826130107426000000001"
 }
