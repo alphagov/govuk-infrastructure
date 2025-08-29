088553503b7c236b074775070d1330a5ad1179ae
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2025-08-29 13:35:14 +0100

    Add skeleton cluster-access TF root and TFC workspaces

---
+++ terraform/deployments/cluster-access/remote.tf
@@ -0,0 +1,12 @@
+locals {
+  env_suffix = startswith(var.govuk_environment, "eph-") ? "ephemeral" : "${var.govuk_environment}"
+}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "tfe_outputs" "cluster_infrastructure" {
+  organization = "govuk"
+  workspace    = "cluster-infrastructure-${var.govuk_environment}"
+}
