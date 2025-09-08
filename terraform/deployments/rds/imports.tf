d28233335111c425a198012d4114457ddf3ae976
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-09-08 14:42:57 +0100

    Remove logical replication params for link_checker_api staging and production
    Description:
    - `link-checker-api` is imported in staging and production into Terraform so we can remove logical replication (staging and prod) and turn off backups (staging)

--- terraform/deployments/rds/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["link_checker_api"]
-  id = "link-checker-api-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["link_checker_api"]
-  id = "staging-link-checker-api-postgres-20250827092600496900000002"
-}
