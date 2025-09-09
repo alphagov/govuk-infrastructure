27fdbc43988da106c57e486043ab2137f6ea1065
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-09-09 10:56:23 +0100

    Remove logical replication and backups for staging transition
    Description:
    - https://github.com/alphagov/govuk-infrastructure/pull/2757 imported staging transition into Terraform
    - Remove logical replication as it's no longer required
    - Turn off backups as they are no longer required

--- terraform/deployments/rds/staging_transition_postgres_14_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["transition"]
-  id = "transition-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["transition"]
-  id = "staging-transition-postgres-20250908163308605600000001"
-}
