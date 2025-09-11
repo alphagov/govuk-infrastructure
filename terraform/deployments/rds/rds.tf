ada93c0ddb7ecb9ee5d962c1b7711b990ebe79fd
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2025-09-11 15:44:04 +0100

    Default to encrypted at rest RDS databases

--- terraform/deployments/rds/rds.tf
+++ terraform/deployments/rds/rds.tf
@@ -89,8 +89,8 @@ resource "aws_db_instance" "instance" {
   final_snapshot_identifier = "${each.value.name}-final-snapshot"
   skip_final_snapshot       = var.skip_final_snapshot
 
-  storage_encrypted = try(each.value.encryption_at_rest, false)
-  kms_key_id        = try(each.value.encryption_at_rest, false) ? aws_kms_alias.rds.name : null
+  storage_encrypted = try(each.value.encryption_at_rest, true)
+  kms_key_id        = try(each.value.encryption_at_rest, true) ? aws_kms_alias.rds.name : null
 
   tags = { Name = "govuk-rds-${each.value.name}-${each.value.engine}", project = lookup(each.value, "project", "GOV.UK - Other") }
 }
