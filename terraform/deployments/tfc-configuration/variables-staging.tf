3716e103c2857a9dc648096f279e3354efc71ef1
Jonathan Harden <jfharden@gmail.com>
2025-09-17 11:57:07 +0100

    Revert "Create encrypted snapshot of chat in staging & disable deletion protection"

--- terraform/deployments/tfc-configuration/variables-staging.tf
+++ terraform/deployments/tfc-configuration/variables-staging.tf
@@ -227,8 +227,6 @@ module "variable-set-rds-staging" {
         performance_insights_enabled = false
         project                      = "GOV.UK - AI"
         encryption_at_rest           = false
-        create_encrypted_snapshot    = true
-        deletion_protection          = false
       }
 
       ckan = {
