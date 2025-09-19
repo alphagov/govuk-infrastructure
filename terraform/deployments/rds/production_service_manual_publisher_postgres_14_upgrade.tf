7d7f214a54d77fe0e823d299a1735923ed711879
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-09-19 15:27:03 +0100

    Import service_manual_publisher postgres14 to Terraform in production
    We are upgrading the DB.

---
+++ terraform/deployments/rds/production_service_manual_publisher_postgres_14_upgrade.tf
@@ -0,0 +1,9 @@
+import {
+  to = aws_db_instance.instance["service_manual_publisher"]
+  id = "service-manual-publisher-postgres"
+}
+
+import {
+  to = aws_db_parameter_group.engine_params["service_manual_publisher"]
+  id = "production-service-manual-publisher-postgres-20250917085919426400000001"
+}
