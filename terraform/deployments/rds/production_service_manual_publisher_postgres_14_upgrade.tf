30f8d117c55d63e3a56287f10f5d840ba46319d4
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-09-19 15:43:55 +0100

    Import ckan aws_db_parameter_group
    It was accidentally deleted.

--- terraform/deployments/rds/production_service_manual_publisher_postgres_14_upgrade.tf
+++ terraform/deployments/rds/production_service_manual_publisher_postgres_14_upgrade.tf
@@ -7,3 +7,8 @@ import {
   to = aws_db_parameter_group.engine_params["service_manual_publisher"]
   id = "production-service-manual-publisher-postgres-20250917085919426400000001"
 }
+
+import {
+  to = aws_db_parameter_group.engine_params["ckan"]
+  id = "production-ckan-postgres-20250721153103318500000001"
+}
