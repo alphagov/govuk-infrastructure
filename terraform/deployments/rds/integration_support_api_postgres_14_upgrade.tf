44e101856ecbff94d7163b8bc6033996353e18ec
David Mays <david.mays@digital.cabinet-office.gov.uk>
2025-10-01 16:52:36 +0100

    Import Integration Support API RDS Instance

--- terraform/deployments/rds/integration_support_api_postgres_14_upgrade.tf
+++ terraform/deployments/rds/integration_support_api_postgres_14_upgrade.tf
@@ -1,25 +1,9 @@
-resource "aws_db_parameter_group" "support_api_postgresql_14_green_params" {
+import {
+  to = aws_db_instance.instance["support_api"]
+  id = "support-api-postgres"
+}
 
-  name_prefix = "${var.govuk_environment}-support-api-postgres-"
-  family      = "postgres14"
-
-  parameter {
-    name         = "rds.logical_replication"
-    value        = "1"
-    apply_method = "pending-reboot"
-  }
-
-  parameter {
-    name         = "max_logical_replication_workers"
-    value        = "20"
-    apply_method = "pending-reboot"
-  }
-
-  parameter {
-    name         = "max_worker_processes"
-    value        = "25"
-    apply_method = "pending-reboot"
-  }
-
-  lifecycle { create_before_destroy = true }
+import {
+  to = aws_db_parameter_group.engine_params["support_api"]
+  id = "integration-support-api-postgres-20250826151928116600000002"
 }
