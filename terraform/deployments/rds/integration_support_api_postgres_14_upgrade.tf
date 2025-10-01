6aa308f2cdc7b65d63dcf38003893f7d84f83780
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-10-01 17:12:36 +0100

    Add staging support-api postgres14 params
    Description:
    - https://github.com/alphagov/govuk-infrastructure/pull/2913 imported the support-api Postgres 14 database so now we can remove the logical replication parameters
    - Add the staging support-api postgres14 parameter group
    - https://github.com/alphagov/govuk-infrastructure/issues/2907

--- terraform/deployments/rds/integration_support_api_postgres_14_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["support_api"]
-  id = "support-api-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["support_api"]
-  id = "integration-support-api-postgres-20250826151928116600000002"
-}
