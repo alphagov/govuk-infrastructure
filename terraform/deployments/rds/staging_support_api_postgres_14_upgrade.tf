f6b9f0ac855c3ec7d67a0ffec7cd8646c7b7352b
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-10-08 15:33:56 +0100

    Add production `support-api` postgres14 parameter group
    Description:
    - Add the Postgres14 parameter group for `support-api`
    - https://github.com/alphagov/govuk-infrastructure/issues/2907

--- terraform/deployments/rds/staging_support_api_postgres_14_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["support_api"]
-  id = "support-api-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["support_api"]
-  id = "staging-support-api-postgres-20250827092600496800000001"
-}
