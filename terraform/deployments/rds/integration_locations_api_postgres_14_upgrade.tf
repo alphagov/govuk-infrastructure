07064e755451920332a4e922a9bd88b2bab7c4c9
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-10-20 16:44:15 +0100

    Add staging `locations-api` Postgres 14 parameters
    Description:
    - Create `locations-api` parameter group for Postgres 14 upgrade
    - https://github.com/alphagov/govuk-infrastructure/issues/3004

--- terraform/deployments/rds/integration_locations_api_postgres_14_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["locations_api"]
-  id = "locations-api-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["locations_api"]
-  id = "integration-locations-api-postgres-20250828113749103400000006"
-}
