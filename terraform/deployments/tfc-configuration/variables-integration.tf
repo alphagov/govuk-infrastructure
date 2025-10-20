b0832ce4147f9fa516c807bb6de61e37c5e37d28
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-10-20 15:59:46 +0100

    Bump `locations-api` parameter group to Postgres 14
    Description:
    - Bumps `locations-api` parameter group to Postgres 14
    - https://github.com/alphagov/govuk-infrastructure/issues/3004

--- terraform/deployments/tfc-configuration/variables-integration.tf
+++ terraform/deployments/tfc-configuration/variables-integration.tf
@@ -493,7 +493,7 @@ module "variable-set-rds-integration" {
 
       locations_api = {
         engine         = "postgres"
-        engine_version = "13"
+        engine_version = "14"
         engine_params = {
           log_min_duration_statement = { value = 10000 }
           log_statement              = { value = "all" }
@@ -516,7 +516,7 @@ module "variable-set-rds-integration" {
             apply_method = "pending-reboot"
           }
         }
-        engine_params_family         = "postgres13"
+        engine_params_family         = "postgres14"
         name                         = "locations-api"
         allocated_storage            = 1000
         instance_class               = "db.m6g.large"
