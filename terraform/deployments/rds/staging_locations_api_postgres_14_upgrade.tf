0d5b417b34a8fad51ea52acf074dd5062bc59a32
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-10-21 11:06:45 +0100

    Add `locations-api` Postgres 14 parameter group for production
    Description:
    - Add `locations-api` Postgres 14 parameter group for production
    - https://github.com/alphagov/govuk-infrastructure/issues/3004

--- terraform/deployments/rds/staging_locations_api_postgres_14_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["locations_api"]
-  id = "locations-api-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["locations_api"]
-  id = "staging-locations-api-postgres-20250828115303735800000005"
-}
