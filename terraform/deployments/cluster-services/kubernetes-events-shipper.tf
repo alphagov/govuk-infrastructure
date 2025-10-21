da08385b50e1fef6ebefd984658710b55cb2904f
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-10-21 11:36:00 +0100

    Bump `kubernetes-event-shipper` Helm chart to 1.1.1
    Description:
    - Bumps `kubernetes-event-shipper` Helm chart to 1.1.1
    - See https://github.com/alphagov/govuk-helm-charts/pull/3626

--- terraform/deployments/cluster-services/kubernetes-events-shipper.tf
+++ terraform/deployments/cluster-services/kubernetes-events-shipper.tf
@@ -7,6 +7,6 @@ resource "helm_release" "kubernetes_events_shipper" {
   name       = "kubernetes-events-shipper"
   namespace  = local.services_ns
   repository = "https://alphagov.github.io/govuk-helm-charts/"
-  version    = "1.1.0"
+  version    = "1.1.1"
   timeout    = var.helm_timeout_seconds
 }
