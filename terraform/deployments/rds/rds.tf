bc9d542d706804b63231b788e003ebfd6936c839
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2025-10-23 15:01:00 +0100

    Create new security groups (and rules) for normalised RDS instances

--- terraform/deployments/rds/rds.tf
+++ terraform/deployments/rds/rds.tf
@@ -148,7 +148,7 @@ resource "aws_db_instance" "normalised_instance" {
   copy_tags_to_snapshot       = true
   monitoring_interval         = 60
   monitoring_role_arn         = data.tfe_outputs.logging.nonsensitive_values.rds_enhanced_monitoring_role_arn
-  vpc_security_group_ids      = [aws_security_group.rds[each.key].id]
+  vpc_security_group_ids      = [aws_security_group.normalised_rds[each.key].id]
   ca_cert_identifier          = "rds-ca-rsa2048-g1"
   apply_immediately           = var.govuk_environment != "production"
   allow_major_version_upgrade = lookup(each.value, "allow_major_version_upgrade", false)
--- terraform/deployments/rds/security_groups.tf
+++ terraform/deployments/rds/security_groups.tf
@@ -40,3 +40,55 @@ resource "aws_security_group_rule" "postgres" {
   source_security_group_id = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.node_security_group_id
 
 }
+
+resource "aws_security_group" "normalised_rds" {
+  for_each = {
+    for db_name, db in var.databases : db_name => db
+    if lookup(db, "launch_new_db", false)
+  }
+
+  name        = "${local.identifier_prefix}${lookup(each.value, "new_name", each.value.name)}-${var.govuk_environment}-${each.value.engine}-rds-access"
+  vpc_id      = data.tfe_outputs.vpc.nonsensitive_values.id
+  description = "Access to ${each.value.name} RDS"
+
+  lifecycle { create_before_destroy = true }
+}
+
+resource "aws_security_group_rule" "normalised_rds_mysql" {
+  for_each = {
+    for db_name, db in var.databases : db_name => db
+    if lookup(db, "launch_new_db", false) &&
+    db.engine == "mysql" &&
+    !lookup(db, "isolate_new_db", false)
+  }
+
+  security_group_id = aws_security_group.normalised_rds[each.key].id
+  description       = "Access to MySQL database from EKS worker nodes"
+
+  type      = "ingress"
+  protocol  = "tcp"
+  from_port = 3306
+  to_port   = 3306
+
+  source_security_group_id = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.node_security_group_id
+}
+
+resource "aws_security_group_rule" "normalised_rds_postgres" {
+  for_each = {
+    for db_name, db in var.databases : db_name => db
+    if lookup(db, "launch_new_db", false) &&
+    db.engine == "postgres" &&
+    !lookup(db, "isolate_new_db", false)
+  }
+
+  security_group_id = aws_security_group.normalised_rds[each.key].id
+  description       = "Access to PostgreSQL database from EKS worker nodes"
+
+  type      = "ingress"
+  protocol  = "tcp"
+  from_port = 5432
+  to_port   = 5432
+
+  source_security_group_id = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.node_security_group_id
+
+}
