f58728d95c8fa80f96050fc683638edb2726cc51
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2025-10-29 16:02:49 +0000

    Demote an HMRC specialist manual section.
    This manual section is getting bad feedback because it is being surfaced but
    is generally not what users are looking for. This means we need to demote it.

--- terraform/deployments/search-api-v2/serving_config_global_default.tf
+++ terraform/deployments/search-api-v2/serving_config_global_default.tf
@@ -8,6 +8,7 @@ module "serving_config_global_default" {
   boost_control_ids = [
     module.control_global_boost_demote_low.id,
     module.control_global_boost_demote_medium.id,
+    module.control_global_boost_demote_low_pages.id,
     module.control_global_boost_demote_pages.id,
     module.control_global_boost_demote_strong.id,
     module.control_global_boost_promote_low.id,
@@ -67,8 +68,34 @@ module "control_global_boost_demote_low" {
 }
 
 locals {
+  # Pages to demote by 0.25
+  # We do not have any pages that are demoted by -0.25 yet. This is a placeholder
+  # for when we do. Remove the dummy page when a real page is added.
+  demote_pages_low = [
+    "/this/page/does/not/exist"
+  ]
+  demote_pages_low_expr = join(",", [for page in local.demote_pages_low : "\"${page}\""])
+}
+
+module "control_global_boost_demote_low_pages" {
+  source = "./modules/control"
+
+  id           = "boost_demote_pages_low"
+  display_name = "Boost: Demote specific pages low"
+  engine_id    = google_discovery_engine_search_engine.govuk_global.engine_id
+  action = {
+    boostAction = {
+      filter     = "link: ANY(${local.demote_pages_low_expr})",
+      fixedBoost = -0.25
+      dataStore  = google_discovery_engine_data_store.govuk_content.name
+    }
+  }
+}
+
+locals {
   # Pages to demote by 0.5
   demote_pages_medium = [
+    "/hmrc-internal-manuals/self-assessment-manual/sam100130",
     "/hmrc-internal-manuals/tax-credits-manual/tcm1000248",
     "/hmrc-internal-manuals/tax-credits-manual/tcm1000267",
     "/hmrc-internal-manuals/tax-credits-manual/tcm1000541",
