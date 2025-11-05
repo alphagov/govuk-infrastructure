b7df6666c634dc1f2d013c19db0c4ad77c65e4a2
Murilo Dal Ri <murilodalri@gmail.com>
2025-11-05 13:37:19 +0000

    Remove logical replication parameters for account api staging postgresql14

--- terraform/deployments/rds/staging_account_api_postgres14_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["account_api"]
-  id = "account-api-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["account_api"]
-  id = "staging-account-api-postgres-20250828115303735700000004"
-}
