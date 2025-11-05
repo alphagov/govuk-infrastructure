fe4a41656038c73b2e93412e218bcb06e2631dc1
Murilo Dal Ri <murilodalri@gmail.com>
2025-11-05 13:27:10 +0000

    Import account-api postgres14 staging to Terraform

---
+++ terraform/deployments/rds/staging_account_api_postgres14_upgrade.tf
@@ -0,0 +1,9 @@
+import {
+  to = aws_db_instance.instance["account_api"]
+  id = "account-api-postgres"
+}
+
+import {
+  to = aws_db_parameter_group.engine_params["account_api"]
+  id = "staging-account-api-postgres-20250828115303735700000004"
+}
