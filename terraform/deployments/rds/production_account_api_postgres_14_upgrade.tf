dc349670543d0a6d5fe8066d2ff7d7bb8f702a5b
Murilo Dal Ri <murilodalri@gmail.com>
2025-11-05 13:55:19 +0000

    Add account api psql 14 params

---
+++ terraform/deployments/rds/production_account_api_postgres_14_upgrade.tf
@@ -0,0 +1,25 @@
+resource "aws_db_parameter_group" "account_api_postgresql_14_green_params" {
+
+  name_prefix = "${var.govuk_environment}-account-api-postgres-"
+  family      = "postgres14"
+
+  parameter {
+    name         = "rds.logical_replication"
+    value        = "1"
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "max_logical_replication_workers"
+    value        = "20"
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "max_worker_processes"
+    value        = "25"
+    apply_method = "pending-reboot"
+  }
+
+  lifecycle { create_before_destroy = true }
+}
