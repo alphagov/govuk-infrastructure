3e27f7b90dc7503034ab13040f11f4179aaee099
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-11-11 13:17:54 +0000

    Add `content-tagger` production postgres14 parameter group
    Description:
    - Add `content-tagger` production postgres14 parameter group
    - https://github.com/alphagov/govuk-infrastructure/issues/3108

--- terraform/deployments/rds/staging_content_tagger_postgres_14_upgrade.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_db_instance.instance["content_tagger"]
-  id = "content-tagger-postgres"
-}
-
-import {
-  to = aws_db_parameter_group.engine_params["content_tagger"]
-  id = "staging-content-tagger-postgres-20250828115303735900000007"
-}
