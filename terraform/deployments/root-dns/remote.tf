7e8f686ae98d661278f4919208bd750091323373
David Mays <david.mays@digital.cabinet-office.gov.uk>
2025-12-04 15:37:42 +0000

    Only load the Fastly Data Resource in Prod

--- terraform/deployments/root-dns/remote.tf
+++ terraform/deployments/root-dns/remote.tf
@@ -4,6 +4,9 @@ data "tfe_outputs" "vpc" {
 }
 
 data "tfe_outputs" "fastly_www" {
+  # This is only needed in Production
+  count = var.govuk_environment == "production" ? 1 : 0
+
   organization = "govuk"
   workspace    = "govuk-fastly-www-${var.govuk_environment}"
 }
