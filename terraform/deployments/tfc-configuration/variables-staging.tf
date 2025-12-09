231bd8a057d44e9a52c39eda40ea9b324a8de06c
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2025-12-09 18:06:36 +0000

    Need to include search-admin

--- terraform/deployments/tfc-configuration/variables-staging.tf
+++ terraform/deployments/tfc-configuration/variables-staging.tf
@@ -653,10 +653,10 @@ module "variable-set-rds-staging" {
         backup_window                = "08:00-08:30"
         auto_minor_version_upgrade   = false
         prepare_to_launch_new_db     = false
-        isolate                      = false
+        isolate                      = true
         launch_new_db                = false
-        cname_point_to_new_instance  = false
         new_db_deletion_protection   = false
+        cname_point_to_new_instance  = false
       }
 
       service_manual_publisher = {
