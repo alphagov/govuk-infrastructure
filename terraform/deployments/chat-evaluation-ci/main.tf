8f4e9f19089160a862951792a5b8ac3311089dfc
Chae Cramb <chae.cramb@digital.cabinet-office.gov.uk>
2025-12-18 09:08:19 +0000

    Add GitHub Actions OIDC role for govuk-chat-evaluation Bedrock CI
    Add new `chat-ci` Terraform deployment that creates:
    
    - GitHub Actions OIDC provider in the govuk-test AWS account
    - IAM role assumable by `alphagov/govuk-chat-evaluation` workflows
    - Bedrock invoke policy scoped to `openai.gpt-oss-120b-1:0` model
    
    This allows govuk-chat-evaluation CI to run `real_bedrock` pytest-marked
    tests by authenticating via OIDC and invoking the Bedrock model in
    eu-west-1.
    
    DeepEval uses the Converse API rather than Invoke model, however to call
    this permission for the `bedrock:InvokeModel` operation is required.[^1]
    
    Also adds the `chat-ci` workspace configuration in tfc-configuration.
    
    [^1]: https://docs.aws.amazon.com/bedrock/latest/userguide/conversation-inference-call.html

---
+++ terraform/deployments/chat-evaluation-ci/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  cloud {
+    organization = "govuk"
+    workspaces {
+      tags = ["chat-evaluation-ci", "test", "aws"]
+    }
+  }
+
+  required_version = "~> 1.10"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      product              = "govuk"
+      system               = "govuk-chat-evaluation"
+      environment          = var.govuk_environment
+      owner                = "govuk-platform-engineering@digital.cabinet-office.gov.uk"
+      repository           = "govuk-infrastructure"
+      terraform-deployment = basename(abspath(path.root))
+      Service              = "govuk-chat-evaluation-ci"
+    }
+  }
+}
