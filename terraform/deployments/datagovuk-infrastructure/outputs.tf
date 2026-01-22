0b7d975f69ffe7f66db2b0bcbbdea84c49f6dba9
Amar Puttana <amaranatha.puttana@digital.cabinet-office.gov.uk>
2026-01-22 15:14:01 +0000

    feat/dguk-158: Added WAF rate limits for CKAN

--- terraform/deployments/datagovuk-infrastructure/outputs.tf
+++ terraform/deployments/datagovuk-infrastructure/outputs.tf
@@ -23,3 +23,27 @@ output "find_waf_rate_limits" {
     block       = local.waf_rate_limits.block
   }
 }
+output "ckan_waf_arn" {
+  description = "ARN of the CKAN WAF Web ACL"
+  value       = aws_wafv2_web_acl.ckan.arn
+}
+output "ckan_waf_id" {
+  description = "ID of the CKAN WAF Web ACL"
+  value       = aws_wafv2_web_acl.ckan.id
+}
+output "ckan_alb_arn" {
+  description = "ARN of the CKAN ALB"
+  value       = data.aws_lb.ckan.arn
+}
+output "ckan_cloudwatch_log_group" {
+  description = "CloudWatch CKAN Log Group for WAF logs"
+  value       = aws_cloudwatch_log_group.ckan_waf.name
+}
+output "ckan_waf_rate_limits" {
+  description = "Current rate limit configuration for CKAN WAF"
+  value = {
+    environment = var.govuk_environment
+    warning     = local.ckan_rate_limits.warning
+    block       = local.ckan_rate_limits.block
+  }
+}
