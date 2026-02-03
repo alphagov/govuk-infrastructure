cecd136b1849ee9f9d5a5447172c05a6abd74944
Jaskaran Sarkaria <jaskaran.sarkaria@digital.cabinet-office.gov.uk>
2026-02-03 09:33:05 +0000

    fix: 🐛 falco into privileged ns

--- terraform/deployments/cluster-services/falco.tf
+++ terraform/deployments/cluster-services/falco.tf
@@ -1,9 +1,27 @@
+resource "kubernetes_namespace_v1" "falco" {
+  metadata {
+    name = "falco"
+    annotations = {
+      "argocd.argoproj.io/sync-options" = "ServerSideApply=true"
+    }
+    labels = {
+      "app.kubernetes.io/managed-by"  = "Terraform"
+      "argocd.argoproj.io/managed-by" = "cluster-services"
+      # https://kubernetes-sigs.github.io/aws-load-balancer-controller/latest/deploy/pod_readiness_gate/
+      "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled"
+      "pod-security.kubernetes.io/audit"        = "privileged"
+      "pod-security.kubernetes.io/enforce"      = "privileged"
+      "pod-security.kubernetes.io/warn"         = "privileged"
+    }
+  }
+}
+
 resource "helm_release" "falco" {
-  name             = "flaco"
+  name             = "falco"
   repository       = "https://falcosecurity.github.io/charts"
   chart            = "falco"
   version          = "8.0.0"
-  namespace        = local.services_ns
+  namespace        = "falco"
   create_namespace = false
 
   values = [yamlencode({
@@ -14,5 +32,7 @@ resource "helm_release" "falco" {
       }
     ]
   })]
+
+  depends_on = [kubernetes_namespace_v1.falco]
 }
 
