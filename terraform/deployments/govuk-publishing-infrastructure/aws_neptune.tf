4dc550160b6175fa3e43cf447824aad5e175f529
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2026-02-09 09:40:59 +0000

    Skeleton file for neptune, WIP

--- terraform/deployments/govuk-publishing-infrastructure/aws_neptune.tf
+++ terraform/deployments/govuk-publishing-infrastructure/aws_neptune.tf
@@ -1,4 +1,5 @@
-resource "aws_neptune_cluster" "govuk_ai_accelerator" {
+# cluster base defintion
+resource "aws_neptune_cluster" "govuk_ai_accelerator_cluster" {
   cluster_identifier                  = "govuk-ai-accelerator-cluster"
   engine                              = "neptune"
   backup_retention_period             = 5
@@ -9,4 +10,22 @@ resource "aws_neptune_cluster" "govuk_ai_accelerator" {
   iam_roles                           = data.aws_iam_roles.developer.arns
   storage_encrypted                   = true
   vpc_security_group_ids              = [aws_security_group.instance[each.key].id]
+  publicly_accessible                 = false
+  neptune_subnet_group_name           = "TBD"
+}
+
+# instances - 1 reader, 1 writer, inherit from cluster
+resource "aws_neptune_cluster_instance" "govuk_ai_accelerator_instance" {
+  count                               = 2
+  cluster_identifier                  = aws_neptune_cluster.govuk_ai_accelerator_cluster.id
+  engine                              = "neptune"
+  instance_class                      = "db.r4.large"
+  apply_immediately                   = true
+}
+
+# defined entry point to cluster
+resource "aws_neptune_cluster_endpoint" "example" {
+  cluster_identifier          = aws_neptune_cluster.test.cluster_identifier
+  cluster_endpoint_identifier = "example"
+  endpoint_type               = "READER"
 }
