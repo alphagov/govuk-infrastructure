ad4e4add4dbabb46362b15991cacb09e5830996c
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2026-02-09 12:24:21 +0000

    Update import.tf

--- terraform/deployments/github/import.tf
+++ terraform/deployments/github/import.tf
@@ -9,11 +9,6 @@ import {
 }
 
 import {
-  to = github_branch_protection.govuk_repos["govuk-cookie-consent"]
-  id = "govuk-cookie-consent:main"
-}
-
-import {
   to = github_repository.govuk_repos["govuk-cookie-consent"]
   id = "govuk-cookie-consent"
 }
