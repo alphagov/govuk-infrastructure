924274431d6523b932518a5ba2b8dcd8eee28a61
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-02-12 09:15:39 +0000

    IA-2491: create gcp-gds-bq-processing TFC workspace to manage GCP project gds-bq-processing

---
+++ terraform/deployments/gcp-gds-bq-processing/README.md
@@ -0,0 +1,1 @@
+# gcp-gds-bq-processing
--- terraform/deployments/tfc-configuration/gcp-projects.tf
+++ terraform/deployments/tfc-configuration/gcp-projects.tf
@@ -58,3 +58,33 @@ module "gcp-ga4-aggregate-analytics" {
     local.gcp_credentials["production"],
   ]
 }
+
+module "gcp-gds-bq-processing" {
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
+
+  organization        = var.organization
+  workspace_name      = "gcp-gds-bq-processing"
+  workspace_desc      = "GCP project management for the gds-bq-processing production project"
+  workspace_tags      = ["production", "gds-bq-processing", "gcp"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/deployments/gcp-gds-bq-processing/"
+  trigger_patterns    = ["/terraform/deployments/gcp-gds-bq-processing/**/*"]
+  global_remote_state = true
+
+  project_name = "govuk-data-engineering"
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Production"            = "write"
+    "Google Cloud Data Production" = "write"
+  }
+
+  variable_set_ids = [
+    local.gcp_credentials["production"],
+  ]
+}
