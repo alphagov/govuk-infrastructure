0f72ea33ec2572124aba42c1a70f55e6bc53a28a
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-02-16 15:38:03 +0000

    IA-2491: bind analyst group to gds-bq-processing dataform.viewer role

--- terraform/deployments/gcp-gds-bq-processing/project_iam_binding.tf
+++ terraform/deployments/gcp-gds-bq-processing/project_iam_binding.tf
@@ -4,7 +4,6 @@ resource "google_project_iam_binding" "project_owners" {
 
   members = [
     "group:gcp-gds-bq-processing-owners@digital.cabinet-office.gov.uk",
-    "user:ian.ansell@digital.cabinet-office.gov.uk",
     "serviceAccount:terraform-cloud-production@govuk-production.iam.gserviceaccount.com",
   ]
 }
@@ -14,5 +13,20 @@ resource "google_project_iam_binding" "project_editors" {
   role    = "roles/editor"
   members = [
     "group:gcp-gds-bq-processing-editors@digital.cabinet-office.gov.uk",
+  ]
+}
+
+# Creating this role with no members allows terraform to enforce that nobody should have project-wide roles/viewer access.
+resource "google_project_iam_binding" "project_viewers" {
+  project = google_project.project.project_id
+  role    = "roles/viewer"
+  members = []
+}
+
+resource "google_project_iam_binding" "project_dataform_viewers" {
+  project = google_project.project.project_id
+  role    = "roles/dataform.viewer"
+  members = [
+    "group:gcp-gds-bq-processing-analysts@digital.cabinet-office.gov.uk",
   ]
 }
