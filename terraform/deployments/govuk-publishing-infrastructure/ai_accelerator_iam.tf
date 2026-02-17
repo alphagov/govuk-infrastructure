30cb04e0068ea42fde26ee7fb4e855bb3d1f35da
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2026-02-17 15:29:08 +0000

    Added limitations to the resource list for bedrock

--- terraform/deployments/govuk-publishing-infrastructure/ai_accelerator_iam.tf
+++ terraform/deployments/govuk-publishing-infrastructure/ai_accelerator_iam.tf
@@ -7,23 +7,25 @@ data "aws_iam_policy_document" "govuk_ai_accelerator_bedrock_access" {
       "bedrock:ListFoundationModels"
     ]
     effect    = "Allow"
-    resources = ["*"]
+    resources = ["arn:aws:bedrock:eu-west-1:${data.aws_caller_identity.current.account_id}:*"]
   }
 }
 
 data "aws_iam_policy_document" "govuk_ai_accelerator_s3_access" {
   statement {
+    sid = "GovukAiAcceleratorS3RootAccessPolicy"
     actions   = ["s3:GetBucketLocation", "s3:ListBucket"]
-    resources = [aws_s3_bucket.assets.arn]
+    resources = [aws_s3_bucket.govuk_ai_accelerator_data[0].arn]
   }
 
   statement {
+    sid = "GovukAiAcceleratorS3ObjectAccessPolicy"
     actions = [
       "s3:GetObject",
       "s3:GetObjectAcl",
       "s3:PutObject",
       "s3:PutObjectAcl"
     ]
-    resources = ["${aws_s3_bucket.assets.arn}/*"]
+    resources = ["${aws_s3_bucket.govuk_ai_accelerator_data[0].arn}/*"]
   }
 }
