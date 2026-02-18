f9ed3f40c452ca812fdc95ad2ef1c095533df96e
Jaskaran Sarkaria <jaskaran.sarkaria@digital.cabinet-office.gov.uk>
2026-02-18 15:27:00 +0000

    fix: 🐛 remove extra policy block and update principals

--- terraform/shared-modules/s3/outputs.tf
+++ terraform/shared-modules/s3/outputs.tf
@@ -5,8 +5,3 @@ output "name" {
 output "arn" {
   value = aws_s3_bucket.this.arn
 }
-
-output "irsa_policy_arn" {
-  description = "IAM policy ARN for access to the S3 bucket"
-  value       = aws_iam_policy.this.arn
-}
