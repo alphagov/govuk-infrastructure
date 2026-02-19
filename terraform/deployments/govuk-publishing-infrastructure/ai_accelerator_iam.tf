979c419547957be8870c0df2caf08c65e5115cb0
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2026-02-19 09:20:28 +0000

    Adding the IRSA role for AI Accelerator

--- terraform/deployments/govuk-publishing-infrastructure/ai_accelerator_iam.tf
+++ terraform/deployments/govuk-publishing-infrastructure/ai_accelerator_iam.tf
@@ -1,4 +1,12 @@
+# IAM role for govuk-ai-accelerator application with access to Bedrock and S3
+
+locals {
+  govuk_ai_accelerator_service_account_name = "govuk-ai-accelerator-app"
+}
+
 data "aws_iam_policy_document" "govuk_ai_accelerator_bedrock_access" {
+  count = var.govuk_environment == "integration" ? 1 : 0
+
   statement {
     sid = "BedrockAssumeInvokeModelsRolePolicy"
     actions = [
@@ -20,6 +28,8 @@ data "aws_iam_policy_document" "govuk_ai_accelerator_bedrock_access" {
 }
 
 data "aws_iam_policy_document" "govuk_ai_accelerator_s3_access" {
+  count = var.govuk_environment == "integration" ? 1 : 0
+
   statement {
     sid       = "GovukAiAcceleratorS3RootAccessPolicy"
     actions   = ["s3:GetBucketLocation", "s3:ListBucket"]
@@ -35,5 +45,44 @@ data "aws_iam_policy_document" "govuk_ai_accelerator_s3_access" {
       "s3:PutObjectAcl"
     ]
     resources = ["${aws_s3_bucket.govuk_ai_accelerator_data[0].arn}/*"]
+  }
+}
+
+resource "aws_iam_policy" "govuk_ai_accelerator_s3_access_policy" {
+  count = var.govuk_environment == "integration" ? 1 : 0
+
+  name        = "govuk-${var.govuk_environment}-govuk-ai-accelerator-s3-policy"
+  description = "Policy for govuk-ai-accelerator application with access to S3"
+  policy      = data.aws_iam_policy_document.govuk_ai_accelerator_s3_access.json
+}
+
+resource "aws_iam_policy" "govuk_ai_accelerator_bedrock_access_policy" {
+  count = var.govuk_environment == "integration" ? 1 : 0
+
+  name        = "govuk-${var.govuk_environment}-govuk-ai-accelerator-bedrock-policy"
+  description = "Policy for govuk-ai-accelerator application with access to bedrock"
+  policy      = data.aws_iam_policy_document.govuk_ai_accelgovuk_ai_accelerator_bedrock_accesserator_s3_access.json
+}
+
+# IRSA role for GOVUK AI Accelerator service account
+module "govuk_reports_iam_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+  version = "~> 6.0"
+
+  name                 = "${local.govuk_ai_accelerator_service_account_name}-${data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_id}"
+  use_name_prefix      = false
+  description          = "Role for govuk-ai-accelerator application. Corresponds to ${local.govuk_ai_accelerator_service_account_name} k8s ServiceAccount."
+  max_session_duration = 28800
+
+  policies = {
+    "${aws_iam_policy.govuk_ai_accelerator_s3_access_policy.name}" = aws_iam_policy.govuk_ai_accelerator_s3_access_policy.arn,
+    "${aws_iam_policy.govuk_ai_accelerator_bedrock_access_policy.name}" = aws_iam_policy.govuk_ai_accelerator_bedrock_access_policy.arn
+  }
+
+  oidc_providers = {
+    main = {
+      provider_arn               = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_oidc_provider_arn
+      namespace_service_accounts = ["apps:${local.govuk_ai_accelerator_service_account_name}"]
+    }
   }
 }
