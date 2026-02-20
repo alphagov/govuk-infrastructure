42eaa2bb83b82c43a93321edfc24ce77e0a157ad
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2026-02-20 11:58:56 +0000

    Extend EKS log group retention to 2 years

--- terraform/deployments/tfc-configuration/variables-integration.tf
+++ terraform/deployments/tfc-configuration/variables-integration.tf
@@ -8,7 +8,7 @@ module "variable-set-integration" {
     cluster_infrastructure_state_bucket = "govuk-terraform-integration"
 
     cluster_version               = "1.33"
-    cluster_log_retention_in_days = 7
+    cluster_log_retention_in_days = 728
 
     vpc_cidr = "10.1.0.0/16"
 
