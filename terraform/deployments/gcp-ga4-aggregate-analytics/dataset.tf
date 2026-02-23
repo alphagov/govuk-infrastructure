fd7013d2f5877a95edc2583810ff9bf31bbbbbb8
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-02-23 16:39:23 +0000

    IA-2490: add dataset for GA4 events data

---
+++ terraform/deployments/gcp-ga4-aggregate-analytics/dataset.tf
@@ -0,0 +1,13 @@
+locals {
+  # 1000ms * 60s * 60m * 24h * 90 days
+  ninety_days_in_ms = 1000 * 60 * 60 * 24 * 90
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                      = "analytics_523297687"
+  friendly_name                   = "Analytics Property ID 523297687"
+  description                     = "This dataset contains events tables from GA4 property ID 523297687."
+  location                        = "europe-west2"
+  default_partition_expiration_ms = local.ninety_days_in_ms
+  default_table_expiration_ms     = local.ninety_days_in_ms
+}
