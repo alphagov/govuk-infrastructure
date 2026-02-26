4babbe28549f3efe854995c61952bc76b2e119c3
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2026-02-26 16:01:17 +0000

    Add project ideas for the knowledge graph

--- terraform/deployments/tfc-configuration/variables-production.tf
+++ terraform/deployments/tfc-configuration/variables-production.tf
@@ -733,5 +733,6 @@ module "variable-set-gov-graph-production" {
   tfvars = {
     google_cloud_folder          = "278098142879"
     google_cloud_billing_account = "015C7A-FAF970-B0D375"
+    project_id                   = "govuk-knowledge-graph"
   }
 }
--- terraform/deployments/tfc-configuration/variables-staging.tf
+++ terraform/deployments/tfc-configuration/variables-staging.tf
@@ -725,5 +725,6 @@ module "variable-set-gov-graph-staging" {
   tfvars = {
     google_cloud_folder          = "278098142879"
     google_cloud_billing_account = "015C7A-FAF970-B0D375"
+    project_id                   = "govuk-knowledge-graph-staging"
   }
 }
