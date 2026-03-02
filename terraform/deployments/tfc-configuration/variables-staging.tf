21422d1db233cc2dd26ca85297c21eefb479a7a1
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2026-03-02 16:19:40 +0000

    Add more variables for govgraph

--- terraform/deployments/tfc-configuration/variables-staging.tf
+++ terraform/deployments/tfc-configuration/variables-staging.tf
@@ -731,5 +731,8 @@ module "variable-set-gov-graph-staging" {
     google_cloud_folder          = "278098142879"
     google_cloud_billing_account = "015C7A-FAF970-B0D375"
     project_id                   = "govuk-knowledge-graph-staging"
+    region                       = "europe-west2"
+    zone                         = "europe-west2-b"
+    location                     = "EUROPE-WEST2"
   }
 }
