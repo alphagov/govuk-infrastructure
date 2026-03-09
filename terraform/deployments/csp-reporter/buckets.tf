43b1a9fdeec55e850bf55aafd3386a32f8818039
David Mays <david.mays@digital.cabinet-office.gov.uk>
2026-03-09 16:59:17 +0000

    Migrate CSP Bucket to Secure S3 Module

--- terraform/deployments/csp-reporter/buckets.tf
+++ terraform/deployments/csp-reporter/buckets.tf
@@ -1,44 +1,20 @@
-# Bucket to store data from Kinesis Firehose, stores both successes and errors
-resource "aws_s3_bucket" "csp_reports" {
-  bucket = "govuk-${var.govuk_environment}-csp-reports"
+module "secure_s3_bucket_csp_reports" {
+  source = "github.com/alphagov/govuk-infrastructure/terraform/shared-modules/s3?ref=3f260111d76ce69eeb1f6b9b8d3ea52e1bd467b4"
+  name   = "govuk-${var.govuk_environment}-csp-reports"
 
-  tags = {
-    Name = "govuk-${var.govuk_environment}-csp-reports"
-  }
+  versioning_enabled = false
+  lifecycle_rules = [
+    {
+      id     = "govuk-${var.govuk_environment}-csp-reports-lifecycle"
+      status = "Enabled"
+      expiration = {
+        days = 30
+      }
+    }
+  ]
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "csp_reports_lifecycle" {
-  bucket = aws_s3_bucket.csp_reports.id
-
-  rule {
-    id     = "govuk-${var.govuk_environment}-csp-reports-lifecycle"
-    status = "Enabled"
-
-    expiration {
-      days = 30
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "csp_reports" {
-  bucket = aws_s3_bucket.csp_reports.id
-  policy = data.aws_iam_policy_document.csp_reports_bucket_policy.json
-}
-
-data "aws_iam_policy_document" "csp_reports_bucket_policy" {
-  statement {
-    sid    = "DenyNonTLS"
-    effect = "Deny"
-    principals {
-      identifiers = ["*"]
-      type        = "AWS"
-    }
-    actions   = ["s3:*"]
-    resources = ["${aws_s3_bucket.csp_reports.arn}/*"]
-    condition {
-      test     = "Bool"
-      values   = [false]
-      variable = "aws:SecureTransport"
-    }
-  }
+moved {
+  from = aws_s3_bucket.csp_reports
+  to   = module.secure_s3_bucket_csp_reports.aws_s3_bucket.this
 }
