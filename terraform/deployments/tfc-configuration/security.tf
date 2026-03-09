8247713a1d20b10c6dc8c46839ff0528341cbcd2
Sam Simpson <sam@simpson.id>
2026-03-09 13:35:18 +0000

    Use tfvars files for all security workspaces
    https://github.com/alphagov/govuk-infrastructure/issues/3820

--- terraform/deployments/tfc-configuration/security.tf
+++ terraform/deployments/tfc-configuration/security.tf
@@ -1,14 +1,17 @@
 module "security-integration" {
   source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
 
-  organization        = var.organization
-  workspace_name      = "security-integration"
-  workspace_desc      = "This module manages AWS Security resources for use by other modules and resources."
-  workspace_tags      = ["integration", "security", "eks", "aws"]
-  terraform_version   = var.terraform_version
-  execution_mode      = "remote"
-  working_directory   = "/terraform/deployments/security/"
-  trigger_patterns    = ["/terraform/deployments/security/**/*"]
+  organization      = var.organization
+  workspace_name    = "security-integration"
+  workspace_desc    = "This module manages AWS Security resources for use by other modules and resources."
+  workspace_tags    = ["integration", "security", "eks", "aws"]
+  terraform_version = var.terraform_version
+  execution_mode    = "remote"
+  working_directory = "/terraform/deployments/security/"
+  trigger_patterns = [
+    "/terraform/deployments/security/**/*",
+    "/terraform/variables/integration/common.tfvars"
+  ]
   global_remote_state = true
 
   project_name = "govuk-infrastructure"
@@ -23,9 +26,12 @@ module "security-integration" {
     "GOV.UK Production"           = "write"
   }
 
+  tfvars_files = [
+    "integration/common.tfvars"
+  ]
+
   variable_set_ids = [
     local.aws_credentials["integration"],
-    module.variable-set-integration.id,
     module.sensitive-variables.security_common_id,
     module.sensitive-variables.security_integration_id
   ]
@@ -34,14 +40,17 @@ module "security-integration" {
 module "security-staging" {
   source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
 
-  organization        = var.organization
-  workspace_name      = "security-staging"
-  workspace_desc      = "This module manages AWS Security resources for use by other modules and resources."
-  workspace_tags      = ["staging", "security", "eks", "aws"]
-  terraform_version   = var.terraform_version
-  execution_mode      = "remote"
-  working_directory   = "/terraform/deployments/security/"
-  trigger_patterns    = ["/terraform/deployments/security/**/*"]
+  organization      = var.organization
+  workspace_name    = "security-staging"
+  workspace_desc    = "This module manages AWS Security resources for use by other modules and resources."
+  workspace_tags    = ["staging", "security", "eks", "aws"]
+  terraform_version = var.terraform_version
+  execution_mode    = "remote"
+  working_directory = "/terraform/deployments/security/"
+  trigger_patterns = [
+    "/terraform/deployments/security/**/*",
+    "/terraform/variables/staging/common.tfvars"
+  ]
   global_remote_state = true
 
   project_name = "govuk-infrastructure"
@@ -55,9 +64,12 @@ module "security-staging" {
     "GOV.UK Production" = "write"
   }
 
+  tfvars_files = [
+    "staging/common.tfvars"
+  ]
+
   variable_set_ids = [
     local.aws_credentials["staging"],
-    module.variable-set-staging.id,
     module.sensitive-variables.security_common_id,
     module.sensitive-variables.security_staging_id
   ]
@@ -66,14 +78,17 @@ module "security-staging" {
 module "security-production" {
   source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
 
-  organization        = var.organization
-  workspace_name      = "security-production"
-  workspace_desc      = "This module manages AWS Security resources for use by other modules and resources."
-  workspace_tags      = ["production", "security", "eks", "aws"]
-  terraform_version   = var.terraform_version
-  execution_mode      = "remote"
-  working_directory   = "/terraform/deployments/security/"
-  trigger_patterns    = ["/terraform/deployments/security/**/*"]
+  organization      = var.organization
+  workspace_name    = "security-production"
+  workspace_desc    = "This module manages AWS Security resources for use by other modules and resources."
+  workspace_tags    = ["production", "security", "eks", "aws"]
+  terraform_version = var.terraform_version
+  execution_mode    = "remote"
+  working_directory = "/terraform/deployments/security/"
+  trigger_patterns = [
+    "/terraform/deployments/security/**/*",
+    "/terraform/variables/production/common.tfvars"
+  ]
   global_remote_state = true
 
   project_name = "govuk-infrastructure"
@@ -87,9 +102,12 @@ module "security-production" {
     "GOV.UK Production" = "write"
   }
 
+  tfvars_files = [
+    "production/common.tfvars"
+  ]
+
   variable_set_ids = [
     local.aws_credentials["production"],
-    module.variable-set-production.id,
     module.sensitive-variables.security_common_id,
     module.sensitive-variables.security_production_id
   ]
