2d237f0323f4f4b608e67561b61e6b0237983659
David Mays <david.mays@digital.cabinet-office.gov.uk>
2026-03-10 16:46:08 +0000

    Add other moved resource blocks.

--- terraform/deployments/csp-reporter/buckets.tf
+++ terraform/deployments/csp-reporter/buckets.tf
@@ -1,6 +1,8 @@
 module "secure_s3_bucket_csp_reports" {
-  source = "github.com/alphagov/govuk-infrastructure/terraform/shared-modules/s3?ref=3f260111d76ce69eeb1f6b9b8d3ea52e1bd467b4"
-  name   = "govuk-${var.govuk_environment}-csp-reports"
+  source = "../../shared-modules/s3"
+
+  govuk_environment = var.govuk_environment
+  name              = "govuk-${var.govuk_environment}-csp-reports"
 
   versioning_enabled = false
   lifecycle_rules = [
@@ -17,4 +19,14 @@ module "secure_s3_bucket_csp_reports" {
 moved {
   from = aws_s3_bucket.csp_reports
   to   = module.secure_s3_bucket_csp_reports.aws_s3_bucket.this
+}
+
+moved {
+  from = aws_s3_bucket_lifecycle_configuration.csp_reports_lifecycle
+  to   = module.secure_s3_bucket_csp_reports.aws_s3_bucket_lifecycle_configuration.this[0]
+}
+
+moved {
+  from = aws_s3_bucket_policy.csp_reports
+  to   = module.secure_s3_bucket_csp_reports.aws_s3_bucket_policy.bucket_policy
 }
