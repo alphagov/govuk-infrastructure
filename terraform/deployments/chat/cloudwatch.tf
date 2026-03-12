3a95c79d2b84caa585c04a3a3b9083ec6e3dd568
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-03-12 08:51:09 +0000

    Add CloudWatch log group for Bedrock in eu-west-2 for Titan embeddings
    Configure /aws/bedrock log group with 30 day retention in eu-west-2 to support
    Titan usage for content ingestion. User questions embeddings will still
    be served on eu-west-1.
    
    Also updates the Bedrock IAM role to allow writing to the new log group
    and adds logging configuration for eu-west-2.

--- terraform/deployments/chat/cloudwatch.tf
+++ terraform/deployments/chat/cloudwatch.tf
@@ -1,4 +1,16 @@
-resource "aws_cloudwatch_log_group" "bedrock_log_group" {
+resource "aws_cloudwatch_log_group" "bedrock_log_group_dublin" {
   name              = "/aws/bedrock"
   retention_in_days = 30
+  region            = "eu-west-1"
+}
+
+moved {
+  from = aws_cloudwatch_log_group.bedrock_log_group
+  to = aws_cloudwatch_log_group.bedrock_log_group_dublin
+}
+
+resource "aws_cloudwatch_log_group" "bedrock_log_group_london" {
+  name              = "/aws/bedrock"
+  retention_in_days = 30
+  region            = "eu-west-2"
 }
