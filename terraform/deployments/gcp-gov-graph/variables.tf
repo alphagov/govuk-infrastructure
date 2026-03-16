360995d9869fb494bb21ab29b106a0900920d7bd
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2026-03-16 13:49:23 +0000

    Import gov graph variables
    Also renames the "google_cloud_billing_account", "google_cloud_folder" and
    "google_cloud_apis" variables to match what's currently in the knowledge
    graph

--- terraform/deployments/gcp-gov-graph/variables.tf
+++ terraform/deployments/gcp-gov-graph/variables.tf
@@ -15,40 +15,43 @@ variable "name" {
   description = "A short name for this environment (used in resource IDs)"
 }
 
-variable "google_cloud_folder" {
+variable "folder_id" {
   type        = string
   description = "The ID of the Google Cloud folder to create projects under"
 }
 
-variable "google_cloud_billing_account" {
+variable "billing_account" {
   type        = string
   description = "The ID of the Google Cloud billing account to associate projects with"
 }
 
-variable "google_cloud_apis" {
+variable "services" {
   type        = set(string)
   description = "The Google Cloud APIs to enable for the project"
   default = [
-    # Required to be able to manage resources using Terraform
-    "cloudresourcemanager.googleapis.com",
-    # Required to set up service accounts and manage dynamic credentials
-    "iam.googleapis.com",
-    "iamcredentials.googleapis.com",
-    "sts.googleapis.com",
-    # Required for Discovery Engine
-    "discoveryengine.googleapis.com",
-    # Required for event data pipeline
-    "bigquery.googleapis.com",
-    "bigquerystorage.googleapis.com",
     "storage.googleapis.com",
-    "cloudbuild.googleapis.com",
+    "iam.googleapis.com",
+    "appengine.googleapis.com",
     "artifactregistry.googleapis.com",
-    "cloudfunctions.googleapis.com",
-    "run.googleapis.com",
     "cloudscheduler.googleapis.com",
-    # Required for observability
-    "logging.googleapis.com",
-    "monitoring.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "bigquery.googleapis.com",
+    "bigqueryconnection.googleapis.com",
+    "bigquerydatatransfer.googleapis.com",
+    "run.googleapis.com",
+    "compute.googleapis.com",
+    "dns.googleapis.com",
+    "eventarc.googleapis.com",
+    "networkmanagement.googleapis.com",
+    "pubsub.googleapis.com",
+    "sourcerepo.googleapis.com",
+    "vpcaccess.googleapis.com",
+    "workflows.googleapis.com",
+    "iap.googleapis.com",
+    "secretmanager.googleapis.com",
+    "redis.googleapis.com",
+    "dlp.googleapis.com",
+    "cloudquotas.googleapis.com"
   ]
 }
 
@@ -85,4 +88,132 @@ variable "location" {
 
 variable "project_id" {
   type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project_number" {
+  type = string
+}
+
+variable "govgraph_domain" {
+  type = string
+}
+
+variable "govgraphsearch_domain" {
+  type = string
+}
+
+variable "govsearch_domain" {
+  type = string
+}
+
+variable "application_title" {
+  type = string
+}
+
+variable "enable_auth" {
+  type = string
+}
+
+variable "signon_url" {
+  type = string
+}
+
+variable "oauth_auth_url" {
+  type = string
+}
+
+variable "oauth_token_url" {
+  type = string
+}
+
+variable "oauth_callback_url" {
+  type = string
+}
+
+variable "enable_redis_session_store_instance" {
+  type = bool
+}
+
+variable "gtm_id" {
+  type = string
+}
+
+variable "gtm_auth" {
+  type = string
+}
+
+variable "project_owner_members" {
+  type = set(string)
+}
+
+variable "iap_govgraphsearch_members" {
+  type = set(string)
+}
+
+variable "bigquery_job_user_members" {
+  type = set(string)
+}
+
+variable "storage_data_processed_object_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_private_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_public_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_content_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_publisher_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_functions_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_graph_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_publishing_api_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_smart_survey_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_support_api_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_search_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_test_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_whitehall_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_asset_manager_data_viewer_members" {
+  type = set(string)
+}
+
+variable "bigquery_zendesk_data_viewer_members" {
+  type = set(string)
 }
