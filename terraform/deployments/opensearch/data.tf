1eabc56f55a8f9d89b8af9371e37366fd97d8e0e
Sam Simpson <sam@simpson.id>
2026-03-18 16:40:16 +0000

    opensearch: Use tfe_outputs for all remote values instead of terraform_remote_state
    The state in the buckets referenced here hasn't been used in years and is extremely outdated

--- terraform/deployments/opensearch/data.tf
+++ terraform/deployments/opensearch/data.tf
@@ -6,38 +6,15 @@ data "tfe_outputs" "cluster_infrastructure" {
   organization = "govuk"
   workspace    = "cluster-infrastructure-${var.govuk_environment}"
 }
-data "terraform_remote_state" "infra_vpc" {
-  backend = "s3"
-  config = {
-    bucket = var.govuk_aws_state_bucket
-    key    = "govuk/infra-vpc.tfstate"
-    region = data.aws_region.current.region
-  }
-}
-data "terraform_remote_state" "infra_security_groups" {
-  backend = "s3"
-  config = {
-    bucket = var.govuk_aws_state_bucket
-    key    = "govuk/infra-security-groups.tfstate"
-    region = data.aws_region.current.region
-  }
+
+data "tfe_outputs" "vpc" {
+  organization = "govuk"
+  workspace    = "vpc-${var.govuk_environment}"
 }
 
-data "terraform_remote_state" "infra_root_dns_zones" {
-  backend = "s3"
-  config = {
-    bucket = var.govuk_aws_state_bucket
-    key    = "govuk/infra-root-dns-zones.tfstate"
-    region = var.aws_region
-  }
-}
-data "terraform_remote_state" "infra_networking" {
-  backend = "s3"
-  config = {
-    bucket = var.govuk_aws_state_bucket
-    key    = "govuk/infra-networking.tfstate"
-    region = var.aws_region
-  }
+data "tfe_outputs" "root_dns" {
+  organization = "govuk"
+  workspace    = "root-dns-${var.govuk_environment}"
 }
 
 data "aws_acm_certificate" "govuk_internal" {
