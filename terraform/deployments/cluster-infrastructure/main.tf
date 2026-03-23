b3c7a6cbfd14082e081c16ad801aba8947534d10
David Mays <david.mays@digital.cabinet-office.gov.uk>
2026-03-23 14:58:58 +0000

    Fix positioning of brackets.

--- terraform/deployments/cluster-infrastructure/main.tf
+++ terraform/deployments/cluster-infrastructure/main.tf
@@ -40,10 +40,10 @@ locals {
   }
 
   default_cluster_addons = {
-    coredns = { addon_version = "v1.13.2-eksbuild.1", resolve_conflicts_on_create = "OVERWRITE" }
-    kube-proxy = { addon_version = "v1.34.5-eksbuild.2", resolve_conflicts_on_create = "OVERWRITE"
-    kube-state-metrics = { addon_version = "v2.18.0-eksbuild.1", resolve_conflicts_on_create = "OVERWRITE" } }
-    metrics-server = { addon_version = "v0.8.1-eksbuild.1", resolve_conflicts_on_create = "OVERWRITE" }
+    coredns            = { addon_version = "v1.13.2-eksbuild.1", resolve_conflicts_on_create = "OVERWRITE" }
+    kube-proxy         = { addon_version = "v1.34.5-eksbuild.2", resolve_conflicts_on_create = "OVERWRITE" }
+    kube-state-metrics = { addon_version = "v2.18.0-eksbuild.1", resolve_conflicts_on_create = "OVERWRITE" }
+    metrics-server     = { addon_version = "v0.8.1-eksbuild.1", resolve_conflicts_on_create = "OVERWRITE" }
     vpc-cni = {
       addon_version               = "v1.21.1-eksbuild.5",
       resolve_conflicts_on_create = "OVERWRITE",
