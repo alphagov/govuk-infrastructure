ae05279013a7c42826cecf915fdd809c2127caef
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2026-03-24 14:01:19 +0000

    Remove Tetragon from Terraform
    Removes mentions of Tetragon from Terraform entirely, and removes its S3 bucket
    in the process.

--- terraform/deployments/cluster-services/modules/tetragon/s3.tf
+++
@@ -1,7 +0,0 @@
-module "secure_s3_bucket" {
-  source = "github.com/alphagov/govuk-infrastructure/terraform/shared-modules/s3?ref=3f260111d76ce69eeb1f6b9b8d3ea52e1bd467b4"
-
-  name               = local.bucket_name
-  versioning_enabled = true
-  lifecycle_rules    = []
-}
