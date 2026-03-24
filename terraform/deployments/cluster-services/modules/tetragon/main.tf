d75f351745b1343fb8d13642e5eb3da6a6386a0a
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2026-03-24 11:50:51 +0000

    Uninstall Tetragon and Fluentbit from the cluster
    After evaluation, we have decided against using Tetragon. This commit (and PR)
    are the start of a small chain of changes that will incrementally uninstall it,
    its Kubernetes dependencies, and finally its infrastructure dependencies.

--- terraform/deployments/cluster-services/modules/tetragon/main.tf
+++ terraform/deployments/cluster-services/modules/tetragon/main.tf
@@ -13,25 +13,3 @@ resource "kubernetes_namespace_v1" "tetragon" {
     }
   }
 }
-
-resource "helm_release" "tetragon" {
-  name       = "tetragon"
-  repository = "https://helm.cilium.io"
-  chart      = "tetragon"
-  version    = "1.6.0"
-  namespace  = kubernetes_namespace_v1.tetragon.id
-
-  values = [yamlencode({
-    tetragon = {
-      exportAllowList = jsonencode({ "in_init_tree" : false, "namespace" : ["apps"], "event_set" : ["PROCESS_EXEC", "PROCESS_EXIT", "PROCESS_KPROBE", "PROCESS_UPROBE", "PROCESS_TRACEPOINT", "PROCESS_LSM"] })
-      exportDenyList  = <<EOF
-        {"health_check":true}
-        {"namespace":["", "kube-system"]}
-      EOF
-      exportFilePerm  = "644"
-    }
-    export = {
-      mode = ""
-    }
-  })]
-}
