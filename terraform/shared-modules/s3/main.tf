da69d102976b8d7fc4a76fc23d51c4183ab2eada
Sam Simpson <sam@simpson.id>
2026-04-20 15:16:12 +0100

    Set blocked_encryption_types in S3 SSE config
    The TF provider wants to make perpetual changes if this isn't set

--- terraform/shared-modules/s3/main.tf
+++ terraform/shared-modules/s3/main.tf
@@ -135,6 +135,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
 
   rule {
+    blocked_encryption_types = ["NONE"]
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
