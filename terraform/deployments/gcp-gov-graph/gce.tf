d631cf7c9d3645a931bdbb081520b6b460877345
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2026-05-08 13:41:58 +0100

    Generify the asset manager script

--- terraform/deployments/gcp-gov-graph/gce.tf
+++ terraform/deployments/gcp-gov-graph/gce.tf
@@ -582,7 +582,7 @@ resource "google_compute_instance_template" "whitehall" {
 resource "google_compute_instance_template" "asset_manager" {
   name         = "asset-manager"
   machine_type = "c2d-highmem-2"
-  metadata_startup_script = templatefile("scripts/asset_manager_startup.sh.tftpl", {
+  metadata_startup_script = templatefile("scripts/app_startup.sh.tftpl", {
     project_id = var.project_id
     image      = module.asset-manager-container.source_image
     }
