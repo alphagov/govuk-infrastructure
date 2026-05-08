b3a2f09aab0c0eaaf0afedf48000333c936164e3
David Mays <david.mays@digital.cabinet-office.gov.uk>
2026-05-08 12:45:01 +0100

    Migrate ai-accelerator-data S3 to Secure Module

--- terraform/deployments/govuk-publishing-infrastructure/ai_accelerator_s3.tf
+++ terraform/deployments/govuk-publishing-infrastructure/ai_accelerator_s3.tf
@@ -1,85 +1,43 @@
-resource "aws_s3_bucket" "govuk_ai_accelerator_data" {
-  bucket = "govuk-ai-accelerator-data-integration"
-  count  = var.enable_govuk_ai_accelerator ? 1 : 0
+module "secure_s3_bucket_ai_accelerator_data" {
+  source            = "../../shared-modules/s3"
+  count             = var.enable_govuk_ai_accelerator ? 1 : 0
+  govuk_environment = var.govuk_environment
+
+  name = "govuk-ai-accelerator-data-${var.govuk_environment}"
+
 }
 
-resource "aws_s3_bucket_public_access_block" "govuk_ai_accelerator_data_access_block" {
-  bucket = aws_s3_bucket.govuk_ai_accelerator_data[0].id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  count = var.enable_govuk_ai_accelerator ? 1 : 0
+moved {
+  from = aws_s3_bucket.govuk_ai_accelerator_data[0].id
+  to   = module.secure_s3_bucket_ai_accelerator_data[0].aws_s3_bucket.this.id
 }
 
-resource "aws_s3_bucket_versioning" "govuk_ai_accelerator_data_versioning" {
-  bucket = aws_s3_bucket.govuk_ai_accelerator_data[0].id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-
-  count = var.enable_govuk_ai_accelerator ? 1 : 0
+moved {
+  from = aws_s3_bucket_public_access_block.govuk_ai_accelerator_data_access_block[0].id
+  to   = module.secure_s3_bucket_ai_accelerator_data[0].aws_s3_bucket_public_access_block.this.id
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "govuk_ai_accelerator_data_encryption" {
-  bucket = aws_s3_bucket.govuk_ai_accelerator_data[0].id
-
-  rule {
-    blocked_encryption_types = ["NONE"]
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-
-  count = var.enable_govuk_ai_accelerator ? 1 : 0
+moved {
+  from = aws_s3_bucket_versioning.govuk_ai_accelerator_data_versioning[0].id
+  to   = module.secure_s3_bucket_ai_accelerator_data[0].aws_s3_bucket_versioning.this.id
 }
 
-data "aws_iam_policy_document" "https_only" {
-  count = var.enable_govuk_ai_accelerator ? 1 : 0
-
-  statement {
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    sid     = "https_only"
-    effect  = "Deny"
-    actions = ["s3:*"]
-    resources = [
-      aws_s3_bucket.govuk_ai_accelerator_data[0].arn,
-      "${aws_s3_bucket.govuk_ai_accelerator_data[0].arn}/*"
-    ]
-    condition {
-      test     = "Bool"
-      values   = ["false"]
-      variable = "aws:SecureTransport"
-    }
-  }
+moved {
+  from = aws_s3_bucket_server_side_encryption_configuration.govuk_ai_accelerator_data_encryption[0].id
+  to   = module.secure_s3_bucket_ai_accelerator_data[0].aws_s3_bucket_server_side_encryption_configuration.this.id
 }
 
-resource "aws_s3_bucket_policy" "govuk_ai_accelerator_data_bucket_policy" {
-  bucket = aws_s3_bucket.govuk_ai_accelerator_data[0].id
-  policy = data.aws_iam_policy_document.https_only[0].json
-
-  count = var.enable_govuk_ai_accelerator ? 1 : 0
+moved {
+  from = aws_s3_bucket_policy.govuk_ai_accelerator_data_bucket_policy[0].id
+  to   = module.secure_s3_bucket_ai_accelerator_data[0].aws_s3_bucket_policy.this.id
 }
 
-resource "aws_s3_bucket_ownership_controls" "govuk_ai_accelerator_data_owner_controls" {
-  bucket = aws_s3_bucket.govuk_ai_accelerator_data[0].id
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-
-  count = var.enable_govuk_ai_accelerator ? 1 : 0
+moved {
+  from = aws_s3_bucket_ownership_controls.govuk_ai_accelerator_data_owner_controls[0].id
+  to   = module.secure_s3_bucket_ai_accelerator_data[0].aws_s3_bucket_ownership_controls.this.id
 }
 
-resource "aws_s3_bucket_logging" "govuk_ai_accelerator_data_logging" {
-  bucket        = aws_s3_bucket.govuk_ai_accelerator_data[0].id
-  target_bucket = "govuk-integration-aws-logging"
-  target_prefix = "s3/govuk-ai-accelerator-data-integration/"
-
-  count = var.enable_govuk_ai_accelerator ? 1 : 0
+moved {
+  from = aws_s3_bucket_logging.govuk_ai_accelerator_data_logging[0].id
+  to   = module.secure_s3_bucket_ai_accelerator_data[0].aws_s3_bucket_logging.this.id
 }
