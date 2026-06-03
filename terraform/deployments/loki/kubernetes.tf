a7f165213cb2fb466c40dda796dbfff35bae2883
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2026-06-03 12:04:50 +0100

    Install loki helm chart

--- terraform/deployments/loki/kubernetes.tf
+++ terraform/deployments/loki/kubernetes.tf
@@ -3,3 +3,27 @@ resource "kubernetes_namespace_v1" "loki" {
     name = "loki"
   }
 }
+
+locals {
+  loki_helm_chart_values = templatefile(
+    "./files/loki_helm_values.tpl.yaml",
+    {
+      iam_role_arn      = aws_iam_role.loki.arn,
+      aws_region        = "eu-west-1",
+      chunk_bucket_name = module.s3_bucket_chunks.name,
+      ruler_bucket_name = module.s3_bucket_ruler.name,
+      alert_manager_url = "http://kube-prometheus-stack-alertmanager.monitoring.svc.cluster.local:9093", // FIXME
+    }
+  )
+}
+
+resource "helm_release" "loki" {
+  chart            = "loki"
+  name             = "loki"
+  namespace        = kubernetes_namespace_v1.loki.id
+  create_namespace = false
+  repository       = "https://grafana-community.github.io/helm-charts"
+  version          = "17.1.6"
+  timeout          = 300
+  values           = [local.loki_helm_chart_values]
+}
