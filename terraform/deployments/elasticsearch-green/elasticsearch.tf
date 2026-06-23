fdf1f91cce2c91eaf91dd0542ca542cd3e2f67c0
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2026-06-23 15:32:17 +0100

    Override custom domain in ES import

--- terraform/deployments/elasticsearch-green/elasticsearch.tf
+++ terraform/deployments/elasticsearch-green/elasticsearch.tf
@@ -32,6 +32,7 @@ module "opensearch" {
   disable_node_to_node_encryption_for_green_cluster          = true
   disable_enforced_https_for_green_cluster                   = true
   override_security_group_ids_for_green_cluster              = [data.tfe_outputs.security.nonsensitive_values.govuk_elasticsearch6_access_sg_id]
+  override_custom_domain_endpoint_for_green_cluster          = "elasticsearch6-opensearch.${var.govuk_environment}.govuk-internal.digital"
   elasticsearch_domain_additional_tags_for_green_cluster = {
     Project       = "green"
     aws_stackname = "green"
