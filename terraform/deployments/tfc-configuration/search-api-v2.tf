426df2f9d2d03f27223dfe00f7384a1b7f2c9df9
Andy Hunt <andy.hunt@dsit.gov.uk>
2026-07-13 15:30:25 +0100

    Refactor search-api-v2 Terraform Cloud workspaces
    A while back, we changed how our Terraform Cloud workspaces are structured, to
    focus on usign tfvars files instead of Terraform Cloud variable sets.
    
    The search-api-v2 workspaces were not working like this, making it harder to
    maintain. This commit refactors the workspaces to work like the others.

--- terraform/deployments/tfc-configuration/search-api-v2.tf
+++ terraform/deployments/tfc-configuration/search-api-v2.tf
@@ -1,45 +1,171 @@
-resource "tfe_project" "project" {
+resource "tfe_project" "search-api-v2" {
   name = "govuk-search-api-v2"
 }
 
-# Main integration module
-module "environment_integration" {
-  source = "./modules/search-api-v2"
 
-  name                          = "integration"
-  google_project_id             = "search-api-v2-integration"
-  google_project_number         = "780375417592"
-  google_workload_provider_name = "projects/780375417592/locations/global/workloadIdentityPools/terraform-cloud-id-pool/providers/terraform-cloud-provider-oidc"
-  google_service_account_email  = "tfc-service-account@search-api-v2-integration.iam.gserviceaccount.com"
-  tfc_project                   = tfe_project.project
-  terraform_version             = var.terraform_version
-  vcs_repo_branch               = "main"
+## Moved because the original resource had a generic name, and it should've been more specific
+moved {
+  from = tfe_project.project
+  to   = tfe_project.search-api-v2
 }
 
-# Main staging module
-module "environment_staging" {
-  source = "./modules/search-api-v2"
+module "search-api-v2-integration" {
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
 
-  name                          = "staging"
-  google_project_id             = "search-api-v2-staging"
-  google_project_number         = "773027887517"
-  google_workload_provider_name = "projects/773027887517/locations/global/workloadIdentityPools/terraform-cloud-id-pool/providers/terraform-cloud-provider-oidc"
-  google_service_account_email  = "tfc-service-account@search-api-v2-staging.iam.gserviceaccount.com"
-  tfc_project                   = tfe_project.project
-  terraform_version             = var.terraform_version
-  vcs_repo_branch               = "main"
+  organization      = var.organization
+  workspace_name    = "search-api-v2-integration"
+  workspace_desc    = "Provisions search-api-v2 Discovery Engine resources for the integration environment"
+  workspace_tags    = ["govuk", "search-api-v2", "search-api-v2-environment", "integration"]
+  terraform_version = var.terraform_version
+  execution_mode    = "remote"
+  auto_apply        = true
+  working_directory = "/terraform/deployments/search-api-v2/"
+  trigger_patterns = [
+    "/terraform/deployments/search-api-v2/**/*",
+    "/terraform/variables/integration/search-api-v2.tfvars"
+  ]
+
+  project_name = tfe_project.search-api-v2.name
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Non-Production (r/o)" = "write"
+    "GOV.UK Production"           = "write"
+  }
+
+  envvars = {
+    "TFC_GCP_PROVIDER_AUTH"             = true
+    "TFC_GCP_WORKLOAD_PROVIDER_NAME"    = "projects/780375417592/locations/global/workloadIdentityPools/terraform-cloud-id-pool/providers/terraform-cloud-provider-oidc"
+    "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL" = "tfc-service-account@search-api-v2-integration.iam.gserviceaccount.com"
+  }
+
+  tfvars_files = [
+    "integration/search-api-v2.tfvars"
+  ]
+
+  variable_set_ids = [
+    local.gcp_credentials["integration"]
+  ]
 }
 
-# Main production module
-module "environment_production" {
-  source = "./modules/search-api-v2"
+moved {
+  from = module.environment_integration.tfe_workspace.environment_workspace
+  to   = module.search-api-v2-integration.tfe_workspace.ws
+}
 
-  name                          = "production"
-  google_project_id             = "search-api-v2-production"
-  google_project_number         = "931453572747"
-  google_workload_provider_name = "projects/931453572747/locations/global/workloadIdentityPools/terraform-cloud-id-pool/providers/terraform-cloud-provider-oidc"
-  google_service_account_email  = "tfc-service-account@search-api-v2-production.iam.gserviceaccount.com"
-  tfc_project                   = tfe_project.project
-  terraform_version             = var.terraform_version
-  vcs_repo_branch               = "main"
+moved {
+  from = module.environment_integration.tfe_workspace_settings.environment_workspace_settings
+  to   = module.search-api-v2-integration.tfe_workspace_settings.ws
+}
+
+
+module "search-api-v2-staging" {
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
+
+  organization      = var.organization
+  workspace_name    = "search-api-v2-staging"
+  workspace_desc    = "Provisions search-api-v2 Discovery Engine resources for the staging environment"
+  workspace_tags    = ["govuk", "search-api-v2", "search-api-v2-environment", "staging"]
+  terraform_version = var.terraform_version
+  execution_mode    = "remote"
+  auto_apply        = true
+  working_directory = "/terraform/deployments/search-api-v2/"
+  trigger_patterns = [
+    "/terraform/deployments/search-api-v2/**/*",
+    "/terraform/variables/staging/search-api-v2.tfvars"
+  ]
+
+  project_name = tfe_project.search-api-v2.name
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Non-Production (r/o)" = "write"
+    "GOV.UK Production"           = "write"
+  }
+
+  envvars = {
+    "TFC_GCP_PROVIDER_AUTH"             = true
+    "TFC_GCP_WORKLOAD_PROVIDER_NAME"    = "projects/773027887517/locations/global/workloadIdentityPools/terraform-cloud-id-pool/providers/terraform-cloud-provider-oidc"
+    "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL" = "tfc-service-account@search-api-v2-staging.iam.gserviceaccount.com"
+  }
+
+  tfvars_files = [
+    "staging/search-api-v2.tfvars"
+  ]
+
+  variable_set_ids = [
+    local.gcp_credentials["staging"]
+  ]
+}
+
+moved {
+  from = module.environment_staging.tfe_workspace.environment_workspace
+  to   = module.search-api-v2-staging.tfe_workspace.ws
+}
+
+moved {
+  from = module.environment_staging.tfe_workspace_settings.environment_workspace_settings
+  to   = module.search-api-v2-staging.tfe_workspace_settings.ws
+}
+
+
+module "search-api-v2-production" {
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
+
+  organization      = var.organization
+  workspace_name    = "search-api-v2-production"
+  workspace_desc    = "Provisions search-api-v2 Discovery Engine resources for the production environment"
+  workspace_tags    = ["govuk", "search-api-v2", "search-api-v2-environment", "production"]
+  terraform_version = var.terraform_version
+  execution_mode    = "remote"
+  auto_apply        = false
+  working_directory = "/terraform/deployments/search-api-v2/"
+  trigger_patterns = [
+    "/terraform/deployments/search-api-v2/**/*",
+    "/terraform/variables/production/search-api-v2.tfvars"
+  ]
+
+  project_name = tfe_project.search-api-v2.name
+  vcs_repo = {
+    identifier     = "alphagov/govuk-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  team_access = {
+    "GOV.UK Non-Production (r/o)" = "write"
+    "GOV.UK Production"           = "write"
+  }
+
+  envvars = {
+    "TFC_GCP_PROVIDER_AUTH"             = true
+    "TFC_GCP_WORKLOAD_PROVIDER_NAME"    = "projects/931453572747/locations/global/workloadIdentityPools/terraform-cloud-id-pool/providers/terraform-cloud-provider-oidc"
+    "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL" = "tfc-service-account@search-api-v2-production.iam.gserviceaccount.com"
+  }
+
+  tfvars_files = [
+    "production/search-api-v2.tfvars"
+  ]
+
+  variable_set_ids = [
+    local.gcp_credentials["production"]
+  ]
+}
+
+moved {
+  from = module.environment_production.tfe_workspace.environment_workspace
+  to   = module.search-api-v2-production.tfe_workspace.ws
+}
+
+moved {
+  from = module.environment_production.tfe_workspace_settings.environment_workspace_settings
+  to   = module.search-api-v2-production.tfe_workspace_settings.ws
 }
---
+++ terraform/variables/staging/search-api-v2.tfvars
@@ -0,0 +1,3 @@
+gcp_project_id     = "search-api-v2-staging"
+gcp_project_number = "773027887517"
+gcp_env            = "staging"
