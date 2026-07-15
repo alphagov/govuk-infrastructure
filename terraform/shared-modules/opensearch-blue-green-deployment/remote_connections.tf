20750a80f462d110e5e408031ad4f33d90d344e1
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2026-07-15 15:55:28 +0100

    Authorize vpc endpoint use in opensearch

--- terraform/shared-modules/opensearch-blue-green-deployment/remote_connections.tf
+++ terraform/shared-modules/opensearch-blue-green-deployment/remote_connections.tf
@@ -19,6 +19,16 @@ resource "aws_opensearch_outbound_connection" "to_green_from_blue" {
   accept_connection = true
 }
 
+resource "aws_opensearch_authorize_vpc_endpoint_access" "blue" {
+  count = var.create_remote_connection_to_import_to_green_from_blue || (
+    var.launch_blue_domain && var.blue_cluster_options != null && var.blue_cluster_options.create_vpc_endpoint
+  ) ? 1 : 0
+
+  domain_name = module.blue_domain[0].opensearch_domain_name
+  account     = data.aws_caller_identity.current.account_id
+  region      = data.aws_region.current.name
+}
+
 resource "aws_opensearch_outbound_connection" "to_blue_from_green" {
   count = var.create_remote_connection_to_import_to_green_from_blue ? 1 : 0
 
@@ -39,3 +49,14 @@ resource "aws_opensearch_outbound_connection" "to_blue_from_green" {
 
   accept_connection = true
 }
+
+resource "aws_opensearch_authorize_vpc_endpoint_access" "green" {
+  count = var.create_remote_connection_to_import_to_blue_from_green || (
+    var.launch_green_domain && var.green_cluster_options != null && var.green_cluster_options.create_vpc_endpoint
+  ) ? 1 : 0
+
+  domain_name = module.green_domain[0].opensearch_domain_name
+  account     = data.aws_caller_identity.current.account_id
+  region      = data.aws_region.current.name
+}
+
