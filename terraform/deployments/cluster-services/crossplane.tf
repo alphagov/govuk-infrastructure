5bb2338201929b9dec65de5d4f8dcefa986d6e81
jaskaran <jaskaran.sarkaria2@dsit.gov.uk>
2026-08-11 16:32:15 +0100

    fixup! fixup! fixup! feat: 🎸 add crossplane

--- terraform/deployments/cluster-services/crossplane.tf
+++ terraform/deployments/cluster-services/crossplane.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "crossplane" {
   name             = "crossplane"
   repository       = "https://charts.crossplane.io"
-  chart            = "crossplane-stable"
+  chart            = "crossplane"
   version          = "2.4.0-rc.0.223.g2d040d2da"
   namespace        = "crossplane-system"
   create_namespace = true
