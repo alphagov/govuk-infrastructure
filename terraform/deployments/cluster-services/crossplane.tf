5ccf0188721f4eabc0e4cad1eb8c804e22a0cafd
jaskaran <jaskaran.sarkaria2@dsit.gov.uk>
2026-08-11 16:40:40 +0100

    fixup! fixup! fixup! fixup! fixup! feat: 🎸 add crossplane

--- terraform/deployments/cluster-services/crossplane.tf
+++ terraform/deployments/cluster-services/crossplane.tf
@@ -1,8 +1,8 @@
 resource "helm_release" "crossplane" {
   name             = "crossplane"
   repository       = "https://charts.crossplane.io/stable"
-  chart            = "crossplane"
-  version          = "2.4.0"
+  chart            = "crossplane-stable"
+  version          = "2.3.4"
   namespace        = "crossplane-system"
   create_namespace = true
   timeout          = var.helm_timeout_seconds
