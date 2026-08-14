ca13a5d082600d71af0084c1c45d4bccba9531db
aaronfowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-08-14 08:51:45 +0100

    add govgraph ingestion SA to project-level cloud run invoker members

--- terraform/deployments/gcp-gov-graph/main-gcp.tf
+++ terraform/deployments/gcp-gov-graph/main-gcp.tf
@@ -478,6 +478,8 @@ data "google_iam_policy" "project" {
     role = "roles/workflows.invoker"
     members = [
       google_service_account.eventarc.member,
+      google_service_account.rds_parquet_bq_loader.member,
+      google_service_account.docdb_bq_loader.member,
     ]
   }
 
