8dcc929168056e98f7eb1a3c2a54c5c84946fd98
Lucy Stringer <lucy.stringer@digital.cabinet-office.gov.uk>
2026-08-21 17:22:13 +0100

    Create complete_query_requests quota_preference on production only Set value to 35000 on production only, to match quota increase already agreed with Google. Default value of 300 included in `variables.tf` represents the current default value in GCP

--- terraform/deployments/search-api-v2/cloud_quotas.tf
+++ terraform/deployments/search-api-v2/cloud_quotas.tf
@@ -7,3 +7,13 @@ resource "google_cloud_quotas_quota_preference" "evaluation_create_requests_pref
     preferred_value = var.evaluation_create_requests
   }
 }
+
+resource "google_cloud_quotas_quota_preference" "complete_query_requests_preference" {
+  parent   = "projects/${var.gcp_project_id}"
+  name     = "discoveryengine_complete_query_requests"
+  service  = "discoveryengine.googleapis.com"
+  quota_id = "CompleteQueryRequestsPerMinutePerProject"
+  quota_config {
+    preferred_value = var.complete_query_requests
+  }
+}
