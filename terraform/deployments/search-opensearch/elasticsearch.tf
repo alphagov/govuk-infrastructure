338fc379a6cca7fee123e1316323280073ca2580
Jos Koetsier <Jos.Koetsier@digital.cabinet-office.gov.uk>
2026-09-11 09:48:29 +0100

    Remove the Green Elasticsearch cluster from all environments
    Search-API now uses the blue cluster so the green ones can be
    decomissioned

--- terraform/deployments/search-opensearch/elasticsearch.tf
+++ terraform/deployments/search-opensearch/elasticsearch.tf
@@ -23,17 +23,4 @@ module "opensearch" {
 
   create_remote_connection_to_import_to_blue_from_green = var.create_remote_connection_to_import_to_blue_from_green
   create_remote_connection_to_import_to_green_from_blue = var.create_remote_connection_to_import_to_green_from_blue
-
-
-  // WARNING: The following option must be removed once the existing elasticsearch 6 green cluster has been destroyed
-  use_aws_elasticsearch_domain_resource_for_green_cluster    = var.use_aws_elasticsearch_domain_resource_for_green_cluster
-  override_aws_elasticsearch_domain_name_for_green_cluster   = "green-elasticsearch6-domain"
-  log_resource_policy_name_suffix_override_for_green_cluster = "-domain_log_write"
-  disable_node_to_node_encryption_for_green_cluster          = true
-  disable_enforced_https_for_green_cluster                   = true
-  override_security_group_ids_for_green_cluster              = [data.tfe_outputs.security.nonsensitive_values.govuk_elasticsearch6_access_sg_id]
-  override_custom_domain_endpoint_for_green_cluster          = "green-elasticsearch6.${var.govuk_environment}.govuk-internal.digital"
-  create_additional_manual_snapshot_role_name                = "green-elasticsearch6-manual-snapshot-role"
-  override_old_snapshot_bucket_name                          = "govuk-${var.govuk_environment}-elasticsearch6-manual-snapshots"
-  create_original_snapshot_bucket                            = true
 }
