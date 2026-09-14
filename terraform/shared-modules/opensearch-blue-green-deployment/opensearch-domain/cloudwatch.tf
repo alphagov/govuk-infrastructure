b7a4a0f5698ead1829bf61240fe362d024cc96d1
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2026-09-14 14:00:58 +0100

    Rip out all the deprecated and legacy code from the opensearch-shared-blue-green module submodule opensearch-domain

--- terraform/shared-modules/opensearch-blue-green-deployment/opensearch-domain/cloudwatch.tf
+++ terraform/shared-modules/opensearch-blue-green-deployment/opensearch-domain/cloudwatch.tf
@@ -3,29 +3,29 @@ locals {
 }
 
 resource "aws_cloudwatch_log_group" "index_slow_logs" {
-  name              = "${var.log_group_prefix_override != null ? var.log_group_prefix_override : "/aws/opensearch/${var.opensearch_domain_name}"}/${try(var.log_group_name_overrides.index_slow_logs, "index-slow")}"
+  name              = "/aws/opensearch/${var.opensearch_domain_name}/index-slow"
   retention_in_days = var.log_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "search_slow_logs" {
-  name              = "${var.log_group_prefix_override != null ? var.log_group_prefix_override : "/aws/opensearch/${var.opensearch_domain_name}"}/${try(var.log_group_name_overrides.search_slow_logs, "search-slow")}"
+  name              = "/aws/opensearch/${var.opensearch_domain_name}/search-slow"
   retention_in_days = var.log_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "error_logs" {
-  name              = "${var.log_group_prefix_override != null ? var.log_group_prefix_override : "/aws/opensearch/${var.opensearch_domain_name}"}/${try(var.log_group_name_overrides.error_logs, "error-logs")}"
+  name              = "/aws/opensearch/${var.opensearch_domain_name}/error-logs"
   retention_in_days = var.log_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "audit_logs" {
   count = local.audit_logs_enabled ? 1 : 0
 
-  name              = "${var.log_group_prefix_override != null ? var.log_group_prefix_override : "/aws/opensearch/${var.opensearch_domain_name}"}/audit-logs"
+  name              = "/aws/opensearch/${var.opensearch_domain_name}/audit-logs"
   retention_in_days = var.log_retention_in_days
 }
 
 resource "aws_cloudwatch_log_resource_policy" "opensearch_log_resource_policy" {
-  policy_name     = "${var.opensearch_domain_name}${var.log_resource_policy_name_suffix_override == null ? "-domain-write" : var.log_resource_policy_name_suffix_override}"
+  policy_name     = "${var.opensearch_domain_name}-domain-write"
   policy_document = data.aws_iam_policy_document.opensearch_logs.json
 }
 
@@ -64,7 +64,7 @@ data "aws_iam_policy_document" "opensearch_logs" {
       variable = "aws:SourceArn"
       // Cannot use the resource directly to set this since it must exist before the OpenSearch domain
       values = [
-        "arn:aws:es:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:domain/${var.override_aws_elasticsearch_domain_name == null ? var.opensearch_domain_name : var.override_aws_elasticsearch_domain_name}"
+        "arn:aws:es:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:domain/${var.opensearch_domain_name}"
       ]
     }
   }
