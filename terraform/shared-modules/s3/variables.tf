c0dcafb233da598aff87e690198795e6f97d597c
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2026-09-17 17:59:42 +0100

    Use the replication rules as a set so we can maintain the order

--- terraform/shared-modules/s3/variables.tf
+++ terraform/shared-modules/s3/variables.tf
@@ -175,7 +175,7 @@ variable "cors_rules" {
 variable "replication_config" {
   type = object({
     role = string
-    rules = optional(set(object({
+    rules = optional(list(object({
       id       = string
       status   = string
       priority = optional(number)
